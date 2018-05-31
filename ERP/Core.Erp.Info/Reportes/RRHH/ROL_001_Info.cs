59d7b764195348c9f7f1af0d9ace67960bb5a05e
carloscede <carloscede@hotmail.es>
2018-05-31 08:14:45 -0500

    modificacion rpt

--- ERP/Core.Erp.Info/Reportes/RRHH/ROL_001_Info.cs
+++ ERP/Core.Erp.Info/Reportes/RRHH/ROL_001_Info.cs
@@ -8,35 +8,39 @@ namespace Core.Erp.Info.Reportes.RRHH
 {
    public class ROL_001_Info
     {
-        public decimal IdEmpleado { get; set; }
-        public string Ruc { get; set; }
-        public string Empleado { get; set; }
-        public string IdRubro { get; set; }
-        public string Tag { get; set; }
-        public string DescRubroLargo { get; set; }
-        public string DescNombreRubroCorto { get; set; }
-        public int Orden { get; set; }
-        public double Valor { get; set; }
-        public string NominaLiqui { get; set; }
-        public string Nomina { get; set; }
-        public System.DateTime FechaIni { get; set; }
-        public System.DateTime FechaFin { get; set; }
-        public string EstadoPeriodo { get; set; }
-        public string Departamento { get; set; }
+        public int IdEmpresa { get; set; }
         public int IdNominaTipo { get; set; }
         public int IdNominaTipoLiqui { get; set; }
-        public int IdPeriodo { get; set; }
+        public string Descripcion { get; set; }
+        public string Observacion { get; set; }
+        public string Cerrado { get; set; }
+        public string de_descripcion { get; set; }
+        public decimal IdEmpleado { get; set; }
+        public string IdRubro { get; set; }
+        public int Orden { get; set; }
         public Nullable<bool> rub_visible_reporte { get; set; }
-        public string Division { get; set; }
-        public string Sucursal { get; set; }
-        public string CentroCosto { get; set; }
-        public Nullable<int> IdDivision { get; set; }
-        public string IdCentroCosto { get; set; }
-        public int IdEmpresa { get; set; }
-        public string CodigoEmpleado { get; set; }
+        public string pe_nombre { get; set; }
+        public string pe_nombreCompleto { get; set; }
+        public string pe_cedulaRuc { get; set; }
+        public string em_codigo { get; set; }
         public int IdDepartamento { get; set; }
-        public Nullable<int> IdArea { get; set; }
-        public string DescripcionArea { get; set; }
-        public int secuencia { get; set; }
+        public int IdArea { get; set; }
+        public int IdDivision { get; set; }
+        public string Area { get; set; }
+        public string Division { get; set; }
+        public System.DateTime pe_FechaIni { get; set; }
+        public System.DateTime pe_FechaFin { get; set; }
+        public string pe_estado { get; set; }
+        public string Nomina { get; set; }
+        public int IdPeriodo { get; set; }
+        public string ru_codRolGen { get; set; }
+        public string pe_apellido { get; set; }
+        public string ru_descripcion { get; set; }
+        public string NombreCorto { get; set; }
+        public string DescripcionProcesoNomina { get; set; }
+        public double Valor { get; set; }
+        public string Su_Descripcion { get; set; }
+        public string Empleado { get; set; }
+        public Nullable<System.DateTime> em_fechaIngaRol { get; set; }
     }
 }
