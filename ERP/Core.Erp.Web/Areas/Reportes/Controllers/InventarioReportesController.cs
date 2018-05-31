2fd60100f59e9d5962d69b5e729237fbc99f68e7
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-05-31 11:26:59 -0500

    cambios filtros reporte inv

--- ERP/Core.Erp.Web/Areas/Reportes/Controllers/InventarioReportesController.cs
+++ ERP/Core.Erp.Web/Areas/Reportes/Controllers/InventarioReportesController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Core.Erp.Info.Helps;
 
 namespace Core.Erp.Web.Areas.Reportes.Controllers
 {
@@ -40,24 +41,60 @@ namespace Core.Erp.Web.Areas.Reportes.Controllers
         public ActionResult INV_003(DateTime? fecha_corte, int IdSucursal= 0,int IdBodega= 0,decimal IdProducto =0,string IdCategoria ="",int IdLinea =0,int IdGrupo =0,int IdSubgrupo =0, bool mostrar_stock_0 = false)
         {
 
-            INV_003_Rpt model = new INV_003_Rpt();
-            model.p_IdEmpresa.Value = Convert.ToInt32(Session["IdEmpresa"]);
-            model.p_IdSucursal.Value = IdSucursal;
-            model.p_IdBodega.Value = IdBodega;
-            model.p_IdCategoria.Value = IdCategoria;
-            model.p_IdLinea.Value = IdLinea;
-            model.p_IdGrupo.Value = IdGrupo;
-            model.p_IdSubgrupo.Value = IdSubgrupo;
-            model.p_fecha_corte.Value = fecha_corte == null ? DateTime.Now : fecha_corte;
-            model.p_mostrar_stock_0.Value = mostrar_stock_0;
+            cl_filtros_Info model = new cl_filtros_Info
+            {
+                IdSucursal = IdSucursal,
+                IdBodega = IdBodega,
+                IdProducto = IdProducto,
+                IdCategoria = IdCategoria,
+                IdLinea = IdLinea,
+                IdGrupo = IdGrupo,
+                IdSubGrupo =IdSubgrupo,
+                fecha_fin = fecha_corte == null ? DateTime.Now : Convert.ToDateTime(fecha_corte),
+                mostrar_registros_0 = mostrar_stock_0
+            };
 
+            INV_003_Rpt report = new INV_003_Rpt();
+            report.p_IdEmpresa.Value = Convert.ToInt32(Session["IdEmpresa"]);
+            report.p_IdSucursal.Value = model.IdSucursal;
+            report.p_IdBodega.Value = model.IdBodega;
+            report.p_IdCategoria.Value = model.IdCategoria;
+            report.p_IdLinea.Value = model.IdLinea;
+            report.p_IdGrupo.Value = model.IdGrupo;
+            report.p_IdSubgrupo.Value = model.IdSubGrupo;
+            report.p_fecha_corte.Value = model.fecha_fin;
+            report.p_mostrar_stock_0.Value = model.mostrar_registros_0;
 
-            model.usuario = Session["IdUsuario"].ToString();
-            model.empresa = Session["nom_empresa"].ToString();
+            report.usuario = Session["IdUsuario"].ToString();
+            report.empresa = Session["nom_empresa"].ToString();
+
             if (IdProducto == 0)
-                model.RequestParameters = false;
+                report.RequestParameters = false;
+            ViewBag.Report = report;
             return View(model);
         }
+        [HttpPost]
+        public ActionResult INV_003(cl_filtros_Info model)
+        {
 
+            INV_003_Rpt report = new INV_003_Rpt();
+            report.p_IdEmpresa.Value = Convert.ToInt32(Session["IdEmpresa"]);
+            report.p_IdSucursal.Value = model.IdSucursal;
+            report.p_IdBodega.Value = model.IdBodega;
+            report.p_IdCategoria.Value = model.IdCategoria;
+            report.p_IdLinea.Value = model.IdLinea;
+            report.p_IdGrupo.Value = model.IdGrupo;
+            report.p_IdSubgrupo.Value = model.IdSubGrupo;
+            report.p_fecha_corte.Value = model.fecha_fin;
+            report.p_mostrar_stock_0.Value = model.mostrar_registros_0;
+
+            report.usuario = Session["IdUsuario"].ToString();
+            report.empresa = Session["nom_empresa"].ToString();
+
+            if (model.IdProducto == 0)
+                report.RequestParameters = false;
+            ViewBag.Report = report;
+            return View(model);
+        }
     }
 }
