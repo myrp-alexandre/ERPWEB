448a6d740c77dca5a39e1fcbd3592327547e5711
AROCA <AROCA@DESKTOP-V0N0DGJ>
2018-06-26 10:56:43 -0500

    Clases conciliacion caja bus

---
+++ ERP/Core.Erp.Bus/Caja/cp_conciliacion_Caja_det_x_ValeCaja_Bus.cs
@@ -0,0 +1,14 @@
+﻿using Core.Erp.Data.Caja;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Bus.Caja
+{
+    public class cp_conciliacion_Caja_det_x_ValeCaja_Bus
+    {
+        cp_conciliacion_Caja_det_x_ValeCaja_Data odata = new cp_conciliacion_Caja_det_x_ValeCaja_Data();
+    }
+}
