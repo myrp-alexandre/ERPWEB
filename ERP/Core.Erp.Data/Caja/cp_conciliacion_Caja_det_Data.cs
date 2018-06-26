888d284b539ddd4bf658a8fbb7af49f0a49942d5
AROCA <AROCA@DESKTOP-V0N0DGJ>
2018-06-26 10:41:14 -0500

    Clases conciliacion DATA

---
+++ ERP/Core.Erp.Data/Caja/cp_conciliacion_Caja_det_Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Info.Caja
+{
+    public class cp_conciliacion_Caja_det_Data
+    {
+    }
+}
---
+++ ERP/Core.Erp.Data/Caja/cp_conciliacion_Caja_det_x_ValeCaja_Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Data.Caja
+{
+    public class cp_conciliacion_Caja_det_x_ValeCaja_Data
+    {
+    }
+}
