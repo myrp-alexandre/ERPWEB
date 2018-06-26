4f333289ecf9724bac2f37edb94b6765c1cf4d4a
carloscede <carloscede@hotmail.es>
2018-06-26 08:14:01 -0500

    ..

--- ERP/Core.Erp.Bus/Inventario/in_transferencia_Bus.cs
+++ ERP/Core.Erp.Bus/Inventario/in_transferencia_Bus.cs
@@ -3,10 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Core.Erp.Info.Inventario;
+using Core.Erp.Data.Inventario;
 namespace Core.Erp.Bus.Inventario
 {
    public class in_transferencia_Bus
     {
+       in_transferencia_Data odata = new in_transferencia_Data();
+
+        public List<in_transferencia_Info> get_list(int IdEmpresa)
+        {
+            try
+            {
+                return odata.get_list(IdEmpresa);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool guardarDB(in_transferencia_Info info)
+        {
+            try
+            {
+                return odata.guardarDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool modificarDB(in_transferencia_Info info)
+        {
+            try
+            {
+                return odata.modificarDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool anularDB(in_transferencia_Info info)
+        {
+            try
+            {
+                return odata.guardarDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
--- ERP/Core.Erp.Bus/Inventario/in_transferencia_det_Bus.cs
+++ ERP/Core.Erp.Bus/Inventario/in_transferencia_det_Bus.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Core.Erp.Info.Inventario;
+using Core.Erp.Data.Inventario;
 namespace Core.Erp.Bus.Inventario
 {
    public class in_transferencia_det_Bus
     {
+        in_transferencia_det_Data odata = new in_transferencia_det_Data();
+
+        public List<in_transferencia_det_Info> get_list(int IdEmpresa, int IdSucursal, int IdBodega, decimal IdTransferencia)
+        {
+            try
+            {
+                return odata.get_list(IdEmpresa, IdSucursal, IdBodega, IdTransferencia);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool guardarDB(in_transferencia_Info info)
+        {
+            try
+            {
+                return odata.guardarDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool anularDB(in_transferencia_Info info)
+        {
+            try
+            {
+                return odata.guardarDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
