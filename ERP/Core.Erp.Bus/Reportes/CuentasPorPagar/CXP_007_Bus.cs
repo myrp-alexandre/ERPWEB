e6d233a113d969bdd0a483c38a02c20098e0a4fc
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-06-27 10:20:02 -0500

    CXP007

--- ERP/Core.Erp.Bus/Reportes/CuentasPorPagar/CXP_007_Bus.cs
+++ ERP/Core.Erp.Bus/Reportes/CuentasPorPagar/CXP_007_Bus.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Erp.Data.Reportes.CuentasPorPagar;
+using Core.Erp.Info.Reportes.CuentasPorPagar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,21 @@ using System.Threading.Tasks;
 
 namespace Core.Erp.Bus.Reportes.CuentasPorPagar
 {
-    class CXP_007_Bus
+    public class CXP_007_Bus
     {
+        CXP_007_Data odata = new CXP_007_Data();
+    
+        public List<CXP_007_Info> get_list(int IdEmpresa, DateTime Fecha_ini, DateTime Fecha_fin, bool Mostrar_agrupado)
+        {
+            try
+            {
+                return odata.get_list(IdEmpresa, Fecha_ini, Fecha_fin, Mostrar_agrupado);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
--- ERP/Core.Erp.Data/Reportes/CuentasPorPagar/CXP_007_Data.cs
+++ ERP/Core.Erp.Data/Reportes/CuentasPorPagar/CXP_007_Data.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Erp.Info.Reportes.CuentasPorPagar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,68 @@ using System.Threading.Tasks;
 
 namespace Core.Erp.Data.Reportes.CuentasPorPagar
 {
-    class CXP_007_Data
+    public class CXP_007_Data
     {
+        public List<CXP_007_Info> get_list(int IdEmpresa, DateTime Fecha_ini, DateTime Fecha_fin, bool Mostrar_agrupado )
+        {
+            try
+            {
+                List<CXP_007_Info> Lista;
+                using (Entities_reportes Context = new Entities_reportes())
+                {
+                    Lista = (from q in Context.VWCXP_007
+                             where q.IdEmpresa == IdEmpresa
+                             select new CXP_007_Info
+                             {
+                                 IdRow = q.IdRow,
+                                 IdEmpresa = q.IdEmpresa,
+                                 IdTipoCbte_Ogiro = q.IdTipoCbte_Ogiro,
+                                 IdCbteCble_Ogiro = q.IdCbteCble_Ogiro,
+                                 IdOrden_giro_Tipo = q.IdOrden_giro_Tipo,
+                                 Codigo = q.Codigo,
+                                 Descripcion = q.Descripcion,
+                                 IdProveedor = q.IdProveedor,
+                                 pr_nombre = q.pr_nombre,
+                                 pe_cedulaRuc = q.pe_cedulaRuc,
+                                 serie_fact = q.serie_fact,
+                                 num_factura = q.num_factura,
+                                 co_FechaFactura = q.co_FechaFactura,
+                                 subtotal_iva = q.subtotal_iva,
+                                 subtotal_sin_iva = q.subtotal_sin_iva,
+                                 valor_iva = q.valor_iva,
+                                 NAutorizacion = q.NAutorizacion,
+                                 serie_ret = q.serie_ret,
+                                 NumRetencion = q.NumRetencion,
+                                 re_baseRetencion = q.re_baseRetencion,
+                                 re_Codigo_impuesto = q.re_Codigo_impuesto,
+                                 re_Porcen_retencion = q.re_Porcen_retencion,
+                                 re_valor_retencion = q.re_valor_retencion,
+                                 RIVA_0 = q.RIVA_0,
+                                 RIVA_10 = q.RIVA_10,
+                                 RIVA_100 = q.RIVA_100,
+                                 RIVA_20 = q.RIVA_20,
+                                 RIVA_30 = q.RIVA_30,
+                                 RIVA_70 = q.RIVA_70,
+                                 RTF_0 = q.RTF_0,
+                                 RTF_0_1 = q.RTF_0_1,
+                                 RTF_1 = q.RTF_1,
+                                 RTF_10 = q.RTF_10,
+                                 RTF_100 = q.RTF_100,
+                                 RTF_2 = q.RTF_2,
+                                 RTF_8 = q.RTF_8,
+                                 Documento = q.Documento,
+                                 descripcion_cod_sri = q.descripcion_cod_sri,
+                                 re_tipoRet = q.re_tipoRet,
+                                 Num_Autorizacion_OG = q.Num_Autorizacion_OG
+                             }).ToList();
+                }
+                return Lista;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
