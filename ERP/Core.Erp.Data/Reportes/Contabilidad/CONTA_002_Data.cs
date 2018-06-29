5a1ff476fb6599bf3f96b80d6fdc4bf67abe3815
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-06-29 11:40:59 -0500

    .

---
+++ ERP/Core.Erp.Data/Reportes/Contabilidad/CONTA_002_Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Data.Reportes.Contabilidad
+{
+    class CONTA_002_Data
+    {
+    }
+}
