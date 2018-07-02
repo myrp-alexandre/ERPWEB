5fe440ca731b6efba7e6e65325431f4e26f409a0
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-02 15:43:37 -0500

    .

---
+++ ERP/Core.Erp.Data/Reportes/CuentasPorCobrar/CXC_001_diario_Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Data.Reportes.CuentasPorCobrar
+{
+    class CXC_001_diario_Data
+    {
+    }
+}
