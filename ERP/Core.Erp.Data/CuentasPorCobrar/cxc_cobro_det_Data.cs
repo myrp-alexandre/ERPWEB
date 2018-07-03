f4bec6fff155b06a0f3db409a4026f9a434a59a3
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-03 14:27:42 -0500

    .

--- ERP/Core.Erp.Data/CuentasPorCobrar/cxc_cobro_det_Data.cs
+++ ERP/Core.Erp.Data/CuentasPorCobrar/cxc_cobro_det_Data.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Erp.Info.CuentasPorCobrar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,23 @@ namespace Core.Erp.Data.CuentasPorCobrar
 {
     public class cxc_cobro_det_Data
     {
+        public List<cxc_cobro_Info> get_list_caetera(int IdEmpresa, int IdSucursal)
+        {
+            try
+            {
+                List<cxc_cobro_Info> Lista;
+
+                using (Entities_cuentas_por_cobrar Context = new Entities_cuentas_por_cobrar())
+                {
+                    Lista = new List<cxc_cobro_Info>();
+                }
+
+                return Lista;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
     }
 }
