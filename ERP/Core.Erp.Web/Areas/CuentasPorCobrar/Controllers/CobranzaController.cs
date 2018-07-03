6db4e916494f1eb954ecaacac52a3e1b0ccbb951
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-03 14:16:26 -0500

    .

--- ERP/Core.Erp.Web/Areas/CuentasPorCobrar/Controllers/CobranzaController.cs
+++ ERP/Core.Erp.Web/Areas/CuentasPorCobrar/Controllers/CobranzaController.cs
@@ -1,4 +1,5 @@
-﻿using Core.Erp.Bus.Caja;
+﻿using Core.Erp.Bus.Banco;
+using Core.Erp.Bus.Caja;
 using Core.Erp.Bus.CuentasPorCobrar;
 using Core.Erp.Bus.General;
 using Core.Erp.Info.CuentasPorCobrar;
@@ -23,6 +24,8 @@ namespace Core.Erp.Web.Areas.CuentasPorCobrar.Controllers
         caj_parametro_Bus bus_param_caja = new caj_parametro_Bus();
         cxc_cobro_tipo_Bus bus_cobro_tipo = new cxc_cobro_tipo_Bus();
         tb_persona_Bus bus_persona = new tb_persona_Bus();
+        tb_banco_Bus bus_banco = new tb_banco_Bus();
+        ba_Banco_Cuenta_Bus bus_banco_cuenta = new ba_Banco_Cuenta_Bus();
 
         #region Metodos ComboBox bajo demanda
         public ActionResult CmbCliente_Cobranza()
@@ -67,6 +70,12 @@ namespace Core.Erp.Web.Areas.CuentasPorCobrar.Controllers
 
             var lst_cobro_tipo = bus_cobro_tipo.get_list(false);
             ViewBag.lst_cobro_tipo = lst_cobro_tipo;
+
+            var lst_banco = bus_banco.get_list(false);
+            ViewBag.lst_banco = lst_banco;
+
+            var lst_banco_cuenta = bus_banco_cuenta.get_list(IdEmpresa, false);
+            ViewBag.lst_banco_cuenta = lst_banco_cuenta;
         }
         [HttpPost]
         public ActionResult Index(cl_filtros_Info model)
@@ -84,6 +93,8 @@ namespace Core.Erp.Web.Areas.CuentasPorCobrar.Controllers
             {
                 IdEmpresa = IdEmpresa,
                 IdSucursal = IdSucursal,
+                cr_fecha = DateTime.Now.Date,
+                IdCobro_tipo = "EFEC",
                 IdCaja = 1
             };
             cargar_combos();
@@ -111,6 +122,13 @@ namespace Core.Erp.Web.Areas.CuentasPorCobrar.Controllers
             ViewBag.IdSucursal = IdSucursal;
             var model = bus_cobro.get_list(IdEmpresa,IdSucursal, ViewBag.Fecha_ini, ViewBag.Fecha_fin);
             return PartialView("_GridViewPartial_cobranza", model);
+        }
+
+        [ValidateInput(false)]
+        public ActionResult GridViewPartial_cobranza_det()
+        {
+            var model = new object[0];
+            return PartialView("_GridViewPartial_cobranza_det", model);
         }
     }
 }
