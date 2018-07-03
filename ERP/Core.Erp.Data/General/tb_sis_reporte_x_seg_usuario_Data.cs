a226f2f1d5a2af23171ffc7becb56f23aa70c8b3
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-03 11:10:06 -0500

    .

---
+++ ERP/Core.Erp.Data/General/tb_sis_reporte_x_seg_usuario_Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Data.General
+{
+    class tb_sis_reporte_x_seg_usuario_Data
+    {
+    }
+}
