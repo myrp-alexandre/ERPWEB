07cd48a4e343f8b70b56d68e07e1b7eea101dae9
carloscede <carloscede@hotmail.es>
2018-07-04 15:35:34 -0500

    ...

---
+++ ERP/Core.Erp.Web/Areas/General/Controllers/Reportes_pre_impreso.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Core.Erp.Web.Areas.General.Controllers
+{
+    public class Reportes_pre_impreso
+    {
+    }
+}
