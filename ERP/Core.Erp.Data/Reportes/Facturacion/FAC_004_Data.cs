d3a999197672a0e5530f1c6bebd9868ad5839be4
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-04 15:21:33 -0500

    .

--- ERP/Core.Erp.Data/Reportes/Facturacion/FAC_004_Data.cs
+++ ERP/Core.Erp.Data/Reportes/Facturacion/FAC_004_Data.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Erp.Info.Reportes.Facturacion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,65 @@ using System.Threading.Tasks;
 
 namespace Core.Erp.Data.Reportes.Facturacion
 {
-    class FAC_004_Data
+    public class FAC_004_Data
     {
+        public List<FAC_004_Info> get_list(int IdEmpresa, int IdSucursal, int IdBodega, decimal IdNota)
+        {
+            try
+            {
+                List<FAC_004_Info> Lista;
+                using (Entities_reportes Context = new Entities_reportes())
+                {
+                    Lista = (from q in Context.VWFAC_004
+                             where q.IdEmpresa == IdEmpresa
+                             && q.IdSucursal == IdSucursal
+                             && q.IdBodega == IdBodega
+                             && q.IdNota == IdNota
+                             select new FAC_004_Info
+                             {
+                                 IdEmpresa = q.IdEmpresa,
+                                 IdSucursal = q.IdSucursal,
+                                 IdBodega = q.IdBodega,
+                                 IdNota = q.IdNota,
+                                 Secuencia = q.Secuencia,
+                                 CodTipoNota = q.CodTipoNota,
+                                 IdTipoDocumento = q.IdTipoDocumento,
+                                 numDocumento = q.numDocumento,
+                                 IdCaja = q.IdCaja,
+                                 IdCliente = q.IdCliente,
+                                 IdVendedor = q.IdVendedor,
+                                 pe_cedulaRuc = q.pe_cedulaRuc,
+                                 pe_direccion = q.pe_direccion,
+                                 pe_nombreCompleto = q.pe_nombreCompleto,
+                                 pe_telefonoCasa = q.pe_telefonoCasa,
+                                 Ve_Vendedor = q.Ve_Vendedor,
+                                 nombreProducto = q.nombreProducto,
+                                 no_fecha = q.no_fecha,
+                                 no_fecha_venc = q.no_fecha_venc,
+                                 IdDevolucion = q.IdDevolucion,
+                                 IdProducto = q.IdProducto,
+                                 IdTipoNota = q.IdTipoNota,
+                                 interes = q.interes,
+                                 sc_cantidad = q.sc_cantidad,
+                                 sc_iva = q.sc_iva,
+                                 sc_observacion = q.sc_observacion,
+                                 sc_Precio = q.sc_Precio,
+                                 sc_subtotal = q.sc_subtotal,
+                                 sc_total = q.sc_total,
+                                 bo_Descripcion = q.bo_Descripcion,
+                                 Caja = q.Caja,
+                                 Plazo = q.Plazo,
+                                 Su_Descripcion = q.Su_Descripcion,
+                                 valorFlete = q.valorFlete
+                             }).ToList();
+                }
+                return Lista;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
