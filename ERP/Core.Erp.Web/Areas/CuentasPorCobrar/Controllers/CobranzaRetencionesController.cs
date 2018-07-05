22191b2922a7ff0958b0cf5c99eba7c84efaa452
AROCA <AROCA@DESKTOP-V0N0DGJ>
2018-07-05 15:21:43 -0500

    .

--- ERP/Core.Erp.Web/Areas/CuentasPorCobrar/Controllers/CobranzaRetencionesController.cs
+++ ERP/Core.Erp.Web/Areas/CuentasPorCobrar/Controllers/CobranzaRetencionesController.cs
@@ -6,11 +6,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Core.Erp.Bus.CuentasPorCobrar;
 
 namespace Core.Erp.Web.Areas.CuentasPorCobrar.Controllers
 {
     public class CobranzaRetencionesController : Controller
     {
+        cxc_cobro_Bus bus_cobro = new cxc_cobro_Bus();
+        cxc_cobro_det_Bus bus_det = new cxc_cobro_det_Bus();
+        cxc_cobro_tipo_Bus bus_cobro_tipo = new cxc_cobro_tipo_Bus();
         #region Index
         public ActionResult Index()
         {
@@ -27,18 +31,12 @@ namespace Core.Erp.Web.Areas.CuentasPorCobrar.Controllers
         #region Aplicar retención
         public ActionResult AplicarRetencion(int IdSucursal = 0, int IdBodega = 0, decimal IdCbteVta = 0, string CodTipoDocumento = "")
         {
+            int IdEmpresa = Convert.ToInt32(SessionFixed.IdEmpresa);
             cxc_cobro_Info model = new cxc_cobro_Info
             {
-                IdEmpresa = Convert.ToInt32(SessionFixed.IdEmpresa)
+                
             };
-            if (CodTipoDocumento == "FACT")
-            {
-
-            }
-            else
-            {
-
-            }
+            
             return View(model);
         }
 
@@ -56,5 +54,82 @@ namespace Core.Erp.Web.Areas.CuentasPorCobrar.Controllers
             return PartialView("_GridViewPartial_cobranza_ret_iva", model);
         }
         #endregion
+    }
+
+
+    public class cxc_cobro_det_ret_fte_List
+    {
+        public List<cxc_cobro_det_Info> get_list()
+        {
+            if (HttpContext.Current.Session["cxc_cobro_det_ret_fte"] == null)
+            {
+                List<cxc_cobro_det_Info> list = new List<cxc_cobro_det_Info>();
+
+                HttpContext.Current.Session["cxc_cobro_det_ret_fte"] = list;
+            }
+            return (List<cxc_cobro_det_Info>)HttpContext.Current.Session["cxc_cobro_det_ret_fte"];
+        }
+
+        public void set_list(List<cxc_cobro_det_Info> list)
+        {
+            HttpContext.Current.Session["cxc_cobro_det_ret_fte"] = list;
+        }
+
+        public void AddRow(cxc_cobro_det_Info info_det)
+        {
+            List<cxc_cobro_det_Info> list = get_list();
+            if (list.Where(q => q.secuencia == info_det.secuencia).FirstOrDefault() == null)
+                list.Add(info_det);
+        }
+
+        public void UpdateRow(cxc_cobro_det_Info info_det)
+        {
+            cxc_cobro_det_Info edited_info = get_list().Where(m => m.secuencia == info_det.secuencia).First();
+            edited_info.dc_ValorPago = info_det.dc_ValorPago;
+        }
+
+        public void DeleteRow(string secuencia)
+        {
+            List<cxc_cobro_det_Info> list = get_list();
+            list.Remove(list.Where(m => m.secuencia == secuencia).First());
+        }
+    }
+
+    public class cxc_cobro_det_ret_iva_List
+    {
+        public List<cxc_cobro_det_Info> get_list()
+        {
+            if (HttpContext.Current.Session["cxc_cobro_det_ret_iva"] == null)
+            {
+                List<cxc_cobro_det_Info> list = new List<cxc_cobro_det_Info>();
+
+                HttpContext.Current.Session["cxc_cobro_det_ret_iva"] = list;
+            }
+            return (List<cxc_cobro_det_Info>)HttpContext.Current.Session["cxc_cobro_det_ret_iva"];
+        }
+
+        public void set_list(List<cxc_cobro_det_Info> list)
+        {
+            HttpContext.Current.Session["cxc_cobro_det_ret_iva"] = list;
+        }
+
+        public void AddRow(cxc_cobro_det_Info info_det)
+        {
+            List<cxc_cobro_det_Info> list = get_list();
+            if (list.Where(q => q.secuencia == info_det.secuencia).FirstOrDefault() == null)
+                list.Add(info_det);
+        }
+
+        public void UpdateRow(cxc_cobro_det_Info info_det)
+        {
+            cxc_cobro_det_Info edited_info = get_list().Where(m => m.secuencia == info_det.secuencia).First();
+            edited_info.dc_ValorPago = info_det.dc_ValorPago;
+        }
+
+        public void DeleteRow(string secuencia)
+        {
+            List<cxc_cobro_det_Info> list = get_list();
+            list.Remove(list.Where(m => m.secuencia == secuencia).First());
+        }
     }
 }
