a1e508994cd8f5f1c11068f7414bfc72358e19ab
carloscede <carloscede@hotmail.es>
2018-07-05 09:52:39 -0500

    ...

--- ERP/Core.Erp.Bus/General/tb_sis_reporte_diseno_Bus.cs
+++ ERP/Core.Erp.Bus/General/tb_sis_reporte_diseno_Bus.cs
@@ -3,10 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Core.Erp.Info.General;
+using Core.Erp.Data.General;
 namespace Core.Erp.Bus.General
 {
     class tb_sis_reporte_diseno_Bus
     {
+        tb_sis_reporte_diseno_Data odata = new tb_sis_reporte_diseno_Data();
+        public List<tb_sis_reporte_diseno_Info> get_list(bool mostrar_anulados)
+        {
+            try
+            {
+                return odata.get_list(mostrar_anulados);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public tb_sis_reporte_diseno_Info get_info(string IdPais)
+        {
+            try
+            {
+                return odata.get_info(IdPais);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool guardarDB(tb_sis_reporte_diseno_Info info)
+        {
+            try
+            {
+                return odata.guardarDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool modificarDB(tb_sis_reporte_diseno_Info info)
+        {
+            try
+            {
+                return odata.modificarDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool anularDB(tb_sis_reporte_diseno_Info info)
+        {
+            try
+            {
+                return odata.anularDB(info);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
     }
 }
--- ERP/Core.Erp.Info/General/tb_sis_reporte_diseno_Info.cs
+++ ERP/Core.Erp.Info/General/tb_sis_reporte_diseno_Info.cs
@@ -8,5 +8,17 @@ namespace Core.Erp.Info.General
 {
  public   class tb_sis_reporte_diseno_Info
     {
+        public int IdEmpresa { get; set; }
+        public string codDocumentoTipo { get; set; }
+        public byte[] File_Disenio_Reporte { get; set; }
+        public string IdUsuario { get; set; }
+        public Nullable<System.DateTime> Fecha_Transac { get; set; }
+        public string IdUsuarioUltMod { get; set; }
+        public Nullable<System.DateTime> Fecha_UltMod { get; set; }
+        public string IdUsuarioUltAnu { get; set; }
+        public Nullable<System.DateTime> Fecha_UltAnu { get; set; }
+        public string MotivoAnula { get; set; }
+        public string nom_pc { get; set; }
+        public string ip { get; set; }
     }
 }
