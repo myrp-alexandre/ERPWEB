bb683435a9f89cebb7084625bd6752c96955b716
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-09 10:22:23 -0500

    .

---
+++ ERP/Core.Erp.Data/Banco/ba_tipo_nota_Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Data.Banco
+{
+    class ba_tipo_nota_Data
+    {
+    }
+}
---
+++ ERP/Core.Erp.Info/Banco/ba_tipo_nota_Info.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Info.Banco
+{
+    class ba_tipo_nota_Info
+    {
+    }
+}
