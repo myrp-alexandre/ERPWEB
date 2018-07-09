2b1f57d6148cfd3b74ba01a0de3afcf0f0a370df
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-09 12:24:53 -0500

    .

---
+++ ERP/Core.Erp.Data/Banco/ba_Talonario_cheques_x_banco_Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Data.Banco
+{
+    class ba_Talonario_cheques_x_banco_Data
+    {
+    }
+}
---
+++ ERP/Core.Erp.Info/Banco/ba_Talonario_cheques_x_banco_Info.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Info.Banco
+{
+    class ba_Talonario_cheques_x_banco_Info
+    {
+    }
+}
