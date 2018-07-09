5cdb6807a7b81a7cc0f9986ba2c6457aca2b5c3d
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-09 11:46:52 -0500

    .

--- ERP/Core.Erp.Web/Areas/Banco/Controllers/TipoNotaBancoController.cs
+++ ERP/Core.Erp.Web/Areas/Banco/Controllers/TipoNotaBancoController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using Core.Erp.Bus.Banco;
 using Core.Erp.Info.Banco;
+using Core.Erp.Info.Helps;
 
 namespace Core.Erp.Web.Areas.Banco.Controllers
 {
@@ -26,16 +27,73 @@ namespace Core.Erp.Web.Areas.Banco.Controllers
             return PartialView("_GridViewPartial_tipo_nota", model);
         }
 
+        private void cargar_combos()
+        {
+            Dictionary<string, string> lst_tipo_nota = new Dictionary<string, string>();
+            lst_tipo_nota.Add("CHEQ", "Cheque");
+            lst_tipo_nota.Add("DEPO", "Depósito");
+            lst_tipo_nota.Add("NCBA", "Nota de crédito");
+            lst_tipo_nota.Add("NDBA", "Nota de débito");
+            ViewBag.lst_tipo_nota = lst_tipo_nota;
+        }
         public ActionResult Nuevo()
         {
             ba_tipo_nota_Info model = new ba_tipo_nota_Info();
+            cargar_combos();
             return View(model);
         }
 
-        private void cargar_combos()
+        [HttpPost]
+        public ActionResult Nuevo(ba_tipo_nota_Info model)
         {
-            
+            if(!bus_tipo.guardarDB(model))
+            {
+                cargar_combos();
+                return View(model);
+            }
+            return RedirectToAction("Index");
         }
 
+        public ActionResult Modificar(int IdTipoNota = 0)
+        {
+            int IdEmpresa = Convert.ToInt32(Session["IdEmpresa"]);
+            ba_tipo_nota_Info model = bus_tipo.get_info(IdEmpresa, IdTipoNota);
+            if (model == null)
+                return RedirectToAction("Index");
+            cargar_combos();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Modificar(ba_tipo_nota_Info model)
+        {
+            if(!bus_tipo.modificarDB(model))
+            {
+                cargar_combos();
+                return View(model);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Anular(int IdTipoNota = 0)
+        {
+            int IdEmpresa = Convert.ToInt32(Session["IdEmpresa"]);
+            ba_tipo_nota_Info model = bus_tipo.get_info(IdEmpresa, IdTipoNota);
+            if (model == null)
+                return RedirectToAction("Index");
+            cargar_combos();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Anular(ba_tipo_nota_Info model)
+        {
+            if (!bus_tipo.anularDB(model))
+            {
+                cargar_combos();
+                return View(model);
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
