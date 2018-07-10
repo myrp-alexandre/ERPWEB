5438f93056b6f00a940ac37aa72ed664ca5a8051
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-10 14:59:31 -0500

    .

--- ERP/Core.Erp.Data/Banco/ba_Talonario_cheques_x_banco_Data.cs
+++ ERP/Core.Erp.Data/Banco/ba_Talonario_cheques_x_banco_Data.cs
@@ -77,7 +77,7 @@ namespace Core.Erp.Data.Banco
                         IdCbteCble_cbtecble_Usado = Entity.IdCbteCble_cbtecble_Usado,
                         IdEmpresa_cbtecble_Usado = Entity.IdEmpresa_cbtecble_Usado,
                         IdTipoCbte_cbtecble_Usado = Entity.IdTipoCbte_cbtecble_Usado,
-                        Estado_bool = Entity.Estado == "S" ? true : false,
+                        Estado_bool = Entity.Estado == "A" ? true : Convert.ToBoolean(Entity.Estado),
                         Num_cheque = Entity.Num_cheque,
                         secuencia = Entity.secuencia,
                         Usado = Entity.Usado,
@@ -134,12 +134,12 @@ namespace Core.Erp.Data.Banco
                         IdBanco = info.IdBanco,
                         Num_cheque = info.Num_cheque,
                         Usado = info.Usado,
-                        Estado = info.Estado="A",
                         IdCbteCble_cbtecble_Usado = info.IdCbteCble_cbtecble_Usado,
                         IdEmpresa_cbtecble_Usado = info.IdEmpresa_cbtecble_Usado,
                         IdTipoCbte_cbtecble_Usado = info.IdTipoCbte_cbtecble_Usado,
                         secuencia = info.secuencia,
-                        Fecha_uso = info.Fecha_uso
+                        Fecha_uso = info.Fecha_uso,
+                        Estado = info.Estado="A"
                     };
                     Context.ba_Talonario_cheques_x_banco.Add(Entity);
                     Context.SaveChanges();
@@ -163,7 +163,7 @@ namespace Core.Erp.Data.Banco
                     if (Entity == null) return false;
 
                     Entity.Usado = info.Usado;
-                    Entity.Estado = info.Estado;
+                    Entity.Estado = info.Estado_bool == true? "A" : "I";
 
                     Context.SaveChanges();
                 }
@@ -223,6 +223,5 @@ namespace Core.Erp.Data.Banco
             }
         }
 
-        
     }
 }
