0e71cfa370adc4ed8d1e912073eb3dd68a87a1af
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-11 10:10:35 -0500

    .

---
+++ ERP/Core.Erp.Bus/Reportes/RRHH/ROL_014_Bus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Bus.Reportes.RRHH
+{
+    class ROL_014_Bus
+    {
+    }
+}
---
+++ ERP/Core.Erp.Data/Reportes/RRHH/ROL_014_Data.cs
@@ -0,0 +1,36 @@
+﻿using Core.Erp.Info.Reportes.RRHH;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Data.Reportes.RRHH
+{
+    public class ROL_014_Data
+    {
+        public List<ROL_014_Info> get_list(int IdEmpresa, int IdTipoNomina)
+        {
+            try
+            {
+                List<ROL_014_Info> Lista;
+                using (Entities_reportes Context = new Entities_reportes())
+                {
+                    Lista = (from q in Context.VWROL_014
+                             where q.IdEmpresa == IdEmpresa
+                             && q.IdTipoNomina == IdTipoNomina
+                             select new ROL_014_Info
+                             {
+
+                             }).ToList();
+                }
+                return Lista;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
