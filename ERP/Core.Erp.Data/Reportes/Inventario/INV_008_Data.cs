98202360c6cd630d2af0dae2390bb32be50ff3a3
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-11 13:25:33 -0500

    .

---
+++ ERP/Core.Erp.Data/Reportes/Inventario/INV_008_Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Data.Reportes.Inventario
+{
+    class INV_008_Data
+    {
+    }
+}
