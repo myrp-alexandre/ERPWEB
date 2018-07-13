7abe9f7bc346a54de257c98e93ffe0eec6df3615
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-13 12:03:28 -0500

    .

--- ERP/Core.Erp.Web/Areas/Facturacion/Controllers/ProformaController.cs
+++ ERP/Core.Erp.Web/Areas/Facturacion/Controllers/ProformaController.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Erp.Bus.Facturacion;
+using Core.Erp.Bus.General;
 using Core.Erp.Info.Facturacion;
 using DevExpress.Web.Mvc;
 using System;
@@ -24,6 +25,92 @@ namespace Core.Erp.Web.Areas.Facturacion.Controllers
             List<fa_proforma_Info> model = new List<fa_proforma_Info>();
             model = bus_proforma.get_list(IdEmpresa);
             return PartialView("_GridViewPartial_proforma", model);
+        }
+
+        private void cargar_combos()
+        {
+            int IdEmpresa = Convert.ToInt32(Session["IdEmpresa"]);
+            tb_sucursal_Bus bus_sucursal = new tb_sucursal_Bus();
+            var lst_sucursal = bus_sucursal.get_list(IdEmpresa, false);
+            ViewBag.lst_sucursal = lst_sucursal;
+
+            tb_bodega_Bus bus_bodega = new tb_bodega_Bus();
+            var lst_bodega = bus_bodega.get_list(IdEmpresa, false);
+            ViewBag.lst_bodega = lst_bodega;
+
+            fa_cliente_Bus bus_cliente = new fa_cliente_Bus();
+            var lst_cliente = bus_cliente.get_list(IdEmpresa, false);
+            ViewBag.lst_cliente = lst_cliente;
+
+            fa_Vendedor_Bus bus_vendedor = new fa_Vendedor_Bus();
+            var lst_vendedor = bus_vendedor.get_list(IdEmpresa, false);
+            ViewBag.lst_vendedor = lst_vendedor;
+        }
+        public ActionResult Nuevo()
+        {
+            fa_proforma_Info model = new fa_proforma_Info
+            {
+                IdEmpresa = Convert.ToInt32(Session["IdEmpresa"])
+        };
+        cargar_combos();
+        return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(fa_proforma_Info model)
+        {
+            model.IdUsuario_creacion = Session["IdUsuario"].ToString();
+            if (!bus_proforma.guardarDB(model))
+            {
+                cargar_combos();
+                return View(model);
+            };
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Modificar(int IdSucursal = 0 , decimal IdProforma = 0)
+        {
+            int IdEmpresa = Convert.ToInt32(Session["IdEmpresa"]);
+            fa_proforma_Info model = bus_proforma.get_info(IdEmpresa, IdSucursal, IdProforma);
+                if(model == null)
+                return RedirectToAction("Index");
+            cargar_combos();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Modificar(fa_proforma_Info model)
+        {
+            model.IdUsuario_modificacion = Session["IdUsuario"].ToString();
+
+            if (!bus_proforma.modificarDB(model))
+            {
+                cargar_combos();
+                return View(model);
+            };
+            return RedirectToAction("Index");
+        }
+        public ActionResult Anular(int IdSucursal = 0, decimal IdProforma = 0)
+        {
+            int IdEmpresa = Convert.ToInt32(Session["IdEmpresa"]);
+            fa_proforma_Info model = bus_proforma.get_info(IdEmpresa, IdSucursal, IdProforma);
+            if (model == null)
+                return RedirectToAction("Index");
+            cargar_combos();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Anular(fa_proforma_Info model)
+        {
+            model.IdUsuario_anulacion = Session["IdUsuario"].ToString();
+
+            if (!bus_proforma.anularDB(model))
+            {
+                cargar_combos();
+                return View(model);
+            };
+            return RedirectToAction("Index");
         }
     }
 }
