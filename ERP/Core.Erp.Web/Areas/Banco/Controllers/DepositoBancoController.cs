4c1bc3b4548a186181dbe80d97a368add60381b8
AROCA <AROCA@DESKTOP-V0N0DGJ>
2018-07-13 17:10:28 -0500

    .

--- ERP/Core.Erp.Web/Areas/Banco/Controllers/DepositoBancoController.cs
+++ ERP/Core.Erp.Web/Areas/Banco/Controllers/DepositoBancoController.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Core.Erp.Bus.Banco;
+using Core.Erp.Bus.Contabilidad;
+using Core.Erp.Bus.General;
+using Core.Erp.Info.Helps;
+using Core.Erp.Web.Areas.Contabilidad.Controllers;
+using Core.Erp.Web.Helps;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +14,48 @@ namespace Core.Erp.Web.Areas.Banco.Controllers
 {
     public class DepositoBancoController : Controller
     {
-        // GET: Banco/DepositoBanco
+        #region Variables
+        ba_Cbte_Ban_Bus bus_cbteban = new ba_Cbte_Ban_Bus();
+        tb_sucursal_Bus bus_sucursal = new tb_sucursal_Bus();
+        ba_tipo_nota_Bus bus_tipo_nota = new ba_tipo_nota_Bus();
+        tb_persona_Bus bus_persona = new tb_persona_Bus();
+        ba_Banco_Cuenta_Bus bus_banco_cuenta = new ba_Banco_Cuenta_Bus();
+        ct_cbtecble_det_List List_ct = new ct_cbtecble_det_List();
+        ct_cbtecble_det_Bus bus_det_ct = new ct_cbtecble_det_Bus();
+        string mensaje = string.Empty;
+        #endregion
+
+        #region Index
         public ActionResult Index()
         {
-            return View();
+            cl_filtros_Info model = new cl_filtros_Info
+            {
+                IdSucursal = Convert.ToInt32(SessionFixed.IdSucursal)
+            };
+            cargar_combos_consulta();
+            return View(model);
         }
+        [HttpPost]
+        public ActionResult Index(cl_filtros_Info model)
+        {
+            cargar_combos_consulta();
+            return View(model);
+        }
+        private void cargar_combos_consulta()
+        {
+            int IdEmpresa = Convert.ToInt32(SessionFixed.IdEmpresa);
+            var lst_sucursal = bus_sucursal.get_list(IdEmpresa, false);
+            ViewBag.lst_sucursal = lst_sucursal;
+        }
+        public ActionResult GridViewPartial_DepositoBanco(DateTime? Fecha_ini, DateTime? Fecha_fin, int IdSucursal = 0)
+        {
+            int IdEmpresa = Convert.ToInt32(SessionFixed.IdEmpresa);
+            ViewBag.Fecha_ini = Fecha_ini == null ? DateTime.Now.Date.AddMonths(-1) : Convert.ToDateTime(Fecha_ini);
+            ViewBag.Fecha_fin = Fecha_fin == null ? DateTime.Now.Date : Convert.ToDateTime(Fecha_fin);
+            ViewBag.IdSucursal = IdSucursal;
+            var model = bus_cbteban.get_list(IdEmpresa, ViewBag.Fecha_ini, ViewBag.Fecha_fin, IdSucursal, cl_enumeradores.eTipoCbteBancario.DEPO.ToString());
+            return PartialView("_GridViewPartial_DepositoBanco", model);
+        }
+        #endregion
     }
 }
