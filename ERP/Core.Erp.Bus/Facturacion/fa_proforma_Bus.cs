11b5e6f65990698e910e4db54327922ec9672590
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-13 10:10:11 -0500

    .

--- ERP/Core.Erp.Bus/Facturacion/fa_proforma_Bus.cs
+++ ERP/Core.Erp.Bus/Facturacion/fa_proforma_Bus.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Erp.Data.Facturacion;
+using Core.Erp.Info.Facturacion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,74 @@ using System.Threading.Tasks;
 
 namespace Core.Erp.Bus.Facturacion
 {
-    class fa_proforma_Bus
+    public class fa_proforma_Bus
     {
+        fa_proforma_Data odata = new fa_proforma_Data();
+    
+         public List<fa_proforma_Info> get_list(int IdEmpresa)
+        {
+            try
+            {
+                return odata.get_list(IdEmpresa);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public fa_proforma_Info get_info(int IdEmpresa, int IdSucursal, decimal IdProforma)
+        {
+            try
+            {
+                return odata.get_info(IdEmpresa, IdSucursal, IdProforma);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool guardarDB(fa_proforma_Info info)
+        {
+            try
+            {
+                return odata.guardarDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool modificarDB(fa_proforma_Info info)
+        {
+            try
+            {
+                return odata.modificarDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool anularDB(fa_proforma_Info info)
+        {
+            try
+            {
+                return odata.anularDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
     }
 }
--- ERP/Core.Erp.Info/Facturacion/fa_proforma_Info.cs
+++ ERP/Core.Erp.Info/Facturacion/fa_proforma_Info.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Core.Erp.Info.Facturacion
 {
-    class fa_proforma_Info
+    public class fa_proforma_Info
     {
+        public int IdEmpresa { get; set; }
+        public int IdSucursal { get; set; }
+        public decimal IdProforma { get; set; }
+        public decimal IdCliente { get; set; }
+        public string IdTerminoPago { get; set; }
+        public int pf_plazo { get; set; }
+        public string pf_codigo { get; set; }
+        public string pf_observacion { get; set; }
+        public System.DateTime pf_fecha { get; set; }
+        public System.DateTime pf_fecha_vcto { get; set; }
+        public bool estado { get; set; }
+        public string IdUsuario_creacion { get; set; }
+        public Nullable<System.DateTime> fecha_creacion { get; set; }
+        public string IdUsuario_modificacion { get; set; }
+        public Nullable<System.DateTime> fecha_modificacion { get; set; }
+        public string IdUsuario_anulacion { get; set; }
+        public Nullable<System.DateTime> fecha_anulacion { get; set; }
+        public int IdBodega { get; set; }
+        public int IdVendedor { get; set; }
+        public string pf_atencion_a { get; set; }
+        public int pr_dias_entrega { get; set; }
     }
 }
