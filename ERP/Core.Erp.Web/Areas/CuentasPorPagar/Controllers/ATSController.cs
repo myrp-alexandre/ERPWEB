d25e00875fef65b0530cb5a1c5f5f2ae5a32b9c6
carloscede <carloscede@hotmail.es>
2018-07-14 22:11:54 -0500

    ....

--- ERP/Core.Erp.Web/Areas/CuentasPorPagar/Controllers/ATSController.cs
+++ ERP/Core.Erp.Web/Areas/CuentasPorPagar/Controllers/ATSController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Core.Erp.Info.CuentasPorPagar.ATS.ATS_Info;
+using Core.Erp.Bus.Contabilidad;
+using Core.Erp.Info.Helps;
+using Core.Erp.Web.Helps;
 
 namespace Core.Erp.Web.Areas.CuentasPorPagar.Controllers
 {
@@ -12,6 +16,31 @@ namespace Core.Erp.Web.Areas.CuentasPorPagar.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Nuevo()
+        {
+
+            ats_Info model = new ats_Info
+            {
+             
+            };
+            cargar_combos();
+            return View(model);
+        }
+        public ActionResult GridViewPartial_ventas()
+        {
+            List<ventas_Info> model = new List<ventas_Info>();
+            return PartialView("_GridViewPartial_ventas", model);
+        }
+
+        private void cargar_combos(string TipoPersona = "")
+        {
+            int IdEmpresa = Convert.ToInt32(SessionFixed.IdSucursal);
+            ct_periodo_Bus bus_periodo = new ct_periodo_Bus();
+            var lst_periodos = bus_periodo.get_list(IdEmpresa,false);
+
+
         }
     }
 }
