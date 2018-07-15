b300cba8b01aa3ec9b2f3b82851ea39dbd43b157
carloscede <carloscede@hotmail.es>
2018-07-15 17:12:07 -0500

    ...

--- ERP/Core.Erp.Bus/Contabilidad/ats_Bus.cs
+++ ERP/Core.Erp.Bus/Contabilidad/ats_Bus.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Core.Erp.Info.Contabilidad.ATS.ATS_Info;
+using Core.Erp.Data.Contabilidad;
 namespace Core.Erp.Bus.Contabilidad
 {
   public  class ats_Bus
     {
+        ats_Data odata = new ats_Data();
+        public ats_Info get_info(int IdEmpresa, int IdPeriodo)
+        {
+            try
+            {
+                return odata.get_info(IdEmpresa, IdPeriodo); 
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
