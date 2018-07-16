3197189ad6b2c8adcaa0f58ab652df91668eb686
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-16 14:23:18 -0500

    .

---
+++ ERP/Core.Erp.Bus/Facturacion/fa_factura_det_Bus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Bus.Facturacion
+{
+    class fa_factura_det_Bus
+    {
+    }
+}
