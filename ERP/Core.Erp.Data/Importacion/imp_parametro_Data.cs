de8d20a198226a8b1bcddf5d039e52e69b538b5f
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-17 15:11:55 -0500

    ,

---
+++ ERP/Core.Erp.Data/Importacion/imp_parametro_Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Data.Importacion
+{
+    class imp_parametro_Data
+    {
+    }
+}
