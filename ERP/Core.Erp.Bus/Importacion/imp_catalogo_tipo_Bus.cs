7149f83238984feb3da1f14f2389cd5a2aee27b2
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-18 10:28:10 -0500

    .

--- ERP/Core.Erp.Bus/Importacion/imp_catalogo_tipo_Bus.cs
+++ ERP/Core.Erp.Bus/Importacion/imp_catalogo_tipo_Bus.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Erp.Data.Importacion;
+using Core.Erp.Info.Importacion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,69 @@ using System.Threading.Tasks;
 
 namespace Core.Erp.Bus.Importacion
 {
-    class imp_catalogo_tipo_Bus
+    public class imp_catalogo_tipo_Bus
     {
+        imp_catalogo_tipo_Data odata = new imp_catalogo_tipo_Data();
+        public List<imp_catalogo_tipo_Info> get_list(int IdCatalogo_tipo)
+        {
+            try
+            {
+                return odata.get_list(IdCatalogo_tipo);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public imp_catalogo_tipo_Info get_info(int IdCatalogo_tipo)
+        {
+            try
+            {
+                return odata.get_info(IdCatalogo_tipo);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool guardarDB(imp_catalogo_tipo_Info info)
+        {
+            try
+            {
+                return odata.guardarDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool modificarDB(imp_catalogo_tipo_Info info)
+        {
+            try
+            {
+                return odata.modificarDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool anularDB(imp_catalogo_tipo_Info info)
+        {
+            try
+            {
+                return odata.anularDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
     }
 }
--- ERP/Core.Erp.Data/Importacion/imp_catalogo_tipo_Data.cs
+++ ERP/Core.Erp.Data/Importacion/imp_catalogo_tipo_Data.cs
@@ -33,5 +33,123 @@ namespace Core.Erp.Data.Importacion
                 throw;
             }
         }
+
+        public imp_catalogo_tipo_Info get_info(int IdCatalogo_tipo)
+        {
+            try
+            {
+                imp_catalogo_tipo_Info info = new imp_catalogo_tipo_Info();
+                using (Entities_importacion Context = new Entities_importacion())
+                {
+                    imp_catalogo_tipo Entity = Context.imp_catalogo_tipo.FirstOrDefault(q => q.IdCatalogo_tipo == IdCatalogo_tipo);
+                    if (Entity == null) return null;
+
+                    info = new imp_catalogo_tipo_Info
+                    {
+                        IdCatalogo_tipo = Entity.IdCatalogo_tipo,
+                        ct_descripcion = Entity.ct_descripcion,
+                        estado = Entity.estado
+                    };
+                }
+                return info;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private int get_id()
+        {
+            try
+            {
+                int ID = 1;
+                using (Entities_importacion Context = new Entities_importacion())
+                {
+                    var lst = from q in Context.imp_catalogo_tipo
+                              select q;
+                    if (lst.Count() > 0)
+                        ID = lst.Max(q => q.IdCatalogo_tipo) + 1;
+                }
+                return ID;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool guardarDB(imp_catalogo_tipo_Info info)
+        {
+            try
+            {
+                using (Entities_importacion Context = new Entities_importacion())
+                {
+                    imp_catalogo_tipo Entity = new imp_catalogo_tipo
+                    {
+                        IdCatalogo_tipo = info.IdCatalogo_tipo = get_id(),
+                        ct_descripcion = info.ct_descripcion,
+                        estado = info.estado == true
+                    };
+                    Context.imp_catalogo_tipo.Add(Entity);
+                    Context.SaveChanges();
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool modificarDB(imp_catalogo_tipo_Info info)
+        {
+            try
+            {
+                using (Entities_importacion Context = new Entities_importacion())
+                {
+                    imp_catalogo_tipo Entity = Context.imp_catalogo_tipo.FirstOrDefault(q => q.IdCatalogo_tipo == info.IdCatalogo_tipo);
+                    if (Entity == null) return false;
+
+                    Entity.ct_descripcion = info.ct_descripcion;
+                    Context.SaveChanges();
+
+                }
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool anularDB(imp_catalogo_tipo_Info info)
+        {
+            try
+            {
+                using (Entities_importacion Context = new Entities_importacion())
+                {
+                    imp_catalogo_tipo Entity = Context.imp_catalogo_tipo.FirstOrDefault(q => q.IdCatalogo_tipo == info.IdCatalogo_tipo);
+                    if (Entity == null) return false;
+
+                    Entity.estado = info.estado == false;
+                    Context.SaveChanges();
+
+                }
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
     }
 }
