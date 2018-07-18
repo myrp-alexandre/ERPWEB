787b06df087861dd04bca73f9c53b39f0b19dd7f
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-18 09:29:05 -0500

    .

--- ERP/Core.Erp.Bus/Importacion/imp_parametro_Bus.cs
+++ ERP/Core.Erp.Bus/Importacion/imp_parametro_Bus.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Erp.Data.Importacion;
+using Core.Erp.Info.Importacion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,32 @@ namespace Core.Erp.Bus.Importacion
 {
     public class imp_parametro_Bus
     {
+        imp_parametro_Data odata = new imp_parametro_Data();
+    
+        public imp_parametro_Info get_info(int IdEmpresa)
+        {
+            try
+            {
+                return odata.get_info(IdEmpresa);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool guardarDB(imp_parametro_Info info)
+        {
+            try
+            {
+                return odata.guardarDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
--- ERP/Core.Erp.Data/Importacion/imp_parametro_Data.cs
+++ ERP/Core.Erp.Data/Importacion/imp_parametro_Data.cs
@@ -16,9 +16,40 @@ namespace Core.Erp.Data.Importacion
                 imp_parametro_Info info = new imp_parametro_Info();
                 using (Entities_importacion Context = new Entities_importacion())
                 {
-                   
+                    imp_parametro Entity = Context.imp_parametro.FirstOrDefault(q => q.IdEmpresa == IdEmpresa);
+                    if (Entity == null) return null;
+                    info = new imp_parametro_Info
+                    {
+                        IdEmpresa = Entity.IdEmpresa,
+                        IdTipoCbte_liquidacion = Entity.IdTipoCbte_liquidacion,
+                        IdTipoCbte_liquidacion_anu = Entity.IdTipoCbte_liquidacion_anu
+                    };
                 }
                 return info;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool guardarDB(imp_parametro_Info info)
+        {
+            try
+            {
+                using (Entities_importacion Context = new Entities_importacion())
+                {
+                    imp_parametro Entity = new imp_parametro
+                    {
+                        IdEmpresa = info.IdEmpresa,
+                        IdTipoCbte_liquidacion = info.IdTipoCbte_liquidacion,
+                        IdTipoCbte_liquidacion_anu = info.IdTipoCbte_liquidacion_anu
+                    };
+                    Context.imp_parametro.Add(Entity);
+                    Context.SaveChanges();
+                }
+                return true;
             }
             catch (Exception)
             {
