c34a5b1d79398532dcdf8c4d2df9f476e2c18913
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-18 10:02:26 -0500

    .

--- ERP/Core.Erp.Web/Areas/Importacion/Controllers/ParametrosImportacionController.cs
+++ ERP/Core.Erp.Web/Areas/Importacion/Controllers/ParametrosImportacionController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using Core.Erp.Bus.Importacion;
 using Core.Erp.Info.Importacion;
+using Core.Erp.Bus.Contabilidad;
 
 namespace Core.Erp.Web.Areas.Importacion.Controllers
 {
@@ -14,8 +15,28 @@ namespace Core.Erp.Web.Areas.Importacion.Controllers
         imp_parametro_Bus bus_parametro = new imp_parametro_Bus();
         public ActionResult Index()
         {
-            return View();
+            int IdEmpresa = Convert.ToInt32(Session["IdEmpresa"]);
+            imp_parametro_Info model = bus_parametro.get_info(IdEmpresa);
+            if (model == null)
+                model = new imp_parametro_Info { IdEmpresa = IdEmpresa };
+            cargar_combos();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Index(imp_parametro_Info model)
+        {
+            if (!bus_parametro.guardarDB(model))
+                ViewBag.mensaje = "No se pudieron actualizar los registros";
+            cargar_combos();
+            return View(model);
         }
 
+        private void cargar_combos()
+        {
+            int IdEmpresa = Convert.ToInt32(Session["IdEmpresa"]);
+            ct_cbtecble_tipo_Bus bus_comprobante_tipo = new ct_cbtecble_tipo_Bus();
+            var lst_tipo = bus_comprobante_tipo.get_list(IdEmpresa, false);
+            ViewBag.lst_tipo = lst_tipo;
+        }
     }
 }
