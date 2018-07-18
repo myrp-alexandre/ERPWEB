dff8544dfd62b0f2fb0fba8cf4f9f6400e64fb6d
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-18 09:37:52 -0500

    .

--- ERP/Core.Erp.Web/Areas/Importacion/Controllers/ParametrosImportacionController.cs
+++ ERP/Core.Erp.Web/Areas/Importacion/Controllers/ParametrosImportacionController.cs
@@ -1,16 +1,21 @@
-﻿using System;
+﻿using DevExpress.Web.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Core.Erp.Bus.Importacion;
+using Core.Erp.Info.Importacion;
 
 namespace Core.Erp.Web.Areas.Importacion.Controllers
 {
     public class ParametrosImportacionController : Controller
     {
+        imp_parametro_Bus bus_parametro = new imp_parametro_Bus();
         public ActionResult Index()
         {
             return View();
         }
+
     }
 }
