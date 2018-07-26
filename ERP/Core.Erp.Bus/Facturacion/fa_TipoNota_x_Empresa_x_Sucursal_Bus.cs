194f9d420761fef7ad385bf88eb5c0e43540b014
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-26 13:44:41 -0500

    .

--- ERP/Core.Erp.Bus/Facturacion/fa_TipoNota_x_Empresa_x_Sucursal_Bus.cs
+++ ERP/Core.Erp.Bus/Facturacion/fa_TipoNota_x_Empresa_x_Sucursal_Bus.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Erp.Data.Facturacion;
+using Core.Erp.Info.Facturacion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,20 @@ using System.Threading.Tasks;
 
 namespace Core.Erp.Bus.Facturacion
 {
-    class fa_TipoNota_x_Empresa_x_Sucursal_Bus
+    public class fa_TipoNota_x_Empresa_x_Sucursal_Bus
     {
+        fa_TipoNota_x_Empresa_x_Sucursal_Data odata = new fa_TipoNota_x_Empresa_x_Sucursal_Data();
+        public List<fa_TipoNota_x_Empresa_x_Sucursal_Info> get_list(int IdEmpresa, int IdSucursal, int IdTipoNota)
+        {
+            try
+            {
+                return odata.get_list(IdEmpresa, IdSucursal, IdTipoNota);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
