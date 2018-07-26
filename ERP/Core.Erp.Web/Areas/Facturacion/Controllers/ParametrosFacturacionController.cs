d44dbccaf8eb009051f6454a4b4e44ae55059ddd
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-26 10:49:20 -0500

    .

---
+++ ERP/Core.Erp.Web/Areas/Facturacion/Controllers/ParametrosFacturacionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Core.Erp.Web.Areas.Facturacion.Controllers
+{
+    public class ParametrosFacturacionController : Controller
+    {
+        // GET: Facturacion/ParametrosFacturacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
