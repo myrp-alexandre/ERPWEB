d7331c315ee687b48edece6954ddd0caf6cd91bc
carloscede <carloscede@hotmail.es>
2018-07-26 00:31:37 -0500

    ....

--- ERP/Core.Erp.Web/Areas/Importacion/Controllers/OrdenCompraExteriorController.cs
+++ ERP/Core.Erp.Web/Areas/Importacion/Controllers/OrdenCompraExteriorController.cs
@@ -10,6 +10,8 @@ using Core.Erp.Web.Helps;
 using Core.Erp.Info.CuentasPorPagar;
 using DevExpress.Web;
 using Core.Erp.Bus.CuentasPorPagar;
+using Core.Erp.Info.Contabilidad;
+using Core.Erp.Bus.Contabilidad;
 namespace Core.Erp.Web.Areas.Importacion.Controllers
 {
     
@@ -17,21 +19,36 @@ namespace Core.Erp.Web.Areas.Importacion.Controllers
     {
         imp_ordencompra_ext_Bus bus_orden = new imp_ordencompra_ext_Bus();
         cp_proveedor_Bus bus_proveedor = new cp_proveedor_Bus();
+        ct_plancta_Bus bus_plancta = new ct_plancta_Bus();
         #region Metodos ComboBox bajo demanda
         public ActionResult CmbProveedor_exterior()
         {
             cp_proveedor_Info model = new cp_proveedor_Info();
             return PartialView("_CmbProveedor_exterior", model);
         }
-
         public List<cp_proveedor_Info> get_list_bajo_demanda(ListEditItemsRequestedByFilterConditionEventArgs args)
         {
             return bus_proveedor.get_list_bajo_demanda(args, Convert.ToInt32(SessionFixed.IdEmpresa));
         }
-
         public cp_proveedor_Info get_info_bajo_demanda(ListEditItemRequestedByValueEventArgs args)
         {
             return bus_proveedor.get_info_bajo_demanda(args, Convert.ToInt32(SessionFixed.IdEmpresa));
+        }
+
+
+        public ActionResult CmbCuenta_cta_contable()
+        {
+            imp_ordencompra_ext_Info model = new imp_ordencompra_ext_Info();
+           
+            return PartialView("_CmbCuenta_contable", model);
+        }
+        public List<ct_plancta_Info> get_list_bajo_demanda_cta(ListEditItemsRequestedByFilterConditionEventArgs args)
+        {
+            return bus_plancta.get_list_bajo_demanda(args, Convert.ToInt32(SessionFixed.IdEmpresa), false);
+        }
+        public ct_plancta_Info get_info_bajo_demanda_cta(ListEditItemRequestedByValueEventArgs args)
+        {
+            return bus_plancta.get_info_bajo_demanda(args, Convert.ToInt32(SessionFixed.IdEmpresa));
         }
         #endregion
 
