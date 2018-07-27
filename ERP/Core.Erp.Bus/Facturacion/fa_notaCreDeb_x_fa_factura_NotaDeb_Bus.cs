f1e2ced4fb46fb9828ca84c115fd7f55def64c2f
AROCA <AROCA@DESKTOP-V0N0DGJ>
2018-07-27 14:01:11 -0500

    .

---
+++ ERP/Core.Erp.Bus/Facturacion/fa_notaCreDeb_x_fa_factura_NotaDeb_Bus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Bus.Facturacion
+{
+    public class fa_notaCreDeb_x_fa_factura_NotaDeb_Bus
+    {
+    }
+}
