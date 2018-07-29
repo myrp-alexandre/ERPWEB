57a870133b8a114d556f37d28744e61fdcfe7569
carloscede <carloscede@hotmail.es>
2018-07-29 08:13:52 -0500

    combo cliente guia

---
+++ ERP/Core.Erp.Web/Areas/Facturacion/Controllers/GuiaRemisionController.cs
@@ -0,0 +1,38 @@
+﻿using Core.Erp.Bus.General;
+using Core.Erp.Info.Facturacion;
+using Core.Erp.Info.General;
+using Core.Erp.Info.Helps;
+using Core.Erp.Web.Helps;
+using DevExpress.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Core.Erp.Web.Areas.Facturacion.Controllers
+{
+    public class GuiaRemisionController : Controller
+    {
+        tb_persona_Bus bus_persona = new tb_persona_Bus();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #region Metodos ComboBox bajo demanda cliente
+        public ActionResult CmbCliente_Guia()
+        {
+            fa_guia_remision_Info model = new fa_guia_remision_Info();
+            return PartialView("_CmbCliente_Guia", model);
+        }
+        public List<tb_persona_Info> get_list_bajo_demanda(ListEditItemsRequestedByFilterConditionEventArgs args)
+        {
+            return bus_persona.get_list_bajo_demanda(args, Convert.ToInt32(SessionFixed.IdEmpresa), cl_enumeradores.eTipoPersona.CLIENTE.ToString());
+        }
+        public tb_persona_Info get_info_bajo_demanda(ListEditItemRequestedByValueEventArgs args)
+        {
+            return bus_persona.get_info_bajo_demanda(args, Convert.ToInt32(SessionFixed.IdEmpresa), cl_enumeradores.eTipoPersona.CLIENTE.ToString());
+        }
+        #endregion
+    }
+}
