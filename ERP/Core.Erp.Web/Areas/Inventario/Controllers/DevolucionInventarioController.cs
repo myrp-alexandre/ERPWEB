73204ca35f8f76bff2804d64d2b9cd463ea6eb19
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-30 11:56:54 -0500

    .

--- ERP/Core.Erp.Web/Areas/Inventario/Controllers/DevolucionInventarioController.cs
+++ ERP/Core.Erp.Web/Areas/Inventario/Controllers/DevolucionInventarioController.cs
@@ -1,4 +1,5 @@
-﻿using Core.Erp.Bus.Inventario;
+﻿using Core.Erp.Bus.General;
+using Core.Erp.Bus.Inventario;
 using Core.Erp.Info.Helps;
 using Core.Erp.Info.Inventario;
 using Core.Erp.Web.Helps;
@@ -14,7 +15,9 @@ namespace Core.Erp.Web.Areas.Inventario.Controllers
     {
         #region Variables
         in_devolucion_inven_Bus bus_devolucion = new in_devolucion_inven_Bus();
+        tb_sucursal_Bus bus_sucursal = new tb_sucursal_Bus();
         #endregion
+
         #region Index
         public ActionResult Index()
         {
@@ -36,6 +39,20 @@ namespace Core.Erp.Web.Areas.Inventario.Controllers
         }
         #endregion
 
+        #region Metodos
+        private void cargar_combos()
+        {
+            int IdEmpresa = Convert.ToInt32(SessionFixed.IdEmpresa);
+            Dictionary<string, string> lst_signo = new Dictionary<string, string>();
+            lst_signo.Add("+", "Ingreso por devolución");
+            lst_signo.Add("-", "Egreso por devolución");            
+            ViewBag.lst_signo = lst_signo;
+
+            var lst_sucursal = bus_sucursal.get_list(IdEmpresa, false);
+            ViewBag.lst_sucursal = lst_sucursal;
+        }
+        #endregion
+
         #region Acciones
         public ActionResult Nuevo()
         {
@@ -43,6 +60,7 @@ namespace Core.Erp.Web.Areas.Inventario.Controllers
             {
                 IdEmpresa = Convert.ToInt32(SessionFixed.IdEmpresa)
             };
+            cargar_combos();
             return View(model);
         }
 
@@ -55,5 +73,44 @@ namespace Core.Erp.Web.Areas.Inventario.Controllers
             return View();
         }
         #endregion
+    }
+
+    public class in_devolucion_inven_det_List
+    {
+        public List<in_devolucion_inven_det_Info> get_list()
+        {
+            if (HttpContext.Current.Session["in_devolucion_inven_det_Info"] == null)
+            {
+                List<in_devolucion_inven_det_Info> list = new List<in_devolucion_inven_det_Info>();
+
+                HttpContext.Current.Session["in_devolucion_inven_det_Info"] = list;
+            }
+            return (List<in_devolucion_inven_det_Info>)HttpContext.Current.Session["in_devolucion_inven_det_Info"];
+        }
+
+        public void set_list(List<in_devolucion_inven_det_Info> list)
+        {
+            HttpContext.Current.Session["in_devolucion_inven_det_Info"] = list;
+        }
+
+        public void AddRow(in_devolucion_inven_det_Info info_det)
+        {
+            List<in_devolucion_inven_det_Info> list = get_list();
+            info_det.secuencia = list.Count == 0 ? 1 : list.Max(q => q.secuencia) + 1;
+            info_det.cant_devuelta = info_det.cant_devuelta;
+            list.Add(info_det);
+        }
+
+        public void UpdateRow(in_devolucion_inven_det_Info info_det)
+        {
+            in_devolucion_inven_det_Info edited_info = get_list().Where(m => m.secuencia == info_det.secuencia).First();
+            edited_info.cant_devuelta = info_det.cant_devuelta;
+        }
+
+        public void DeleteRow(int secuencia)
+        {
+            List<in_devolucion_inven_det_Info> list = get_list();
+            list.Remove(list.Where(m => m.secuencia == secuencia).First());
+        }
     }
 }
