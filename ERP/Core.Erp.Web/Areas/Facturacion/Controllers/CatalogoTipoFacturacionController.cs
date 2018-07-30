f3a8687dc5665eb1a316e787c84750023e625937
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-07-30 11:52:14 -0500

    .

---
+++ ERP/Core.Erp.Web/Areas/Facturacion/Controllers/CatalogoTipoFacturacionController.cs
@@ -0,0 +1,82 @@
+﻿using Core.Erp.Bus.Facturacion;
+using Core.Erp.Info.Facturacion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Core.Erp.Web.Areas.Facturacion.Controllers
+{
+    public class CatalogoTipoFacturacionController : Controller
+    {
+        fa_catalogo_tipo_Bus bus_fa_catalogotipo = new fa_catalogo_tipo_Bus();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GridViewPartial_catalogotipo_fa()
+        {
+            List<fa_catalogo_tipo_Info> model = bus_fa_catalogotipo.get_list(true);
+            return PartialView("_GridViewPartial_catalogotipo_fa", model);
+        }
+
+        public ActionResult Nuevo()
+        {
+            fa_catalogo_tipo_Info model = new fa_catalogo_tipo_Info();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(fa_catalogo_tipo_Info model)
+        {
+            if (!bus_fa_catalogotipo.guardarDB(model))
+            {
+                return View(model);
+
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Modificar(int IdCatalogo_tipo = 0)
+        {
+            fa_catalogo_tipo_Info model = bus_fa_catalogotipo.get_info(IdCatalogo_tipo);
+            if (model == null)
+                return RedirectToAction("Index");
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Modificar(fa_catalogo_tipo_Info model)
+        {
+            if (!bus_fa_catalogotipo.modificarDB(model))
+
+            {
+                return View(model);
+
+            }
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult Anular(int IdCatalogo_tipo = 0)
+        {
+            fa_catalogo_tipo_Info model = bus_fa_catalogotipo.get_info(IdCatalogo_tipo);
+            if (model == null)
+                return RedirectToAction("Index");
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Anular(fa_catalogo_tipo_Info model)
+        {
+            if (!bus_fa_catalogotipo.anularDB(model))
+            {
+                return View(model);
+
+            }
+            return RedirectToAction("Index");
+
+        }
+    }
+}
