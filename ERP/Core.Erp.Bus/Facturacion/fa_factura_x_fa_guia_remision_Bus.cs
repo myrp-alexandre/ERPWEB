9da2136aa17b7f1f7075cb7ce572ab9ebd2dcdb2
carloscede <carloscede@hotmail.es>
2018-07-30 11:18:38 -0500

    ..

---
+++ ERP/Core.Erp.Bus/Facturacion/fa_factura_x_fa_guia_remision_Bus.cs
@@ -0,0 +1,28 @@
+﻿using Core.Erp.Data.Facturacion;
+using Core.Erp.Info.Facturacion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Bus.Facturacion
+{
+  public  class fa_factura_x_fa_guia_remision_Bus
+    {
+        fa_factura_x_fa_guia_remision_Data odata = new fa_factura_x_fa_guia_remision_Data();
+        public List<fa_guia_remision_det_x_factura_Info> get_list(int IdEmpresa,decimal gi_IdGuiaRemision)
+        {
+            try
+            {
+                return odata.get_list(IdEmpresa, gi_IdGuiaRemision);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        }
+    }
--- ERP/Core.Erp.Info/Facturacion/fa_guia_remision_det_x_factura_Info.cs
+++ ERP/Core.Erp.Info/Facturacion/fa_guia_remision_det_x_factura_Info.cs
@@ -8,5 +8,16 @@ namespace Core.Erp.Info.Facturacion
 {
    public class fa_guia_remision_det_x_factura_Info
     {
+        public int IdEmpresa { get; set; }
+        public int IdSucursal { get; set; }
+        public int IdBodega { get; set; }
+        public decimal IdCbteVta { get; set; }
+        public string CodCbteVta { get; set; }
+        public string vt_tipoDoc { get; set; }
+        public string vt_serie1 { get; set; }
+        public string vt_serie2 { get; set; }
+        public string vt_NumFactura { get; set; }
+        public decimal gi_IdGuiaRemision { get; set; }
+
     }
 }
