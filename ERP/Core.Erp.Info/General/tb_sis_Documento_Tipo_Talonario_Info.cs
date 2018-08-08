a64aec9e06531fb3d19d2d52670aabaaaf4768d6
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-08-08 15:38:54 -0500

    d

--- ERP/Core.Erp.Info/General/tb_sis_Documento_Tipo_Talonario_Info.cs
+++ ERP/Core.Erp.Info/General/tb_sis_Documento_Tipo_Talonario_Info.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,19 @@ namespace Core.Erp.Info.General
 
         public int IdEmpresa { get; set; }
         public string CodDocumentoTipo { get; set; }
+        [Required(ErrorMessage = "El campo establecimiento es obligatorio")]
+        [StringLength(3, MinimumLength = 1, ErrorMessage = "el campo establecimiento debe tener mínimo 1 caracter y máximo 3")]
         public string Establecimiento { get; set; }
+        [Required(ErrorMessage = "El campo punto de emisión es obligatorio")]
+        [StringLength(3, MinimumLength = 1, ErrorMessage = "el campo punto de emisión debe tener mínimo 1 caracter y máximo 3")]
         public string PuntoEmision { get; set; }
+        [Required(ErrorMessage = "El campo número de documento es obligatorio")]
+        [StringLength(3, MinimumLength = 1, ErrorMessage = "el campo número de documento debe tener mínimo 1 caracter y máximo 3")]
         public string NumDocumento { get; set; }
         public Nullable<System.DateTime> FechaCaducidad { get; set; }
         public bool Usado { get; set; }
         public string Estado { get; set; }
+        [Required(ErrorMessage = "El campo sucursal es obligatorio")]
         public int IdSucursal { get; set; }
         public string NumAutorizacion { get; set; }
         public bool es_Documento_Electronico { get; set; }
