a4513ca59fb02dd63a8b8fa04bf9b5b72ee9e9d4
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-08-13 10:54:44 -0500

    d

--- ERP/Core.Erp.Web/Areas/ActivoFijo/Controllers/CategoriaAFController.cs
+++ ERP/Core.Erp.Web/Areas/ActivoFijo/Controllers/CategoriaAFController.cs
@@ -1,17 +1,20 @@
 ﻿using Core.Erp.Bus.ActivoFijo;
 using Core.Erp.Info.ActivoFijo;
-using DevExpress.Web.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Core.Erp.Web.Areas.ActivoFijo.Controllers
 {
+
     public class CategoriaAFController : Controller
     {
+        #region variables
         Af_Activo_fijo_Categoria_Bus bus_categoria = new Af_Activo_fijo_Categoria_Bus();
+        Af_Activo_fijo_tipo_Bus bus_tipo = new Af_Activo_fijo_tipo_Bus();
+
+        #endregion
+
+        #region Index
         public ActionResult Index(int IdEmpresa = 0 ,int IdActivoFijoTipo = 0)
         {
             ViewBag.IdEmpresa = IdEmpresa;
@@ -22,25 +25,30 @@ namespace Core.Erp.Web.Areas.ActivoFijo.Controllers
         [ValidateInput(false)]
         public ActionResult GridViewPartial_categoria_activo(int IdEmpresa = 0, int IdActivoFijoTipo = 0)
         {
-            List<Af_Activo_fijo_Categoria_Info> model = new List<Af_Activo_fijo_Categoria_Info>();
-            model = bus_categoria.get_list(IdEmpresa, IdActivoFijoTipo, true);
+            var model = bus_categoria.get_list(IdEmpresa, IdActivoFijoTipo, true);
             ViewBag.IdEmpresa = IdEmpresa;
             ViewBag.IdActivoFijoTipo = IdActivoFijoTipo;
             return PartialView("_GridViewPartial_categoria_activo", model);
         }
+        #endregion
+
+        #region Metodos
         private void cargar_combos(int IdEmpresa)
         {
-            Af_Activo_fijo_tipo_Bus bus_tipo = new Af_Activo_fijo_tipo_Bus();
             var lst_tipo= bus_tipo.get_list(IdEmpresa, false);
             ViewBag.lst_tipo = lst_tipo;
         }
+
+        #endregion
+
+        #region Acciones
         public ActionResult Nuevo(int IdEmpresa = 0, int IdActivoFijoTipo = 0)
         {
             Af_Activo_fijo_Categoria_Info model = new Af_Activo_fijo_Categoria_Info
             {
 
-            IdEmpresa = IdEmpresa,
-            IdActivoFijoTipo = IdActivoFijoTipo
+                IdEmpresa = IdEmpresa,
+                IdActivoFijoTipo = IdActivoFijoTipo
 
             };
             cargar_combos(IdEmpresa);
@@ -56,14 +64,14 @@ namespace Core.Erp.Web.Areas.ActivoFijo.Controllers
                 cargar_combos(model.IdEmpresa);
                 return View(model);
             }
-            return RedirectToAction("Index", new {IdEmpresa = model.IdEmpresa, IdActivoFijoTipo = model.IdActivoFijoTipo });
+            return RedirectToAction("Index", new { IdEmpresa = model.IdEmpresa, IdActivoFijoTipo = model.IdActivoFijoTipo });
         }
 
         public ActionResult Modificar(int IdEmpresa = 0, int IdActivoFijoTipo = 0, int IdCategoriaAF = 0)
         {
             Af_Activo_fijo_Categoria_Info model = bus_categoria.get_info(IdEmpresa, IdCategoriaAF);
             if (model == null)
-                return RedirectToAction("Index", new { IdActivoFijoTipo = IdActivoFijoTipo });
+                return RedirectToAction("Index", new { IdEmpresa = IdEmpresa, IdActivoFijoTipo = IdActivoFijoTipo });
             ViewBag.IdActivoFijoTipo = IdActivoFijoTipo;
             cargar_combos(IdEmpresa);
             return View(model);
@@ -78,14 +86,14 @@ namespace Core.Erp.Web.Areas.ActivoFijo.Controllers
                 cargar_combos(model.IdEmpresa);
                 return View(model);
             }
-            return RedirectToAction("Index", new { IdActivoFijoTipo = model.IdActivoFijoTipo });
+            return RedirectToAction("Index", new { IdEmpresa = model.IdEmpresa, IdActivoFijoTipo = model.IdActivoFijoTipo });
         }
 
-        public ActionResult Anular(int IdEmpresa =0, int IdActivoFijoTipo = 0, int IdCategoriaAF = 0)
+        public ActionResult Anular(int IdEmpresa = 0, int IdActivoFijoTipo = 0, int IdCategoriaAF = 0)
         {
             Af_Activo_fijo_Categoria_Info model = bus_categoria.get_info(IdEmpresa, IdCategoriaAF);
             if (model == null)
-                return RedirectToAction("Index", new { IdActivoFijoTipo = IdActivoFijoTipo });
+                return RedirectToAction("Index", new { IdEmpresa = IdEmpresa, IdActivoFijoTipo = IdActivoFijoTipo });
             ViewBag.IdActivoFijoTipo = IdActivoFijoTipo;
             cargar_combos(IdEmpresa);
             return View(model);
@@ -100,7 +108,9 @@ namespace Core.Erp.Web.Areas.ActivoFijo.Controllers
                 cargar_combos(model.IdEmpresa);
                 return View(model);
             }
-            return RedirectToAction("Index", new { IdActivoFijoTipo = model.IdActivoFijoTipo });
+            return RedirectToAction("Index", new { IdEmpresa = model.IdEmpresa, IdActivoFijoTipo = model.IdActivoFijoTipo });
         }
     }
+    #endregion
+
 }
