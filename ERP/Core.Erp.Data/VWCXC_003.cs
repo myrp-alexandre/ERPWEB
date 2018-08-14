479abff836138901c98d2a52c9f8293cca96e7a7
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-08-14 10:31:13 -0500

    .

--- ERP/Core.Erp.Data/VWCXC_003.cs
+++ ERP/Core.Erp.Data/VWCXC_003.cs
@@ -12,30 +12,22 @@ namespace Core.Erp.Data
     using System;
     using System.Collections.Generic;
     
-    public partial class VWFAC_005
+    public partial class VWCXC_003
     {
         public int IdEmpresa { get; set; }
         public int IdSucursal { get; set; }
         public int IdBodega { get; set; }
         public decimal IdCbteVta { get; set; }
         public string vt_tipoDoc { get; set; }
+        public string vt_NumFactura { get; set; }
+        public string pe_nombreCompleto { get; set; }
         public decimal IdCliente { get; set; }
-        public Nullable<int> IdContacto { get; set; }
-        public string NomCliente { get; set; }
-        public string NomContacto { get; set; }
         public System.DateTime vt_fecha { get; set; }
-        public double SubtotalIVA0 { get; set; }
-        public double SubtotalIVA { get; set; }
-        public double vt_iva { get; set; }
-        public double Total { get; set; }
-        public double VRetenIVA { get; set; }
-        public double VRetenFTE { get; set; }
-        public double ValorACobrar { get; set; }
-        public double VCobrado { get; set; }
-        public double Saldo { get; set; }
-        public string TipoDocumento { get; set; }
-        public Nullable<bool> EsExportacion { get; set; }
-        public string Su_Descripcion { get; set; }
-        public string Su_CodigoEstablecimiento { get; set; }
+        public Nullable<double> ValorRteFTE { get; set; }
+        public Nullable<double> ValorRteIVA { get; set; }
+        public Nullable<double> PorcentajeRetFTE { get; set; }
+        public Nullable<double> PorcentajeRetIVA { get; set; }
+        public Nullable<double> TotalRTE { get; set; }
+        public Nullable<System.DateTime> cr_fecha { get; set; }
     }
 }
