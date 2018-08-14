58c4f40dc9aa1f6b6bd81633e4441f1ad0db078a
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-08-14 12:11:31 -0500

    .

--- ERP/Core.Erp.Web/Areas/Reportes/Controllers/CuentasPorCobrarReportesController.cs
+++ ERP/Core.Erp.Web/Areas/Reportes/Controllers/CuentasPorCobrarReportesController.cs
@@ -1,13 +1,39 @@
-﻿using Core.Erp.Info.Helps;
+﻿using Core.Erp.Bus.Facturacion;
+using Core.Erp.Bus.General;
+using Core.Erp.Info.General;
+using Core.Erp.Info.Helps;
 using Core.Erp.Web.Helps;
 using Core.Erp.Web.Reportes.CuentasPorCobrar;
+using DevExpress.Web;
 using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace Core.Erp.Web.Areas.Reportes.Controllers
 {
     public class CuentasPorCobrarReportesController : Controller
     {
+
+        #region Metodos ComboBox bajo demanda
+        tb_persona_Bus bus_persona = new tb_persona_Bus();
+
+        public ActionResult CmbCliente_CXC()
+        {
+            cl_filtros_facturacion_Info model = new cl_filtros_facturacion_Info();
+            return PartialView("_CmbCliente_CXC", model);
+        }
+        public List<tb_persona_Info> get_list_bajo_demanda(ListEditItemsRequestedByFilterConditionEventArgs args)
+        {
+            return bus_persona.get_list_bajo_demanda(args, Convert.ToInt32(SessionFixed.IdEmpresa), cl_enumeradores.eTipoPersona.CLIENTE.ToString());
+        }
+        public tb_persona_Info get_info_bajo_demanda(ListEditItemRequestedByValueEventArgs args)
+        {
+            return bus_persona.get_info_bajo_demanda(args, Convert.ToInt32(SessionFixed.IdEmpresa), cl_enumeradores.eTipoPersona.CLIENTE.ToString());
+        }
+
+        
+        #endregion
+
         public ActionResult CXC_001(int IdSucursal = 0, decimal IdCobro = 0)
         {
             CXC_001_Rpt model = new CXC_001_Rpt();
@@ -32,15 +58,9 @@ namespace Core.Erp.Web.Areas.Reportes.Controllers
             model.RequestParameters = false;
             return View(model);
         }
-        public ActionResult CXC_003(DateTime? fecha_ini, DateTime? fecha_fin,  decimal IdCliente = 0)
+        public ActionResult CXC_003()
         {
-            cl_filtros_Info model = new cl_filtros_Info
-            {
-                IdCliente = IdCliente,
-                fecha_ini = fecha_ini == null ? DateTime.Now : Convert.ToDateTime(fecha_ini),
-                fecha_fin = fecha_fin == null ? DateTime.Now : Convert.ToDateTime(fecha_fin)
-            };
-
+            cl_filtros_Info model = new cl_filtros_Info();
             CXC_003_Rpt report = new CXC_003_Rpt();
             report.p_IdEmpresa.Value = Convert.ToInt32(SessionFixed.IdEmpresa);
             report.p_IdCliente.Value = model.IdCliente;
@@ -48,11 +68,10 @@ namespace Core.Erp.Web.Areas.Reportes.Controllers
             report.p_Fecha_fin.Value = model.fecha_fin;
             report.usuario = Session["IdUsuario"].ToString();
             report.empresa = Session["nom_empresa"].ToString();
-
+            ViewBag.Report = report;
 
             return View();
         }
-
         [HttpPost]
         public ActionResult CXC_003(cl_filtros_Info model)
         {
@@ -63,8 +82,8 @@ namespace Core.Erp.Web.Areas.Reportes.Controllers
             report.p_Fecha_ini.Value = model.fecha_ini;
             report.p_Fecha_fin.Value = model.fecha_fin;
             report.usuario = Session["IdUsuario"].ToString();
-            report.empresa = Session["nom_empresa"].ToString(); 
-
+            report.empresa = Session["nom_empresa"].ToString();
+            ViewBag.Report = report;
             
             return View(model);
         }
