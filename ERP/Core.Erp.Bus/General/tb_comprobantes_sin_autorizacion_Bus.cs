c395c6cf38a3a215c5a16ea5f86b6df29756732b
carloscede <carloscede@hotmail.es>
2018-08-23 23:12:00 -0500

    ....

---
+++ ERP/Core.Erp.Bus/General/tb_comprobantes_sin_autorizacion_Bus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Bus.General
+{
+   public class tb_comprobantes_sin_autorizacion_Bus
+    {
+    }
+}
