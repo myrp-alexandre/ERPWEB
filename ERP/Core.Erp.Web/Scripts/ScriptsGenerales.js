681391d8b4df32f09b8aad452ee71401151783b2
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-08-24 12:11:27 -0500

    .

--- ERP/Core.Erp.Web/Scripts/ScriptsGenerales.js
+++ ERP/Core.Erp.Web/Scripts/ScriptsGenerales.js
@@ -4,15 +4,4 @@
     win.focus();
 }
 
-$(document).on('submit', 'form', function () {
-        var buttons = $(this).find('[type="submit"]');
-        if ($(this).valid()) {
-            buttons.each(function (btn) {
-                $(buttons[btn]).prop('disabled', true);
-            });
-        } else {
-            buttons.each(function (btn) {
-                $(buttons[btn]).prop('disabled', false);
-            });
-        }
-    });
+
