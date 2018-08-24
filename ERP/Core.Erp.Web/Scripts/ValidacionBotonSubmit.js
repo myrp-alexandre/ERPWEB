c69f5f97a87b39b8fba06d4755aa568cdcd50f7c
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-08-24 12:37:24 -0500

    .

---
+++ ERP/Core.Erp.Web/Scripts/ValidacionBotonSubmit.js
@@ -0,0 +1,12 @@
+﻿$(document).on('submit', 'form', function () {
+    var buttons = $(this).find('[type="submit"]');
+    if ($(this).valid()) {
+        buttons.each(function (btn) {
+            $(buttons[btn]).prop('disabled', true);
+        });
+    } else {
+        buttons.each(function (btn) {
+            $(buttons[btn]).prop('disabled', false);
+        });
+    }
+});
