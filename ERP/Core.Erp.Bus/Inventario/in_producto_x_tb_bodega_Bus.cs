2791dae8c8ec06b51430ac9bf191a4e17b7821ba
carloscede <carloscede@hotmail.es>
2018-08-27 11:08:50 -0500

    ...

---
+++ ERP/Core.Erp.Bus/Inventario/in_producto_x_tb_bodega_Bus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Bus.Inventario
+{
+   public class in_producto_x_tb_bodega_Bus
+    {
+    }
+}
