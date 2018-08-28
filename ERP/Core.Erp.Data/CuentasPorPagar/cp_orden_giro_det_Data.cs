2faa479fff1a22a7c78e5a67d58bae632699c37b
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-08-28 09:24:27 -0500

    .

--- ERP/Core.Erp.Data/CuentasPorPagar/cp_orden_giro_det_Data.cs
+++ ERP/Core.Erp.Data/CuentasPorPagar/cp_orden_giro_det_Data.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Erp.Info.CuentasPorPagar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,51 @@ namespace Core.Erp.Data.CuentasPorPagar
 {
     public class cp_orden_giro_det_Data
     {
+        public List<cp_orden_giro_det_Info> get_list(int IdEmpresa, int IdTipoCbte_Ogiro, decimal IdCbteCble_Ogiro)
+        {
+            try
+            {
+                List<cp_orden_giro_det_Info> Lista;
+
+                using (Entities_cuentas_por_pagar Context = new Entities_cuentas_por_pagar())
+                {
+                    Lista = (from q in Context.cp_orden_giro_det
+                             where q.IdEmpresa == IdEmpresa
+                              && q.IdTipoCbte_Ogiro == IdTipoCbte_Ogiro
+                              && q.IdCbteCble_Ogiro == IdCbteCble_Ogiro
+                             select new cp_orden_giro_det_Info
+                             {
+                                 IdEmpresa = q.IdEmpresa,
+                                 IdCbteCble_Ogiro = q.IdCbteCble_Ogiro,
+                                 IdTipoCbte_Ogiro = q.IdTipoCbte_Ogiro,
+                                 Secuencia = q.Secuencia,
+                                 ing_IdEmpresa = q.ing_IdEmpresa,
+                                 ing_IdSucursal = q.ing_IdSucursal,
+                                 ing_IdMovi_inven_tipo = q.ing_IdMovi_inven_tipo,
+                                 ing_IdNumMovi = q.ing_IdNumMovi,
+                                 ing_Secuencia = q.ing_Secuencia,
+                                 IdProducto = q.IdProducto,
+                                 Cantidad = q.Cantidad,
+                                 CostoUni = q.CostoUni,
+                                 PorDescuento = q.PorDescuento,
+                                 DescuentoUni = q.DescuentoUni,
+                                 CostoUniFinal = q.CostoUniFinal,
+                                 Subtotal = q.Subtotal,
+                                 IdCod_Impuesto_Iva = q.IdCod_Impuesto_Iva,
+                                 PorIva = q.PorIva,
+                                 ValorIva = q.ValorIva,
+                                 Total = q.Total,
+                                 IdCtaCbleGasto = q.IdCtaCbleGasto
+                             }).ToList();
+                }
+
+                return Lista;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
--- ERP/Core.Erp.Info/CuentasPorPagar/cp_orden_giro_det_Info.cs
+++ ERP/Core.Erp.Info/CuentasPorPagar/cp_orden_giro_det_Info.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Core.Erp.Info.CuentasPorPagar
+﻿namespace Core.Erp.Info.CuentasPorPagar
 {
     public class cp_orden_giro_det_Info
     {
+        public int IdEmpresa { get; set; }
+        public decimal IdCbteCble_Ogiro { get; set; }
+        public int IdTipoCbte_Ogiro { get; set; }
+        public int Secuencia { get; set; }
+        public int ing_IdEmpresa { get; set; }
+        public int ing_IdSucursal { get; set; }
+        public int ing_IdMovi_inven_tipo { get; set; }
+        public decimal ing_IdNumMovi { get; set; }
+        public int ing_Secuencia { get; set; }
+        public decimal IdProducto { get; set; }
+        public double Cantidad { get; set; }
+        public double CostoUni { get; set; }
+        public double PorDescuento { get; set; }
+        public double DescuentoUni { get; set; }
+        public double CostoUniFinal { get; set; }
+        public double Subtotal { get; set; }
+        public string IdCod_Impuesto_Iva { get; set; }
+        public double PorIva { get; set; }
+        public double ValorIva { get; set; }
+        public double Total { get; set; }
+        public string IdCtaCbleGasto { get; set; }
     }
 }
