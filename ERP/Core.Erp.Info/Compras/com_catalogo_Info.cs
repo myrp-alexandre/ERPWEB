6a34e04264fe20ba41b5d16806b012dff1c98ff3
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-08-28 13:06:11 -0500

    .

--- ERP/Core.Erp.Info/Compras/com_catalogo_Info.cs
+++ ERP/Core.Erp.Info/Compras/com_catalogo_Info.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.Erp.Info.Compras
 {
-    class com_catalogo_Info
+    public class com_catalogo_Info
     {
+        [Required(ErrorMessage = ("el campo código es obligatorio"))]
+        [StringLength(25, MinimumLength = 1, ErrorMessage = "el campo código debe tener mínimo 1 caracter y máximo 25")]
+        public string IdCatalogocompra { get; set; }
+        [Required(ErrorMessage = ("el campo catálogo tipo es obligatorio"))]
+        public string IdCatalogocompra_tipo { get; set; }
+        [StringLength(15, MinimumLength = 1, ErrorMessage = "el campo código debe tener mínimo 1 caracter y máximo 15")]
+
+        public string CodCatalogo { get; set; }
+        [Required(ErrorMessage = ("el campo descripción es obligatorio"))]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "el campo descripción debe tener mínimo 1 caracter y máximo 50")]
+        public string Nombre { get; set; }
+        public string Estado { get; set; }
+        public string Abrebiatura { get; set; }
+        public string NombreIngles { get; set; }
+        public Nullable<int> Orden { get; set; }
+        public string IdUsuario { get; set; }
+        public string IdUsuarioUltMod { get; set; }
+        public Nullable<System.DateTime> FechaUltMod { get; set; }
+        public string nom_pc { get; set; }
+        public string ip { get; set; }
+        public string IdUsuarioUltAnu { get; set; }
+        public Nullable<System.DateTime> Fecha_UltAnu { get; set; }
+        public string MotiAnula { get; set; }
     }
 }
---
+++ ERP/Core.Erp.Web/Areas/Compras/Controllers/CatalogoComprasController.cs
@@ -0,0 +1,112 @@
+﻿using DevExpress.Web.Mvc;
+using Core.Erp.Bus.Compras;
+using Core.Erp.Info.Compras;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Core.Erp.Web.Areas.Compras.Controllers
+{
+    public class CatalogoComprasController : Controller
+    {
+        com_catalogo_Bus bus_catalogo = new com_catalogo_Bus();
+        public ActionResult Index(string IdCatalogocompra_tipo = "")
+        {
+            ViewBag.IdCatalogocompra_tipo = IdCatalogocompra_tipo;
+            return View();
+        }
+        
+        [ValidateInput(false)]
+        public ActionResult GridViewPartial_cat_compras(string IdCatalogocompra_tipo = "")
+        {
+            var model = bus_catalogo.get_list(IdCatalogocompra_tipo, true);
+            ViewBag.IdCatalogocompra_tipo = IdCatalogocompra_tipo;
+            return PartialView("_GridViewPartial_cat_compras", model);
+        }
+        private void cargar_combos()
+        {
+            com_catalogo_tipo_Bus bus_catalogotipo = new com_catalogo_tipo_Bus();
+            var lst_catalogo_tipo = bus_catalogotipo.get_list(false);
+            ViewBag.lst_tipos = lst_catalogo_tipo;
+        }
+        public ActionResult Nuevo(string IdCatalogocompra_tipo = "")
+        {
+            com_catalogo_Info model = new com_catalogo_Info
+            {
+                IdCatalogocompra_tipo = IdCatalogocompra_tipo
+            };
+            ViewBag.IdCatalogocompra_tipo = IdCatalogocompra_tipo;
+            cargar_combos();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(com_catalogo_Info model)
+        {
+            model.IdUsuario = Session["IdUsuario"].ToString();
+            if (bus_catalogo.validar_existe_IdCatalogo(model.IdCatalogocompra))
+            {
+                ViewBag.mensaje = "El código ya se encuentra registrado";
+                ViewBag.IdCatalogocompra_tipo = model.IdCatalogocompra_tipo;
+                cargar_combos();
+                return View(model);
+            }
+
+            if (!bus_catalogo.guardarDB(model))
+            {
+                ViewBag.IdCatalogocompra_tipo = model.IdCatalogocompra_tipo;
+                cargar_combos();
+                return View(model);
+            }
+
+            return RedirectToAction("Index", new { IdCatalogocompra_tipo = model.IdCatalogocompra_tipo });
+        }
+
+        public ActionResult Modificar(string IdCatalogocompra_tipo = "", string IdCatalogocompra = "")
+        {
+            com_catalogo_Info model = bus_catalogo.get_info(IdCatalogocompra_tipo, IdCatalogocompra);
+            if (model == null)
+                return RedirectToAction("Index", new { IdCatalogocompra_tipo = IdCatalogocompra_tipo });
+            ViewBag.IdCatalogocompra_tipo = IdCatalogocompra_tipo;
+            cargar_combos();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Modificar(com_catalogo_Info model)
+        {
+            if (!bus_catalogo.modificarDB(model))
+            {
+                ViewBag.IdCatalogocompra_tipo = model.IdCatalogocompra_tipo;
+                cargar_combos();
+                return View(model);
+            }
+            return RedirectToAction("Index", new { IdCatalogocompra_tipo = model.IdCatalogocompra_tipo });
+
+        }
+        public ActionResult Anular(string IdCatalogocompra_tipo = "", string IdCatalogocompra = "")
+        {
+            com_catalogo_Info model = bus_catalogo.get_info(IdCatalogocompra_tipo, IdCatalogocompra);
+            if (model == null)
+                return RedirectToAction("Index", new { IdCatalogocompra_tipo = IdCatalogocompra_tipo });
+            ViewBag.IdCatalogocompra_tipo = IdCatalogocompra_tipo;
+            cargar_combos();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Anular(com_catalogo_Info model)
+        {
+            if (!bus_catalogo.anularDB(model))
+            {
+                ViewBag.IdCatalogocompra_tipo = model.IdCatalogocompra_tipo;
+                cargar_combos();
+                return View(model);
+            }
+            return RedirectToAction("Index", new { IdCatalogocompra_tipo = model.IdCatalogocompra_tipo });
+
+        }
+    }
+}
