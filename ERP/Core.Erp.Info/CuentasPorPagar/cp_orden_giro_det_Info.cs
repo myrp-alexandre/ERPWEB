5b1129fb1158a08a2b09f7a4323f573c3522ec2e
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2018-08-28 16:50:28 -0500

    .

--- ERP/Core.Erp.Info/CuentasPorPagar/cp_orden_giro_det_Info.cs
+++ ERP/Core.Erp.Info/CuentasPorPagar/cp_orden_giro_det_Info.cs
@@ -1,4 +1,6 @@
-﻿namespace Core.Erp.Info.CuentasPorPagar
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Erp.Info.CuentasPorPagar
 {
     public class cp_orden_giro_det_Info
     {
@@ -11,18 +13,31 @@
         public int ing_IdMovi_inven_tipo { get; set; }
         public decimal ing_IdNumMovi { get; set; }
         public int ing_Secuencia { get; set; }
+        [Required(ErrorMessage = "El campo producto es obligatorio")]
+        [Range(0.01,long.MaxValue, ErrorMessage = "El campo producto es obligatorio")]
         public decimal IdProducto { get; set; }
+        [Required(ErrorMessage = "El campo producto es obligatorio")]
         public string IdUnidadMedida { get; set; }
+        [Required(ErrorMessage = "El campo cantidad es obligatorio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El campo cantidad es obligatorio")]
         public double Cantidad { get; set; }
+        [Required(ErrorMessage = "El campo costo unitario es obligatorio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El campo costo unitario es obligatorio")]
         public double CostoUni { get; set; }
+        [Required(ErrorMessage = "El campo porcentaje de descuento es obligatorio")]
         public double PorDescuento { get; set; }
         public double DescuentoUni { get; set; }
         public double CostoUniFinal { get; set; }
         public double Subtotal { get; set; }
+        [Required(ErrorMessage = "El campo impuesto es obligatorio")]
         public string IdCod_Impuesto_Iva { get; set; }
         public double PorIva { get; set; }
         public double ValorIva { get; set; }
         public double Total { get; set; }
         public string IdCtaCbleGasto { get; set; }
+        #region Campos que no existen en la tabla
+        public string pr_descripcion { get; set; }
+        #endregion
+
     }
 }
