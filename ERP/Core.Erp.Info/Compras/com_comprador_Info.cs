a88a932cc85efd7bd0c38a613921335434a1ae69
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-08-28 14:51:53 -0500

    .

---
+++ ERP/Core.Erp.Info/Compras/com_comprador_Info.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Info.Compras
+{
+   public class com_comprador_Info
+    {
+        public int IdEmpresa { get; set; }
+        public decimal IdComprador { get; set; }
+        public string IdUsuario_com { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+        public string IdUsuario { get; set; }
+        public Nullable<System.DateTime> Fecha_Transac { get; set; }
+        public string IdUsuarioUltMod { get; set; }
+        public Nullable<System.DateTime> Fecha_UltMod { get; set; }
+        public string IdUsuarioUltAnu { get; set; }
+        public Nullable<System.DateTime> Fecha_UltAnu { get; set; }
+        public string MotiAnula { get; set; }
+    }
+}
