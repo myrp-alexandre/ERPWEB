c0ceef363fa2b76bfe24d41375b8e31f84313963
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-08-29 15:35:30 -0500

    .

--- ERP/Core.Erp.Info/Compras/com_departamento_Info.cs
+++ ERP/Core.Erp.Info/Compras/com_departamento_Info.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.Erp.Info.Compras
 {
-    class com_departamento_Info
+   public class com_departamento_Info
     {
+        public int IdEmpresa { get; set; }
+        public decimal IdDepartamento { get; set; }
+        [Required(ErrorMessage = ("el campo descripción es obligatorio"))]
+        [StringLength(200, MinimumLength = 1, ErrorMessage = "el campo descripción debe tener mínimo 1 caracter y máximo 200")]
+        public string nom_departamento { get; set; }
+        public string Estado { get; set; }
+        public string IdUsuario { get; set; }
+        public Nullable<System.DateTime> Fecha_Transac { get; set; }
+        public string IdUsuarioUltMod { get; set; }
+        public Nullable<System.DateTime> Fecha_UltMod { get; set; }
+        public string IdUsuarioUltAnu { get; set; }
+        public Nullable<System.DateTime> Fecha_UltAnu { get; set; }
+        public string MotiAnula { get; set; }
     }
 }
---
+++ ERP/Core.Erp.Web/Areas/Compras/Controllers/DepartamentoComprasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Core.Erp.Web.Areas.Compras.Controllers
+{
+    public class DepartamentoComprasController : Controller
+    {
+        // GET: Compras/DepartamentoCompras
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
