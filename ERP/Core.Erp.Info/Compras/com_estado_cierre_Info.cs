c3e5b39e2e13478a801ceb130264642a6d1d1acc
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-08-29 14:50:25 -0500

    .

--- ERP/Core.Erp.Info/Compras/com_estado_cierre_Info.cs
+++ ERP/Core.Erp.Info/Compras/com_estado_cierre_Info.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.Erp.Info.Compras
 {
-    class com_estado_cierre_Info
+   public class com_estado_cierre_Info
     {
+        [Required(ErrorMessage = ("el campo código es obligatorio"))]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "el campo código debe tener mínimo 1 caracter y máximo 50")]
+        public string IdEstado_cierre { get; set; }
+        [Required(ErrorMessage = ("el campo descripción es obligatorio"))]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "el campo descripción debe tener mínimo 1 caracter y máximo 50")]
+        public string Descripcion { get; set; }
+        public string estado { get; set; }
+        public Nullable<System.DateTime> Fecha_Transac { get; set; }
+        public Nullable<System.DateTime> Fecha_UltMod { get; set; }
+        public string IdUsuarioUltMod { get; set; }
+        public Nullable<System.DateTime> FechaHoraAnul { get; set; }
+        public string IdUsuarioUltAnu { get; set; }
+        public string MotivoAnulacion { get; set; }
     }
 }
---
+++ ERP/Core.Erp.Web/Areas/Compras/Controllers/EstadoCierreComprasController.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Core.Erp.Bus.Compras;
+using Core.Erp.Info.Compras;
+
+namespace Core.Erp.Web.Areas.Compras.Controllers
+{
+    public class EstadoCierreComprasController : Controller
+    {
+        com_estado_cierre_Bus bus_estado = new com_estado_cierre_Bus();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ValidateInput(false)]
+        public ActionResult GridViewPartial_estadocierre()
+        {
+            var model = new object[0];
+            return PartialView("_GridViewPartial_estadocierre", model);
+        }
+    }
+}
