0f6cd15b6bbbd74bbff0512096550789e7cd963a
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-08-30 15:16:28 -0500

    .

---
+++ ERP/Core.Erp.Info/Compras/com_TerminoPago_Info.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Info.Compras
+{
+    class com_TerminoPago_Info
+    {
+        public string IdTerminoPago { get; set; }
+        public string Descripcion { get; set; }
+        public int Dias { get; set; }
+        public string Estado { get; set; }
+        public string IdUsuario { get; set; }
+        public string nom_pc { get; set; }
+        public string ip { get; set; }
+        public string IdUsuarioUltMod { get; set; }
+        public Nullable<System.DateTime> FechaUltMod { get; set; }
+        public string IdUsuarioUltAnu { get; set; }
+        public Nullable<System.DateTime> Fecha_UltAnu { get; set; }
+        public string MotiAnula { get; set; }
+    }
+}
