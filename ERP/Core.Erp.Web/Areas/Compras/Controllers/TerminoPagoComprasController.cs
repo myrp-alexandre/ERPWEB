2df7644d54e3428620e10a8c0fa4ec9d9d363064
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-08-30 15:59:03 -0500

    /

--- ERP/Core.Erp.Web/Areas/Compras/Controllers/TerminoPagoComprasController.cs
+++ ERP/Core.Erp.Web/Areas/Compras/Controllers/TerminoPagoComprasController.cs
@@ -1,16 +1,35 @@
-﻿using System;
+﻿using DevExpress.Web.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Core.Erp.Bus.Compras;
+using Core.Erp.Info.Compras;
 
 namespace Core.Erp.Web.Areas.Compras.Controllers
 {
     public class TerminoPagoComprasController : Controller
     {
+        com_TerminoPago_Bus bus_termino = new com_TerminoPago_Bus();
         public ActionResult Index()
         {
             return View();
         }
+
+        [ValidateInput(false)]
+        public ActionResult GridViewPartial_terminopago_com()
+        {
+            var model = bus_termino.get_list(true);
+            return PartialView("_GridViewPartial_terminopago_com", model);
+        }
+
+        public ActionResult Nuevo ()
+        {
+            com_TerminoPago_Info model = new com_TerminoPago_Info();
+            return View(model);
+        }
+
+
     }
 }
