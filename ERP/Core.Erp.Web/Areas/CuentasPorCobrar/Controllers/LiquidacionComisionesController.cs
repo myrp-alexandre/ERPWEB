513eca00dc5ed5aa6f8cb4b907d0200c8c2ef0be
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-09-03 10:54:16 -0500

    v

---
+++ ERP/Core.Erp.Web/Areas/CuentasPorCobrar/Controllers/LiquidacionComisionesController.cs
@@ -0,0 +1,96 @@
+﻿using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Core.Erp.Bus.CuentasPorCobrar;
+using Core.Erp.Bus.Facturacion;
+using Core.Erp.Info.CuentasPorCobrar;
+using Core.Erp.Web.Helps;
+
+namespace Core.Erp.Web.Areas.CuentasPorCobrar.Controllers
+{
+    public class LiquidacionComisionesController : Controller
+    {
+        cxc_liquidacion_comisiones_Bus bus_liq = new cxc_liquidacion_comisiones_Bus();
+        fa_Vendedor_Bus bus_vendedor = new fa_Vendedor_Bus();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ValidateInput(false)]
+        public ActionResult GridViewPartial_liquidacion_com()
+        {
+            int IdEmpresa = Convert.ToInt32(SessionFixed.IdEmpresa);
+            var model = bus_liq.get_list(IdEmpresa, true);
+            return PartialView("_GridViewPartial_liquidacion_com", model);
+        }
+
+        private void cargar_combos(int IdEmpresa)
+        {
+            var lst_vendedor = bus_vendedor.get_list(IdEmpresa, false);
+            ViewBag.lst_vendedor = lst_vendedor;
+        }
+        public ActionResult Nuevo(int IdEmpresa = 0 )
+        {
+            cxc_liquidacion_comisiones_Info model = new cxc_liquidacion_comisiones_Info
+            {
+                IdEmpresa = IdEmpresa,
+                Fecha = DateTime.Now
+            };
+            cargar_combos(IdEmpresa);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Nuevo(cxc_liquidacion_comisiones_Info model)
+        {
+            if (!bus_liq.guardarDB(model))
+            {
+                cargar_combos(model.IdEmpresa);
+                return View(model);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Modificar(int IdEmpresa = 0 , decimal IdLiquidacion = 0)
+        {
+            cxc_liquidacion_comisiones_Info model = bus_liq.get_info(IdEmpresa, IdLiquidacion);
+            if (model == null)
+                return RedirectToAction("Index");
+            cargar_combos(IdEmpresa);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Modificar(cxc_liquidacion_comisiones_Info model)
+        {
+            if (!bus_liq.modificarDB(model))
+            {
+                cargar_combos(model.IdEmpresa);
+                return View(model);
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Anular(int IdEmpresa = 0, decimal IdLiquidacion = 0)
+        {
+            cxc_liquidacion_comisiones_Info model = bus_liq.get_info(IdEmpresa, IdLiquidacion);
+            if (model == null)
+                return RedirectToAction("Index");
+            cargar_combos(IdEmpresa);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Anular(cxc_liquidacion_comisiones_Info model)
+        {
+            if (!bus_liq.anularDB(model))
+            {
+                cargar_combos(model.IdEmpresa);
+                return View(model);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
