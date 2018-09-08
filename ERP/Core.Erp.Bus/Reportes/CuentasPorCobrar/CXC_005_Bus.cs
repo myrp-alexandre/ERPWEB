e508ef8d8f2f310f9fca604fa6d81140c440d6ba
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-09-08 16:07:41 -0500

    l

--- ERP/Core.Erp.Bus/Reportes/CuentasPorCobrar/CXC_005_Bus.cs
+++ ERP/Core.Erp.Bus/Reportes/CuentasPorCobrar/CXC_005_Bus.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Erp.Data.Reportes.CuentasPorCobrar;
+using Core.Erp.Info.Reportes.CuentasPorCobrar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,21 @@ using System.Threading.Tasks;
 
 namespace Core.Erp.Bus.Reportes.CuentasPorCobrar
 {
-    class CXC_005_Bus
+    public class CXC_005_Bus
     {
-    }
+        CXC_005_Data odata = new CXC_005_Data();
+        public List<CXC_005_Info> get_list(int IdEmpresa, decimal IdCLiente, int IdContacto, DateTime? fecha_corte, bool mostrarSaldo0)
+
+        {
+            try
+            {
+                return odata.get_list(IdEmpresa, IdCLiente, IdContacto, fecha_corte, mostrarSaldo0);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+            }
 }
--- ERP/Core.Erp.Data/Reportes/CuentasPorCobrar/CXC_005_Data.cs
+++ ERP/Core.Erp.Data/Reportes/CuentasPorCobrar/CXC_005_Data.cs
@@ -9,16 +9,39 @@ namespace Core.Erp.Data.Reportes.CuentasPorCobrar
 {
   public class CXC_005_Data
     {
-        public List<CXC_005_Info> get_list()
+        public List<CXC_005_Info> get_list(int IdEmpresa, decimal IdCLiente, int IdContacto, DateTime? fecha_corte, bool mostrarSaldo0)
         {
             try
             {
+                decimal IdCliente_ini = IdCLiente;
+                decimal IdCliente_fin = IdCLiente == 0 ? 9999 : IdCLiente;
+
+                int IdContacto_ini = IdContacto;
+                int IdContacto_fin = IdContacto == 0 ? 9999 : IdContacto;
                 List<CXC_005_Info> Lista;
                 using (Entities_reportes Context = new Entities_reportes())
                 {
-                    Lista = (from q in Context.SPCXC_005(IdEmpresa)
+                    Lista = (from q in Context.SPCXC_005(IdEmpresa, IdContacto_ini, IdContacto_fin, IdContacto_ini, IdContacto_fin, fecha_corte, mostrarSaldo0)
                              select new CXC_005_Info
-                             { }).ToList();
+                             {
+                                IdEmpresa = q.IdEmpresa,
+                                IdContacto = q.IdContacto,
+                                IdSucursal = q.IdSucursal,
+                                IdBodega = q.IdBodega,
+                                IdCbteVta = q.IdCbteVta,
+                                IdCliente = q.IdCliente,
+                                Cobrado = q.Cobrado,
+                                IVA = q.IVA,
+                                NomCliente = q.NomCliente,
+                                NomContacto = q.NomContacto,
+                                Saldo = q.Saldo,
+                                Subtotal = q.Subtotal,
+                                Total = q.Total,
+                                vt_fecha = q.vt_fecha,
+                                vt_fech_venc = q.vt_fech_venc,
+                                vt_NumFactura = q.vt_NumFactura,
+                                vt_tipoDoc = q.vt_tipoDoc
+                             }).ToList();
                 }
                 return Lista;
             }
