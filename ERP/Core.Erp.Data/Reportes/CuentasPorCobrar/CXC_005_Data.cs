0a1977d290e476189f2531848e52094691b04f6e
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-09-08 15:56:02 -0500

    w

---
+++ ERP/Core.Erp.Data/Reportes/CuentasPorCobrar/CXC_005_Data.cs
@@ -0,0 +1,32 @@
+﻿using Core.Erp.Info.Reportes.CuentasPorCobrar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Data.Reportes.CuentasPorCobrar
+{
+  public class CXC_005_Data
+    {
+        public List<CXC_005_Info> get_list()
+        {
+            try
+            {
+                List<CXC_005_Info> Lista;
+                using (Entities_reportes Context = new Entities_reportes())
+                {
+                    Lista = (from q in Context.SPCXC_005(IdEmpresa)
+                             select new CXC_005_Info
+                             { }).ToList();
+                }
+                return Lista;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
