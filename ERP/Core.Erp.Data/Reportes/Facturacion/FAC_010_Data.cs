6d97c8b31cdae3b3ba6d3c53daa54da7f5a72e16
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-09-08 13:22:08 -0500

    .

--- ERP/Core.Erp.Data/Reportes/Facturacion/FAC_010_Data.cs
+++ ERP/Core.Erp.Data/Reportes/Facturacion/FAC_010_Data.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Erp.Info.Reportes.Facturacion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,39 @@ using System.Threading.Tasks;
 
 namespace Core.Erp.Data.Reportes.Facturacion
 {
-    class FAC_010_Data
-    {
+    public class FAC_010_Data
+    {public List<FAC_010_Info> get_list(int IdEmpresa, decimal IdProducto, string IdCategoria, int IdLinea, int IdGrupo, int IdSubGrupo, int IdMarca)
+        {
+            try
+            {
+                decimal IdProducto_ini = IdProducto;
+                decimal IdPrducto_fin = IdProducto == 0 ? 9999 : IdProducto;
+                
+
+                int IdMarca_ini = IdMarca;
+                int IdMarca_fin = IdMarca == 0 ? 9999 : IdMarca;
+                List<FAC_010_Info> Lista;
+                using (Entities_reportes Context = new Entities_reportes())
+                {
+                    Lista = (from q in Context.SPFAC_010(IdEmpresa, IdProducto_ini, IdPrducto_fin, IdCategoria, IdLinea, IdGrupo, IdSubGrupo, IdMarca_ini, IdMarca_fin)
+                             select new FAC_010_Info
+                             {
+                                 IdEmpresa = q.IdEmpresa,
+                                 IdProducto = q.IdProducto, 
+                                 IdCategoria = q.IdCatgeoria,
+                                 IdLinea = q.IdLinea, 
+                                 IdGrupo = q.IdGrupo,
+                                 IdSubGrupo = q.IdSubGrupo,
+
+                             }).ToList();
+                }
+                return Lista;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
--- ERP/Core.Erp.Info/Reportes/Facturacion/FAC_010_Info.cs
+++ ERP/Core.Erp.Info/Reportes/Facturacion/FAC_010_Info.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace Core.Erp.Info.Reportes.Facturacion
 {
-    class FAC_010_Info
+  public class FAC_010_Info
     {
+        public int IdEmpresa { get; set; }
+        public decimal IdProducto { get; set; }
+        public int IdProductoTipo { get; set; }
+        public int IdMarca { get; set; }
+        public string IdCategoria { get; set; }
+        public int IdLinea { get; set; }
+        public int IdGrupo { get; set; }
+        public int IdSubGrupo { get; set; }
+        public string IdPresentacion { get; set; }
+        public string NomProducto { get; set; }
+        public string NomPresentacion { get; set; }
+        public string NomMarca { get; set; }
+        public string NomTipoProducto { get; set; }
+        public string NomCategoria { get; set; }
+        public string NomLinea { get; set; }
+        public string NomGrupo { get; set; }
+        public string NomSubGrupo { get; set; }
+        public double PRECIO1 { get; set; }
+        public double PRECIO2 { get; set; }
+        public double PRECIO3 { get; set; }
+        public double PRECIO4 { get; set; }
+        public double PRECIO5 { get; set; }
+        public string Estado { get; set; }
     }
 }
