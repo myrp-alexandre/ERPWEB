28cd2d8ef29eef2d102960217b9ce6909068d72d
carloscede <carloscede@hotmail.es>
2018-09-08 19:39:06 -0500

    ....

---
+++ ERP/Core.Erp.Info/RRHH/RDEP/Rdep_Info.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Info.RRHH.RDEP
+{
+   public class Rdep_Info
+    {
+    }
+}
