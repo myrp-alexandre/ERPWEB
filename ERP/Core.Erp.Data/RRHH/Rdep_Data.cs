50d3b68e63928890146f4c16e28bb75b131b9b65
carloscede <carloscede@hotmail.es>
2018-09-08 20:08:03 -0500

    ....

--- ERP/Core.Erp.Data/RRHH/Rdep_Data.cs
+++ ERP/Core.Erp.Data/RRHH/Rdep_Data.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Core.Erp.Info.RRHH.RDEP;
 namespace Core.Erp.Data.RRHH
 {
    public class Rdep_Data
     {
+        public List<Rdep_Info> gett_list(int IdEmpresa, int Anio)
+        {
+            List<Rdep_Info> Lista = new List<Rdep_Info>();
+
+            try
+            {
+                using (Entities_rrhh context=new Entities_rrhh())
+                {
+                    string sql = "select * from EntidadRegulatoria.vwrdep_IngrEgr_x_Empleado where IdEmpresa='"+IdEmpresa+"' and pe_anio= '"+Anio+"'";
+                    Lista = context.Database.SqlQuery<Rdep_Info>(sql).ToList();
+
+                }
+
+                return Lista;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
     }
 }
