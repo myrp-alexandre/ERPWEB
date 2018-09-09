30321b77ae8d2fea225d6a1ad0a448444c1faf4c
carloscede <carloscede@hotmail.es>
2018-09-09 04:18:48 -0500

    ...

--- ERP/Core.Erp.Web/Areas/RRHH/Controllers/RdepController.cs
+++ ERP/Core.Erp.Web/Areas/RRHH/Controllers/RdepController.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using Core.Erp.Bus.General;
+using Core.Erp.Info.General;
+using Core.Erp.Info.Helps;
+using Core.Erp.Info.RRHH;
+using Core.Erp.Info.RRHH.RDEP;
+using Core.Erp.Web.Helps;
+using DevExpress.Web;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +16,23 @@ namespace Core.Erp.Web.Areas.RRHH.Controllers
     public class RdepController : Controller
     {
         // GET: RRHH/Rdep
+
+        #region Metodos ComboBox bajo demanda
+        tb_persona_Bus bus_persona = new tb_persona_Bus();
+        public ActionResult CmbEmpleado_rdep()
+        {
+            Rdep_Info model = new Rdep_Info();
+            return PartialView("_CmbEmpleado_rdep", model);
+        }
+        public List<tb_persona_Info> get_list_bajo_demanda(ListEditItemsRequestedByFilterConditionEventArgs args)
+        {
+            return bus_persona.get_list_bajo_demanda(args, Convert.ToInt32(SessionFixed.IdEmpresa), cl_enumeradores.eTipoPersona.EMPLEA.ToString());
+        }
+        public tb_persona_Info get_info_bajo_demanda(ListEditItemRequestedByValueEventArgs args)
+        {
+            return bus_persona.get_info_bajo_demanda(args, Convert.ToInt32(SessionFixed.IdEmpresa), cl_enumeradores.eTipoPersona.EMPLEA.ToString());
+        }
+        #endregion
         public ActionResult Index()
         {
             return View();
