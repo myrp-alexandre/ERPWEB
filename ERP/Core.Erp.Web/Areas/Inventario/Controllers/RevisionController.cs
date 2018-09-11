a44ecb77e12536f353e84a097ab629b397cdae63
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-09-11 10:50:23 -0500

    /

--- ERP/Core.Erp.Web/Areas/Inventario/Controllers/RevisionController.cs
+++ ERP/Core.Erp.Web/Areas/Inventario/Controllers/RevisionController.cs
@@ -15,9 +15,13 @@ namespace Core.Erp.Web.Areas.Inventario.Controllers
 {
     public class RevisionController : Controller
     {
+        #region Variables
         in_Producto_Bus bus_producto = new in_Producto_Bus();
         tb_sucursal_Bus bus_sucursal = new tb_sucursal_Bus();
         tb_bodega_Bus bus_bodega = new tb_bodega_Bus();
+        in_Marca_Bus bus_marca = new in_Marca_Bus();
+
+        #endregion
         #region ProductoPadre
         public ActionResult CmbProductoPadre_Revision()
         {
@@ -34,7 +38,6 @@ namespace Core.Erp.Web.Areas.Inventario.Controllers
             return bus_producto.get_info_bajo_demanda(args, Convert.ToInt32(SessionFixed.IdEmpresa));
         }
         #endregion
-
         private void cargar_combos(int IdEmpresa)
         {
             var lst_sucursal = bus_sucursal.get_list(IdEmpresa, false);
@@ -43,7 +46,6 @@ namespace Core.Erp.Web.Areas.Inventario.Controllers
             var lst_bodega = bus_bodega.get_list(IdEmpresa, false);
             ViewBag.lst_bodega = lst_bodega;
 
-            in_Marca_Bus bus_marca = new in_Marca_Bus();
             var lst_marca = bus_marca.get_list(IdEmpresa, false);
             lst_marca.Add(new in_Marca_Info
             {
