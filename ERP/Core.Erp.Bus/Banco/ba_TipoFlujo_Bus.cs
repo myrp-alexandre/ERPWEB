5929ebdbdbcbb357c94a248cb464cb84fcb2ddf2
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-10-23 12:27:38 -0500

    */*

--- ERP/Core.Erp.Bus/Banco/ba_TipoFlujo_Bus.cs
+++ ERP/Core.Erp.Bus/Banco/ba_TipoFlujo_Bus.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Erp.Data.Banco;
+using Core.Erp.Info.Banco;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,76 @@ using System.Threading.Tasks;
 
 namespace Core.Erp.Bus.Banco
 {
-    class ba_TipoFlujo_Bus
+   public class ba_TipoFlujo_Bus
     {
+        ba_TipoFlujo_Data odata = new ba_TipoFlujo_Data();
+        public List<ba_TipoFlujo_Info> get_list(int IdEmpresa, bool mostrar_anulados)
+        {
+            try
+            {
+                return odata.get_list(IdEmpresa, mostrar_anulados);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public ba_TipoFlujo_Info get_info(int IdEmpresa, decimal IdTipoFlujo)
+        {
+            try
+            {
+                return odata.get_info(IdEmpresa, IdTipoFlujo);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool guardarDB(ba_TipoFlujo_Info info)
+
+        {
+            try
+            {
+                return odata.guardarDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool modificarDB(ba_TipoFlujo_Info info)
+
+        {
+            try
+            {
+                return odata.modificarDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool anularDB(ba_TipoFlujo_Info info)
+
+        {
+            try
+            {
+                return odata.anularDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
     }
 }
--- ERP/Core.Erp.Info/Banco/ba_TipoFlujo_Info.cs
+++ ERP/Core.Erp.Info/Banco/ba_TipoFlujo_Info.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,12 @@ namespace Core.Erp.Info.Banco
         public int IdEmpresa { get; set; }
         public decimal IdTipoFlujo { get; set; }
         public Nullable<decimal> IdTipoFlujoPadre { get; set; }
+        [Required(ErrorMessage = ("el campo descripción es obligatorio"))]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "el campo descripción debe tener mínimo 1 caracter y máximo 50")]
         public string Descricion { get; set; }
         public string Estado { get; set; }
         public string Tipo { get; set; }
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "el campo código debe tener mínimo 1 caracter y máximo 50")]
         public string cod_flujo { get; set; }
 
 
