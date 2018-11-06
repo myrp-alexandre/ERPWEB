fc00bdd30c4f44fc38e3d22932fd583227c64f27
carloscede <carloscede@hotmail.es>
2018-11-06 12:41:32 -0800

    ...

--- ERP_database/ERP_database/dbo/Tables/ro_empleado_x_ro_rubro.sql
+++ ERP_database/ERP_database/dbo/Tables/ro_empleado_x_ro_rubro.sql
@@ -5,7 +5,7 @@
     [IdNomina_Tipo]      INT           NOT NULL,
     [IdNomina_TipoLiqui] INT           NOT NULL,
     [IdRubro]            VARCHAR (50)  NOT NULL,
-    [Valor]              MONEY         NOT NULL,
+    [Valor]              FLOAT (53)    NOT NULL,
     [Estado]             CHAR (1)      NULL,
     [IdUsuario]          VARCHAR (20)  NULL,
     [Fecha_Transac]      DATETIME      NULL,
@@ -13,12 +13,15 @@
     [Fecha_UltMod]       DATETIME      NULL,
     [IdUsuarioUltAnu]    VARCHAR (20)  NULL,
     [Fecha_UltAnu]       DATETIME      NULL,
-    [nom_pc]             VARCHAR (50)  NULL,
-    [ip]                 VARCHAR (25)  NULL,
     [MotiAnula]          VARCHAR (200) NULL,
+    [FechaInicio]        DATE          NULL,
+    [FechaFin]           DATE          NULL,
+    [es_indifinido]      BIT           NOT NULL,
     CONSTRAINT [PK_ro_empleado_x_ro_rubro] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdRubroFijo] ASC),
     CONSTRAINT [FK_ro_empleado_x_ro_rubro_ro_empleado] FOREIGN KEY ([IdEmpresa], [IdEmpleado]) REFERENCES [dbo].[ro_empleado] ([IdEmpresa], [IdEmpleado]),
     CONSTRAINT [FK_ro_empleado_x_ro_rubro_ro_Nomina_Tipoliqui] FOREIGN KEY ([IdEmpresa], [IdNomina_Tipo], [IdNomina_TipoLiqui]) REFERENCES [dbo].[ro_Nomina_Tipoliqui] ([IdEmpresa], [IdNomina_Tipo], [IdNomina_TipoLiqui]),
     CONSTRAINT [FK_ro_empleado_x_ro_rubro_ro_rubro_tipo] FOREIGN KEY ([IdEmpresa], [IdRubro]) REFERENCES [dbo].[ro_rubro_tipo] ([IdEmpresa], [IdRubro])
 );
+
+
 
