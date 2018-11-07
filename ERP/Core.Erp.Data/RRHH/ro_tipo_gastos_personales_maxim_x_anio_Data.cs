cb0ea60633e457682c8f7b6b90ed873cb3cf9d79
carloscede <carloscede@hotmail.es>
2018-11-06 16:01:55 -0800

    .....

---
+++ ERP/Core.Erp.Data/RRHH/ro_tipo_gastos_personales_maxim_x_anio_Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Data.RRHH
+{
+  public  class ro_tipo_gastos_personales_maxim_x_anio_Data
+    {
+    }
+}
---
+++ ERP/Core.Erp.Info/RRHH/ro_tipo_gastos_personales_maxim_x_anio_Info.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Info.RRHH
+{
+    class ro_tipo_gastos_personales_maxim_x_anio_Info
+    {
+    }
+}
