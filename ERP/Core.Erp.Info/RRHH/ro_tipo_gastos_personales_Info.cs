dc44495183a71435292152b2591b92f219542e89
carloscede <carloscede@hotmail.es>
2018-11-08 19:17:43 -0800

    ...

--- ERP/Core.Erp.Info/RRHH/ro_tipo_gastos_personales_Info.cs
+++ ERP/Core.Erp.Info/RRHH/ro_tipo_gastos_personales_Info.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,12 @@ namespace Core.Erp.Info.RRHH
 {
    public class ro_tipo_gastos_personales_Info
     {
+        [Required(ErrorMessage = ("El codigo del gasto es obligatorio"))]
+        [StringLength(10, MinimumLength = 4, ErrorMessage = "El campo codigo debe tener mínimo 4 caracteres y máximo 10")]
+
         public string IdTipoGasto { get; set; }
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "El campo codigo debe tener mínimo 4 caracteres y máximo 50")]
+        [Required(ErrorMessage = ("El nombre del gasto es obligatorio"))]
         public string nom_tipo_gasto { get; set; }
         public string estado { get; set; }
         public int orden { get; set; }
