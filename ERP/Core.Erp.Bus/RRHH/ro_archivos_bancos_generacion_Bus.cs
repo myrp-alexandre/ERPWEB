fd5d6f1450a6fcd598943558d2141920b916f357
carloscede <carloscede@hotmail.es>
2018-11-14 00:02:06 -0800

    .....

---
+++ ERP/Core.Erp.Bus/RRHH/ro_archivos_bancos_generacion_Bus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Erp.Bus.RRHH
+{
+   public class ro_archivos_bancos_generacion_Bus
+    {
+    }
+}
---
+++ ERP/Core.Erp.Data/RRHH/ro_archivos_bancos_generacion_Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Data.RRHH
+{
+  public  class ro_archivos_bancos_generacion_Data
+    {
+    }
+}
