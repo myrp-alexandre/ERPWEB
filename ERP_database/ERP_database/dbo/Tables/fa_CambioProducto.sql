168b6b2b880a2b854c1acd23e6c1e3cf313e5cc1
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2018-11-20 10:14:03 -0500

    .

---
+++ ERP_database/ERP_database/dbo/Tables/fa_CambioProducto.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[fa_CambioProducto] (
+    [IdEmpresa]         INT            NOT NULL,
+    [IdSucursal]        INT            NOT NULL,
+    [IdBodega]          INT            NOT NULL,
+    [IdCambio]          INT            NOT NULL,
+    [Fecha]             DATETIME       NOT NULL,
+    [Observacion]       VARCHAR (5000) NULL,
+    [Estado]            BIT            NOT NULL,
+    [IdMovi_inven_tipo] INT            NULL,
+    [IdNumMovi]         NUMERIC (18)   NULL,
+    CONSTRAINT [PK_fa_CambioProducto] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdSucursal] ASC, [IdBodega] ASC, [IdCambio] ASC),
+    CONSTRAINT [FK_fa_CambioProducto_in_Ing_Egr_Inven] FOREIGN KEY ([IdEmpresa], [IdSucursal], [IdMovi_inven_tipo], [IdNumMovi]) REFERENCES [dbo].[in_Ing_Egr_Inven] ([IdEmpresa], [IdSucursal], [IdMovi_inven_tipo], [IdNumMovi])
+);
+
