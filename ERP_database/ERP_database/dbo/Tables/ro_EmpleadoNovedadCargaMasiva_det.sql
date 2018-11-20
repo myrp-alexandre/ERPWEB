60e30f32c552f6b2275f626e0f464000d87f6c4b
carloscede <carloscede@hotmail.es>
2018-11-19 23:51:32 -0800

    ...

---
+++ ERP_database/ERP_database/dbo/Tables/ro_EmpleadoNovedadCargaMasiva_det.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ro_EmpleadoNovedadCargaMasiva_det] (
+    [IdEmpresa]     INT           NOT NULL,
+    [IdCarga]       NUMERIC (18)  NOT NULL,
+    [Secualcial]    INT           NOT NULL,
+    [IdEmpresa_nov] INT           NOT NULL,
+    [IdNovedad]     NUMERIC (18)  NOT NULL,
+    [Observacion]   VARCHAR (MAX) NULL,
+    CONSTRAINT [PK_ro_EmpleadoNovedadCargaMasiva_det] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdCarga] ASC, [Secualcial] ASC),
+    CONSTRAINT [FK_ro_EmpleadoNovedadCargaMasiva_det_ro_empleado_Novedad] FOREIGN KEY ([IdEmpresa], [IdNovedad]) REFERENCES [dbo].[ro_empleado_Novedad] ([IdEmpresa], [IdNovedad]),
+    CONSTRAINT [FK_ro_EmpleadoNovedadCargaMasiva_det_ro_EmpleadoNovedadCargaMasiva] FOREIGN KEY ([IdEmpresa], [IdCarga]) REFERENCES [dbo].[ro_EmpleadoNovedadCargaMasiva] ([IdEmpresa], [IdCarga])
+);
+
