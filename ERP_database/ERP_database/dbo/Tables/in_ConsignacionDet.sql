186d6f1252174ff0952440ac929dbd1aeb69f844
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2018-11-21 10:42:37 -0500

    .

---
+++ ERP_database/ERP_database/dbo/Tables/in_ConsignacionDet.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[in_ConsignacionDet] (
+    [IdEmpresa]      INT           NOT NULL,
+    [IdConsignacion] DECIMAL (18)  NOT NULL,
+    [Secuencia]      INT           NOT NULL,
+    [IdProducto]     NUMERIC (18)  NOT NULL,
+    [IdUnidadMedida] VARCHAR (25)  NOT NULL,
+    [Cantidad]       FLOAT (53)    NOT NULL,
+    [Costo]          FLOAT (53)    NOT NULL,
+    [Observacion]    VARCHAR (MAX) NULL,
+    CONSTRAINT [PK_in_consignacion_det] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdConsignacion] ASC, [Secuencia] ASC),
+    CONSTRAINT [FK_in_consignacion_det_In_consignacion] FOREIGN KEY ([IdEmpresa], [IdConsignacion]) REFERENCES [dbo].[in_Consignacion] ([IdEmpresa], [IdConsignacion]),
+    CONSTRAINT [FK_in_consignacion_det_in_Producto] FOREIGN KEY ([IdEmpresa], [IdProducto]) REFERENCES [dbo].[in_Producto] ([IdEmpresa], [IdProducto]),
+    CONSTRAINT [FK_in_ConsignacionDet_in_UnidadMedida] FOREIGN KEY ([IdUnidadMedida]) REFERENCES [dbo].[in_UnidadMedida] ([IdUnidadMedida])
+);
+
