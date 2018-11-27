1494e85fb94a6242316b336469ae9a0e2ba14a2a
carloscede <carloscede@hotmail.es>
2018-11-27 12:08:47 -0800

    ....

--- ERP/Core.Erp.Data/RRHH/ro_archivosCSV_Data.cs
+++ ERP/Core.Erp.Data/RRHH/ro_archivosCSV_Data.cs
@@ -19,14 +19,24 @@ namespace Core.Erp.Data.RRHH
 
                 using (Entities_rrhh db = new Entities_rrhh())
                 {
-                    oListado = from q in db.spROL_DecimosCSV(idEmpresa, IdRol, IdRubro)
-                               select new ro_rol_detalle_Info
-                               {
-                                   IdEmpresa = q.IdEmpresa,
-                                   IdEmpleado = q.IdEmpleado,
-                                   pe_ape
+                    //oListado = from q in db.spROL_DecimosCSV(idEmpresa, IdRol, IdRubro)
+                    //           select new ro_archivosCSV_Info
+                    //           {
+                    //               IdEmpresa = q.IdEmpresa,
+                    //               IdEmpleado = q.IdEmpleado,
+                    //               pe_apellido=q.pe_apellido,
+                    //               pe_nombre=q.pe_nombre,
+                    //               pe_cedulaRuc=q.pe_cedulaRuc,
+                    //               CodigoSectorial=q.CodigoSectorial,
+                    //               ca_descripcion=q.ca_descripcion,
+                    //               Valor=q.Valor,
+                    //               pe_sexo=q.pe_sexo,
+                    //               Estado=q.Estado,
+                    //               em_fechaIngaRol=q.em_fechaIngaRol,
+                    //               DiasA_considerar_Decimo=q.DiasA_considerar_Decimo,
+                    //               Dias_Decimo=q.Dias_Decimo
 
-                               }).ToList();
+                    //           }).();
                 }
                 return oListado;
             }
