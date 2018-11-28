9219f4e40fee64bf24d55ccee1568b56cb27917f
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-11-27 22:29:59 -0500

    0

---
+++ ERP/Core.Erp.Web/Reportes/Compra/COMP_001_Rpt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace Core.Erp.Web.Reportes.Compra
+{
+    public partial class COMP_001_Rpt : DevExpress.XtraReports.UI.XtraReport
+    {
+        public COMP_001_Rpt()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
