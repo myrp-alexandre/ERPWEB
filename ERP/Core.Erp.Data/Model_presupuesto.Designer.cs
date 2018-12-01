1c037f105cc7b4712cf4da364d026f77ed6f1da1
Jorge Aroca <jaroca@fixed-sa.com>
2018-12-01 14:08:50 -0500

    .

--- ERP/Core.Erp.Data/Model_presupuesto.Designer.cs
+++ ERP/Core.Erp.Data/Model_presupuesto.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\AROCA\Documents\GitHub\ERPWEB\ERP\Core.Erp.Data\Model_contabilidad.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\AROCA\Documents\GitHub\ERPWEB\ERP\Core.Erp.Data\Model_presupuesto.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
