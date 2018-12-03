acc4b9178d0bda51eced36decd19679bbc0c9f31
carloscede <carloscede@hotmail.es>
2018-12-03 00:46:22 -0800

    ....

--- ERP/Core.Erp.Data/RRHH/ro_ArchivosIess_Data.cs
+++ ERP/Core.Erp.Data/RRHH/ro_ArchivosIess_Data.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Core.Erp.Info.RRHH;
 namespace Core.Erp.Data.RRHH
 {
    public class ro_ArchivosIess_Data
     {
+
+        public List<ro_ArchivosIess_Info> get_list(int IdEmpresa, int Anio, int Mes)
+        {
+            try
+            {
+                List<ro_ArchivosIess_Info> lista;
+
+                using (Entities_rrhh Contex=new Entities_rrhh())
+                {
+
+                   
+
+                }
+
+                return new List<ro_ArchivosIess_Info>();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
