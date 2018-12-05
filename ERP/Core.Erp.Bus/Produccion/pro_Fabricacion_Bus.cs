82320677251ba87034b1a7c4754b7a589e1db887
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-12-05 11:55:43 -0500

    /*

--- ERP/Core.Erp.Bus/Produccion/pro_Fabricacion_Bus.cs
+++ ERP/Core.Erp.Bus/Produccion/pro_Fabricacion_Bus.cs
@@ -11,11 +11,59 @@ namespace Core.Erp.Bus.Produccion
    public class pro_Fabricacion_Bus
     {
         pro_Fabricacion_Data odata = new pro_Fabricacion_Data();
-       public List<pro_Fabricacion_Info> GetList(int IdEmpresa, bool mostrar_anulados)
+        public List<pro_Fabricacion_Info> GetList(int IdEmpresa, bool mostrar_anulados)
         {
             try
             {
                 return odata.GetList(IdEmpresa, mostrar_anulados);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public pro_Fabricacion_Info GetInfo(int IdEmpresa, decimal IdFabricacion)
+        {
+            try
+            {
+                return odata.GetInfo(IdEmpresa, IdFabricacion);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool GuardarDB(pro_Fabricacion_Info info)
+        {
+            try
+            {
+                return odata.GuardarDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool ModificarDB(pro_Fabricacion_Info info)
+        {
+            try
+            {
+                return odata.ModificarDB(info);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool AnularDB(pro_Fabricacion_Info info)
+        {
+            try
+            {
+                return odata.AnularDB(info);
             }
             catch (Exception)
             {
