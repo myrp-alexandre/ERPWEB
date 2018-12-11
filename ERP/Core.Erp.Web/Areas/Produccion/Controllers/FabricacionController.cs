65fc5383716947abddf430f7dd97c011c731107a
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-12-10 23:03:34 -0500

    2

--- ERP/Core.Erp.Web/Areas/Produccion/Controllers/FabricacionController.cs
+++ ERP/Core.Erp.Web/Areas/Produccion/Controllers/FabricacionController.cs
@@ -152,6 +152,7 @@ namespace Core.Erp.Web.Areas.Produccion.Controllers
         public ActionResult Anular(pro_Fabricacion_Info model)
         {
             model.IdUsuarioAnulacion = Session["IdUsuario"].ToString();
+              model.LstDet = List_det.get_list(model.IdTransaccionSession);
             if (!bus_fabricacion.AnularDB(model))
             {
                 cargar_combos(model.IdEmpresa);
