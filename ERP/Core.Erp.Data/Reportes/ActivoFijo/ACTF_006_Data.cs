b0595be151ba978f08934efb65918dd09d126904
carloscede <carloscede@hotmail.es>
2018-12-12 09:20:32 -0800

    .....

---
+++ ERP/Core.Erp.Data/Reportes/ActivoFijo/ACTF_006_Data.cs
@@ -0,0 +1,26 @@
+﻿using Core.Erp.Info.Reportes.Inventario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Erp.Data.Reportes.Inventario
+{
+    public class ACTF_006_Data
+    {
+        public List<INV_013_Info> get_list(int IdEmpresa, decimal IdProducto)
+        {
+            try
+            {
+                List<INV_013_Info> Lista=new List<INV_013_Info>();
+
+               
+                return Lista;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
