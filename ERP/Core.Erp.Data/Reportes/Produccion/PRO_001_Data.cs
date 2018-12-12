63bc981262f7ceb96e1c99f488d164a23c5f23a5
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-12-12 14:32:20 -0500

    f

---
+++ ERP/Core.Erp.Data/Reportes/Produccion/PRO_001_Data.cs
@@ -0,0 +1,54 @@
+﻿using Core.Erp.Info.Reportes.Produccion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Data.Reportes.Produccion
+{
+   public class PRO_001_Data
+    {
+        public List<PRO_001_Info> GetList(int IdEmpresa, decimal IdFabricacion)
+        {
+            try
+            {
+                List<PRO_001_Info> Lista;
+                using (Entities_reportes Context = new Entities_reportes())
+                {
+                    Lista = Context.VWPRO_001.Where(q => q.IdEmpresa == IdEmpresa && q.IdFabricacion == IdFabricacion).Select(q => new PRO_001_Info
+                    {
+                        IdEmpresa = q.IdEmpresa,
+                        IdFabricacion = q.IdFabricacion,
+                        Cantidad = q.Cantidad,
+                        Costo = q.Costo,
+                        egr_IdNumMovi = q.egr_IdNumMovi,
+                        eg_bo_Descripcion = q.eg_bo_Descripcion,
+                        eg_NombreTipo = q.eg_NombreTipo,
+                        eg_Su_Descripcion = q.eg_Su_Descripcion,
+                        Estado = q.Estado,
+                        Fecha = q.Fecha,
+                        IdProducto = q.IdProducto,
+                        IdUnidadMedida = q.IdUnidadMedida,
+                        ing_IdNumMovi = q.ing_IdNumMovi,
+                        in_bo_Descripcion = q.in_bo_Descripcion,
+                        in_NombreTipo = q.in_NombreTipo,
+                        in_Su_Descripcion = q.in_Su_Descripcion,
+                        NombreUnidad = q.NombreUnidad,
+                        Observacion = q.Observacion,
+                        pr_descripcion = q.pr_descripcion,
+                        RealizaMovimiento = q.RealizaMovimiento,
+                        Secuencia = q.Secuencia,
+                        Signo = q.Signo
+                    }).ToList();
+                }
+                return Lista;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ ERP/Core.Erp.Info/Reportes/Produccion/PRO_001_Info.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Info.Reportes.Produccion
+{
+    public class PRO_001_Info
+    {
+        public int IdEmpresa { get; set; }
+        public decimal IdFabricacion { get; set; }
+        public int Secuencia { get; set; }
+        public System.DateTime Fecha { get; set; }
+        public string Observacion { get; set; }
+        public bool Estado { get; set; }
+        public string in_Su_Descripcion { get; set; }
+        public string in_bo_Descripcion { get; set; }
+        public string eg_Su_Descripcion { get; set; }
+        public string eg_bo_Descripcion { get; set; }
+        public string in_NombreTipo { get; set; }
+        public string eg_NombreTipo { get; set; }
+        public Nullable<decimal> egr_IdNumMovi { get; set; }
+        public Nullable<decimal> ing_IdNumMovi { get; set; }
+        public string Signo { get; set; }
+        public decimal IdProducto { get; set; }
+        public string IdUnidadMedida { get; set; }
+        public double Cantidad { get; set; }
+        public double Costo { get; set; }
+        public bool RealizaMovimiento { get; set; }
+        public string pr_descripcion { get; set; }
+        public string NombreUnidad { get; set; }
+    }
+}
