6f46d14a93b18d980bbed0b60100ef7707dc6bb3
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-12-12 16:11:59 -0500

    55

---
+++ ERP/Core.Erp.Web/Areas/Reportes/Controllers/ProduccionReportesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Core.Erp.Web.Areas.Reportes.Controllers
+{
+    public class ProduccionReportesController : Controller
+    {
+        // GET: Reportes/ProduccionReportes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ERP/Core.Erp.Web/Reportes/Produccion/PRO_001_Rpt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace Core.Erp.Web.Reportes.Produccion
+{
+    public partial class PRO_001_Rpt : DevExpress.XtraReports.UI.XtraReport
+    {
+        public PRO_001_Rpt()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
