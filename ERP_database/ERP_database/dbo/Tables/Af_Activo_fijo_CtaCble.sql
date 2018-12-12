c3f898da846ab1253b79c682bcd413ad3b95df26
Jorge Aroca <jaroca@fixed-sa.com>
2018-12-12 10:10:16 -0500

    .

--- ERP_database/ERP_database/dbo/Tables/Af_Activo_fijo_CtaCble.sql
+++ ERP_database/ERP_database/dbo/Tables/Af_Activo_fijo_CtaCble.sql
@@ -1,13 +1,15 @@
 ﻿CREATE TABLE [dbo].[Af_Activo_fijo_CtaCble] (
-    [IdEmpresa]    INT          NOT NULL,
-    [IdActivoFijo] INT          NOT NULL,
-    [Secuencia]    INT          NOT NULL,
-    [IdCatalogo]   VARCHAR (35) NOT NULL,
-    [IdCtaCble]    VARCHAR (20) NOT NULL,
-    [Porcentaje]   FLOAT (53)   NOT NULL,
+    [IdEmpresa]      INT          NOT NULL,
+    [IdActivoFijo]   INT          NOT NULL,
+    [Secuencia]      INT          NOT NULL,
+    [IdDepartamento] NUMERIC (18) NOT NULL,
+    [IdCtaCble]      VARCHAR (20) NOT NULL,
+    [Porcentaje]     FLOAT (53)   NOT NULL,
     CONSTRAINT [PK_Af_Activo_fijo_CtaCble] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdActivoFijo] ASC, [Secuencia] ASC),
     CONSTRAINT [FK_Af_Activo_fijo_CtaCble_Af_Activo_fijo] FOREIGN KEY ([IdEmpresa], [IdActivoFijo]) REFERENCES [dbo].[Af_Activo_fijo] ([IdEmpresa], [IdActivoFijo]),
-    CONSTRAINT [FK_Af_Activo_fijo_CtaCble_Af_Catalogo] FOREIGN KEY ([IdCatalogo]) REFERENCES [dbo].[Af_Catalogo] ([IdCatalogo]),
+    CONSTRAINT [FK_Af_Activo_fijo_CtaCble_Af_Departamento] FOREIGN KEY ([IdEmpresa], [IdDepartamento]) REFERENCES [dbo].[Af_Departamento] ([IdEmpresa], [IdDepartamento]),
     CONSTRAINT [FK_Af_Activo_fijo_CtaCble_ct_plancta] FOREIGN KEY ([IdEmpresa], [IdCtaCble]) REFERENCES [dbo].[ct_plancta] ([IdEmpresa], [IdCtaCble])
 );
+
+
 
---
+++ ERP_database/ERP_database/dbo/Tables/Af_Departamento.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Af_Departamento] (
+    [IdEmpresa]             INT           NOT NULL,
+    [IdDepartamento]        NUMERIC (18)  NOT NULL,
+    [Descripcion]           VARCHAR (MAX) NOT NULL,
+    [Estado]                BIT           NOT NULL,
+    [IdUsuarioCreacion]     VARCHAR (50)  NULL,
+    [FechaCreacion]         DATETIME      NULL,
+    [IdUsuarioModificacion] VARCHAR (50)  NULL,
+    [FechaModificacion]     DATETIME      NULL,
+    [IdUsuarioAnulacion]    VARCHAR (50)  NULL,
+    [FechaAnulacion]        DATETIME      NULL,
+    [MotivoAnulacion]       VARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Af_Departamento] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdDepartamento] ASC)
+);
+
