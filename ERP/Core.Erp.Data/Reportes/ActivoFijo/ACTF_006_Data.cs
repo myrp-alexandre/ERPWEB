af84246f828c3f85226b04836d71e026ba43e5e7
carloscede <carloscede@hotmail.es>
2018-12-12 09:24:40 -0800

    ...

--- ERP/Core.Erp.Data/Reportes/ActivoFijo/ACTF_006_Data.cs
+++ ERP/Core.Erp.Data/Reportes/ActivoFijo/ACTF_006_Data.cs
@@ -8,13 +8,19 @@ namespace Core.Erp.Data.Reportes.Inventario
 {
     public class ACTF_006_Data
     {
-        public List<ACTF_006_Info> get_list(int IdEmpresa, decimal IdProducto)
+        public List<ACTF_006_Info> get_list(int IdEmpresa, decimal IdActivoFijo)
         {
             try
             {
                 List<ACTF_006_Info> Lista=new List<ACTF_006_Info>();
+                using (Entities_activo_fijo Context = new Entities_activo_fijo())
+                {
+                    Lista = Context.Af_Activo_fijo.Where(q => q.IdEmpresa == IdEmpresa && q.IdActivoFijo == IdActivoFijo).Select(q => new ACTF_006_Info
+                    {
+                        
+                    }).ToList();
+                }
 
-               
                 return Lista;
             }
             catch (Exception)
