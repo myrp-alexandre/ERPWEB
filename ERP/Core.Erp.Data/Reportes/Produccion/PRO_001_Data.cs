e25e3920a9175bfbd28c4b523283bc6aec01b392
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-12-13 09:24:38 -0500

    5

--- ERP/Core.Erp.Data/Reportes/Produccion/PRO_001_Data.cs
+++ ERP/Core.Erp.Data/Reportes/Produccion/PRO_001_Data.cs
@@ -47,7 +47,7 @@ namespace Core.Erp.Data.Reportes.Produccion
                 }
                 return Lista;
             }
-            catch (Exception e)
+            catch (Exception)
             {
 
                 throw;
