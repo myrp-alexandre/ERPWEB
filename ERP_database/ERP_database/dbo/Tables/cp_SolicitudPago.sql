528ca43693fbca3a5934ab337687c8ecc5ba7161
Jorge Aroca <jaroca@fixed-sa.com>
2018-12-22 16:27:41 -0500

    .

---
+++ ERP_database/ERP_database/dbo/Tables/cp_SolicitudPago.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[cp_SolicitudPago] (
+    [IdEmpresa]             INT            NOT NULL,
+    [IdSolicitud]           NUMERIC (18)   NOT NULL,
+    [Fecha]                 DATETIME       NOT NULL,
+    [IdProveedor]           NUMERIC (18)   NOT NULL,
+    [Concepto]              VARCHAR (MAX)  NOT NULL,
+    [Estado]                BIT            NOT NULL,
+    [Valor]                 FLOAT (53)     NOT NULL,
+    [Solicitante]           VARCHAR (1000) NOT NULL,
+    [IdUsuarioCreacion]     VARCHAR (50)   NULL,
+    [FechaCreacion]         DATETIME       NULL,
+    [IdUsuarioModificacion] VARCHAR (50)   NULL,
+    [FechaModificacion]     DATETIME       NULL,
+    [IdUsuarioAnulacion]    VARCHAR (50)   NULL,
+    [FechaAnulacion]        DATETIME       NULL,
+    [MotivoAnulacion]       VARCHAR (MAX)  NULL,
+    CONSTRAINT [PK_cp_SolicitudPago] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdSolicitud] ASC),
+    CONSTRAINT [FK_cp_SolicitudPago_cp_proveedor] FOREIGN KEY ([IdEmpresa], [IdProveedor]) REFERENCES [dbo].[cp_proveedor] ([IdEmpresa], [IdProveedor])
+);
+
