42597258d32fe869527acf1c5528cefb747291fd
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-12-26 13:37:19 -0500

    i

--- ERP/Core.Erp.Web/Areas/RRHH/Controllers/HorarioPlanificacionController.cs
+++ ERP/Core.Erp.Web/Areas/RRHH/Controllers/HorarioPlanificacionController.cs
@@ -29,7 +29,7 @@ namespace Core.Erp.Web.Areas.RRHH.Controllers
         ro_area_Bus bus_area = new ro_area_Bus();
         ro_departamento_Bus bus_departamento = new ro_departamento_Bus();
         ro_cargo_Bus bus_cargo = new ro_cargo_Bus();
-        ro_horario_planificacion_Info info = new ro_horario_planificacion_Info();
+        ro_horario_planificacion_Info model = new ro_horario_planificacion_Info();
         int IdEmpresa = 0;
         #endregion
         #region Combo bajo demanda
@@ -64,12 +64,25 @@ namespace Core.Erp.Web.Areas.RRHH.Controllers
         [ValidateInput(false)]      
         public ActionResult Nuevo()
         {
+            #region Validar Session
+            if (string.IsNullOrEmpty(SessionFixed.IdTransaccionSession))
+                return RedirectToAction("Login", new { Area = "", Controller = "Account" });
+            SessionFixed.IdTransaccionSession = (Convert.ToDecimal(SessionFixed.IdTransaccionSession) + 1).ToString();
+            SessionFixed.IdTransaccionSessionActual = SessionFixed.IdTransaccionSession;
+            #endregion
             cargar_combos();
-            ro_horario_planificacion_Info info = new ro_horario_planificacion_Info();
-            info.FechaInicio = DateTime.Now;
-            info.FechaFin = DateTime.Now.AddDays(30);
-            info.lst_planificacion_det = new List<ro_horario_planificacion_det_Info>();
-            return View(info);
+            ro_horario_planificacion_Info model = new ro_horario_planificacion_Info
+            {
+
+                IdTransaccionSession = Convert.ToDecimal(SessionFixed.IdTransaccionSession),
+                FechaInicio = DateTime.Now,
+                FechaFin = DateTime.Now.AddDays(30),
+                lst_planificacion_det = new List<ro_horario_planificacion_det_Info>(),
+                IdHorario = 1
+
+            };
+            lst_planificacion_det.set_list(model.lst_planificacion_det, model.IdTransaccionSession);
+            return View(model);
         }
         [HttpPost]
         public ActionResult Nuevo(ro_horario_planificacion_Info model)
@@ -77,7 +90,7 @@ namespace Core.Erp.Web.Areas.RRHH.Controllers
             if (model == null)
                 return View(model);
             model.IdEmpresa=Convert.ToInt32( Session["IdEmpresa"].ToString());
-            model.lst_planificacion_det = lst_planificacion_det.get_list();
+            model.lst_planificacion_det = lst_planificacion_det.get_list(model.IdTransaccionSession);
             if (model.lst_planificacion_det == null || model.lst_planificacion_det.Count() == 0)
             {
                 ViewBag.mensaje = "No existe detalle para la planificacion";
@@ -90,16 +103,25 @@ namespace Core.Erp.Web.Areas.RRHH.Controllers
         }
         public ActionResult Modificar(int IdPlanificacion)
         {
+            #region Validar Session
+            if (string.IsNullOrEmpty(SessionFixed.IdTransaccionSession))
+                return RedirectToAction("Login", new { Area = "", Controller = "Account" });
+            SessionFixed.IdTransaccionSession = (Convert.ToDecimal(SessionFixed.IdTransaccionSession) + 1).ToString();
+            SessionFixed.IdTransaccionSessionActual = SessionFixed.IdTransaccionSession;
+            #endregion
             cargar_combos();
             IdEmpresa = Convert.ToInt32(Session["IdEmpresa"]);
-            info = bus_planificacion.get_info(IdEmpresa, IdPlanificacion);
-            lst_planificacion_det.set_list(info.lst_planificacion_det);
+            model = bus_planificacion.get_info(IdEmpresa, IdPlanificacion);
+
+            model.IdTransaccionSession = Convert.ToDecimal(SessionFixed.IdTransaccionSession);
+            model.lst_planificacion_det = bus_planificacion_det.get_list(IdEmpresa, IdPlanificacion);
+            lst_planificacion_det.set_list(model.lst_planificacion_det, model.IdTransaccionSession);
             return View(bus_planificacion.get_info(IdEmpresa, IdPlanificacion));
         }
         [HttpPost]
         public ActionResult Modificar(ro_horario_planificacion_Info model)
         {
-            model.lst_planificacion_det = lst_planificacion_det.get_list();
+            model.lst_planificacion_det = lst_planificacion_det.get_list(model.IdTransaccionSession);
             if (model.lst_planificacion_det == null || model.lst_planificacion_det.Count() == 0)
             {
                 ViewBag.mensaje = "No existe detalle para la planificacion";
@@ -118,16 +140,22 @@ namespace Core.Erp.Web.Areas.RRHH.Controllers
         }
         public ActionResult Anular(int IdPlanificacion)
         {
+            #region Validar Session
+            if (string.IsNullOrEmpty(SessionFixed.IdTransaccionSession))
+                return RedirectToAction("Login", new { Area = "", Controller = "Account" });
+            SessionFixed.IdTransaccionSession = (Convert.ToDecimal(SessionFixed.IdTransaccionSession) + 1).ToString();
+            SessionFixed.IdTransaccionSessionActual = SessionFixed.IdTransaccionSession;
+            #endregion
             cargar_combos();
             IdEmpresa = Convert.ToInt32(Session["IdEmpresa"]);
-            info = bus_planificacion.get_info(IdEmpresa, IdPlanificacion);
-            lst_planificacion_det.set_list(info.lst_planificacion_det);
+            model = bus_planificacion.get_info(IdEmpresa, IdPlanificacion);
+            lst_planificacion_det.set_list(model.lst_planificacion_det, model.IdTransaccionSession);
             return View(bus_planificacion.get_info(IdEmpresa, IdPlanificacion));
         }
         [HttpPost]
         public ActionResult Anular(ro_horario_planificacion_Info model)
         {
-            model.lst_planificacion_det = lst_planificacion_det.get_list();
+            model.lst_planificacion_det = lst_planificacion_det.get_list(model.IdTransaccionSession);
 
             model.IdEmpresa = Convert.ToInt32(Session["IdEmpresa"]);
             model.IdUsuario = Session["IdUsuario"].ToString();
@@ -140,16 +168,14 @@ namespace Core.Erp.Web.Areas.RRHH.Controllers
         }
 
         #endregion
+        #region Detalle
 
-        [ValidateInput(false)]
-        public ActionResult Consultar(DateTime fi, DateTime ff, int IdNomina=0, int IdSucursal=0, int IdDivision=0, int IdArea=0, int IdDepartamento=0, int IdCargo=0,decimal IdEmpleado = 0 ,int IdHorario=0)
+        public JsonResult Consultar(DateTime fi, DateTime ff, int IdNomina=0, int IdSucursal=0, int IdDivision=0, int IdArea=0, int IdDepartamento=0, int IdCargo=0,decimal IdEmpleado = 0 ,int IdHorario=0, decimal IdTransaccionSession = 0)
         {
-
-           
             IdEmpresa =Convert.ToInt32( Session["IdEmpresa"].ToString());
-            info = bus_planificacion.get_list(IdEmpresa, IdNomina,IdSucursal,IdDivision,IdArea,IdDepartamento,IdCargo, IdEmpleado, fi,ff,IdHorario);
-            lst_planificacion_det.set_list(info.lst_planificacion_det);
-             return Json("", JsonRequestBehavior.AllowGet);
+            var model = bus_planificacion.get_list(IdEmpresa, IdNomina,IdSucursal,IdDivision,IdArea,IdDepartamento,IdCargo, IdEmpleado, fi,ff,IdHorario);
+            lst_planificacion_det.set_list(model.lst_planificacion_det, IdTransaccionSession);
+            return Json(model, JsonRequestBehavior.AllowGet);
     }
 
 
@@ -167,9 +193,9 @@ namespace Core.Erp.Web.Areas.RRHH.Controllers
         public ActionResult EditingAddNew([ModelBinder(typeof(DevExpressEditorsBinder))] ro_horario_planificacion_det_Info info_det)
         {
             if (ModelState.IsValid)
-                lst_planificacion_det.AddRow(info_det);
+                lst_planificacion_det.AddRow(info_det, Convert.ToDecimal(SessionFixed.IdTransaccionSessionActual));
             ro_horario_planificacion_Info model = new ro_horario_planificacion_Info();
-            model.lst_planificacion_det = lst_planificacion_det.get_list();
+            model.lst_planificacion_det = lst_planificacion_det.get_list(Convert.ToDecimal(SessionFixed.IdTransaccionSessionActual));
             cargar_combos_detalle();
             return PartialView("_GridViewPartial_horario_planificacion_det", model);
         }
@@ -178,18 +204,18 @@ namespace Core.Erp.Web.Areas.RRHH.Controllers
         public ActionResult EditingUpdate([ModelBinder(typeof(DevExpressEditorsBinder))] ro_horario_planificacion_det_Info info_det)
         {
             if (ModelState.IsValid)
-                lst_planificacion_det.UpdateRow(info_det);
+                lst_planificacion_det.UpdateRow(info_det, Convert.ToDecimal(SessionFixed.IdTransaccionSessionActual));
             ro_horario_planificacion_Info model = new ro_horario_planificacion_Info();
-            model.lst_planificacion_det = lst_planificacion_det.get_list();
+            model.lst_planificacion_det = lst_planificacion_det.get_list(Convert.ToDecimal(SessionFixed.IdTransaccionSessionActual));
             cargar_combos_detalle();
             return PartialView("_GridViewPartial_horario_planificacion_det", model);
         }
 
-        public ActionResult EditingDelete([ModelBinder(typeof(DevExpressEditorsBinder))] ro_horario_planificacion_det_Info info_det)
+        public ActionResult EditingDelete(int Secuencia)
         {
-            lst_planificacion_det.DeleteRow(Convert.ToInt32( info_det.Secuencia));
+            lst_planificacion_det.DeleteRow(Secuencia, Convert.ToDecimal(SessionFixed.IdTransaccionSessionActual));
             ro_horario_planificacion_Info model = new ro_horario_planificacion_Info();
-            model.lst_planificacion_det = lst_planificacion_det.get_list();
+            model.lst_planificacion_det = lst_planificacion_det.get_list(Convert.ToDecimal(SessionFixed.IdTransaccionSessionActual));
             cargar_combos_detalle();
             return PartialView("_GridViewPartial_horario_planificacion_det", model);
         }
@@ -198,13 +224,13 @@ namespace Core.Erp.Web.Areas.RRHH.Controllers
         public ActionResult GridViewPartial_horario_planificacion_det()
         {
             cargar_combos_detalle();
-            info = new ro_horario_planificacion_Info();
-            info.IdEmpresa=Convert.ToInt32( Session["IdEmpresa"].ToString());
-            info.lst_planificacion_det = lst_planificacion_det.get_list();
-            return PartialView("_GridViewPartial_horario_planificacion_det", info);
+            SessionFixed.IdTransaccionSessionActual = Request.Params["TransaccionFixed"] != null ? Request.Params["TransaccionFixed"].ToString() : SessionFixed.IdTransaccionSessionActual;
+            ro_horario_planificacion_Info model = new ro_horario_planificacion_Info();
+            model.lst_planificacion_det = lst_planificacion_det.get_list(Convert.ToDecimal(SessionFixed.IdTransaccionSessionActual));
+            return PartialView("_GridViewPartial_horario_planificacion_det", model);
         }
-
-       
+        #endregion
+        #region Combos
         private void cargar_combos_detalle()
         {
             int IdEmpresa = Convert.ToInt32(Session["IdEmpresa"]);
@@ -222,42 +248,47 @@ namespace Core.Erp.Web.Areas.RRHH.Controllers
             ViewBag.lst_horario = bus_horario.get_list(IdEmpresa, false);
 
         }
+
+        #endregion
+
     }
 
     public class ro_horario_planificacion_det_lst
     {
-        public List<ro_horario_planificacion_det_Info> get_list()
+        string Variable = "ro_horario_planificacion_det_Info";
+
+        public List<ro_horario_planificacion_det_Info> get_list(decimal IdTransaccionSession)
         {
-            if (HttpContext.Current.Session["ro_horario_planificacion_det_Info"] == null)
+            if (HttpContext.Current.Session[Variable + IdTransaccionSession.ToString()] == null)
             {
                 List<ro_horario_planificacion_det_Info> list = new List<ro_horario_planificacion_det_Info>();
 
-                HttpContext.Current.Session["ro_horario_planificacion_det_Info"] = list;
+                HttpContext.Current.Session[Variable + IdTransaccionSession.ToString()] = list;
             }
-            return (List<ro_horario_planificacion_det_Info>)HttpContext.Current.Session["ro_horario_planificacion_det_Info"];
+            return (List<ro_horario_planificacion_det_Info>)HttpContext.Current.Session[Variable + IdTransaccionSession.ToString()];
         }
 
-        public void set_list(List<ro_horario_planificacion_det_Info> list)
+        public void set_list(List<ro_horario_planificacion_det_Info> list, decimal IdTransaccionSession)
         {
-            HttpContext.Current.Session["ro_horario_planificacion_det_Info"] = list;
+            HttpContext.Current.Session[Variable + IdTransaccionSession.ToString()] = list;
         }
 
-        public void AddRow(ro_horario_planificacion_det_Info info_det)
+        public void AddRow(ro_horario_planificacion_det_Info info_det, decimal IdTransaccionSession)
         {
-            List<ro_horario_planificacion_det_Info> list = get_list();
+            List<ro_horario_planificacion_det_Info> list = get_list(IdTransaccionSession);
             list.Add(info_det);
         }
 
-        public void UpdateRow(ro_horario_planificacion_det_Info info_det)
+        public void UpdateRow(ro_horario_planificacion_det_Info info_det, decimal IdTransaccionSession)
         {
 
-            ro_horario_planificacion_det_Info edited_info = get_list().Where(m => m.Secuencia == info_det.Secuencia).First();
+            ro_horario_planificacion_det_Info edited_info = get_list(IdTransaccionSession).Where(m => m.Secuencia == info_det.Secuencia).First();
             edited_info.IdHorario = info_det.IdHorario;
         }
 
-        public void DeleteRow( int Secuencia)
+        public void DeleteRow( int Secuencia, decimal IdTransaccionSession)
         {
-            List<ro_horario_planificacion_det_Info> list = get_list();
+            List<ro_horario_planificacion_det_Info> list = get_list(IdTransaccionSession);
             list.Remove(list.Where(m => m.Secuencia == Secuencia).First());
         }
     }
