9c58dce1690a53e97ffbc66436ff68ff9123065c
Jorge Aroca <jaroca@fixed-sa.com>
2018-12-25 21:42:04 -0500

    .

--- ERP_database/ERP_database/dbo/Tables/in_categorias.sql
+++ ERP_database/ERP_database/dbo/Tables/in_categorias.sql
@@ -18,9 +18,11 @@
     [MotivoAnulacion]  VARCHAR (50)  NULL,
     [cod_categoria]    VARCHAR (50)  NULL,
     CONSTRAINT [PK_in_categorias] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdCategoria] ASC),
-    CONSTRAINT [FK_in_categorias_ct_plancta] FOREIGN KEY ([IdEmpresa], [IdCtaCtble_Inve]) REFERENCES [dbo].[ct_plancta] ([IdEmpresa], [IdCtaCble]),
-    CONSTRAINT [FK_in_categorias_ct_plancta1] FOREIGN KEY ([IdEmpresa], [IdCtaCtble_Costo]) REFERENCES [dbo].[ct_plancta] ([IdEmpresa], [IdCtaCble]),
-    CONSTRAINT [FK_in_categorias_ct_plancta2] FOREIGN KEY ([IdEmpresa], [IdCtaCble_venta]) REFERENCES [dbo].[ct_plancta] ([IdEmpresa], [IdCtaCble]),
+    CONSTRAINT [FK_in_categorias_ct_plancta3] FOREIGN KEY ([IdEmpresa], [IdCtaCtble_Inve]) REFERENCES [dbo].[ct_plancta] ([IdEmpresa], [IdCtaCble]),
+    CONSTRAINT [FK_in_categorias_ct_plancta4] FOREIGN KEY ([IdEmpresa], [IdCtaCtble_Costo]) REFERENCES [dbo].[ct_plancta] ([IdEmpresa], [IdCtaCble]),
+    CONSTRAINT [FK_in_categorias_ct_plancta5] FOREIGN KEY ([IdEmpresa], [IdCtaCble_venta]) REFERENCES [dbo].[ct_plancta] ([IdEmpresa], [IdCtaCble]),
     CONSTRAINT [FK_in_categorias_tb_empresa] FOREIGN KEY ([IdEmpresa]) REFERENCES [dbo].[tb_empresa] ([IdEmpresa])
 );
+
+
 
