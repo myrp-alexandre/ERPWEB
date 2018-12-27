bb0a958e9174b50445d5be930e6d5525d0fbdf5d
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2018-12-27 13:39:18 -0500

    Update tb_parametro.sql

--- ERP_database/ERP_database/dbo/Tables/tb_parametro.sql
+++ ERP_database/ERP_database/dbo/Tables/tb_parametro.sql
@@ -2,6 +2,7 @@
     [IdEmpresa]      INT          NOT NULL,
     [IdCod_Impuesto] VARCHAR (25) NOT NULL,
     [Porcentaje]     FLOAT (53)   NOT NULL,
+    [EsMultiSucursal] BIT NOT NULL, 
     CONSTRAINT [PK_tb_parametro] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC),
     CONSTRAINT [FK_tb_parametro_tb_sis_Impuesto] FOREIGN KEY ([IdCod_Impuesto]) REFERENCES [dbo].[tb_sis_Impuesto] ([IdCod_Impuesto])
 );
