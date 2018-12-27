86affa47bb8db5ffeb85d4411f6ecad38c4d42c5
carloscede <carloscede@hotmail.es>
2018-12-27 13:44:55 -0800

    ....

--- ERP/Core.Erp.Data/RRHH/ro_SancionesPorMarcaciones_Data.cs
+++ ERP/Core.Erp.Data/RRHH/ro_SancionesPorMarcaciones_Data.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Erp.Info.RRHH;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,113 @@ namespace Core.Erp.Data.RRHH
 {
    public class ro_SancionesPorMarcaciones_Data
     {
+
+        public bool guardarDB(ro_SancionesPorMarcaciones_Info info)
+        {
+            try
+            {
+                using (Entities_rrhh Context=new Entities_rrhh())
+                {
+
+                    ro_SancionesPorMarcaciones entity = new ro_SancionesPorMarcaciones
+                    {
+                        IdEmpresa=info.IdEmpresa,
+                        IdAjuste=get_id(info.IdEmpresa),
+                        IdNomina_Tipo=info.IdNomina_Tipo,
+                        IdNomina_TipoLiqui=info.IdNomina_TipoLiqui,
+                        FechaInicio=info.FechaInicio, FecaFin=info.FecaFin,
+                        Observacion=info.Observacion,
+                        Fecha_Transac=DateTime.Now,
+                        Estado=true
+
+                    };
+                    Context.ro_SancionesPorMarcaciones.Add(entity);
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool modificarDB(ro_SancionesPorMarcaciones_Info info)
+        {
+            try
+            {
+                using (Entities_rrhh Context = new Entities_rrhh())
+                {
+
+                    ro_SancionesPorMarcaciones entity = Context.ro_SancionesPorMarcaciones.Where(v => v.IdEmpresa == info.IdEmpresa && v.IdAjuste == info.IdAjuste).FirstOrDefault();
+                    if (entity == null)
+                        return false;
+                    entity.FechaInicio = info.FechaInicio;
+                    entity.FecaFin = info.FecaFin;
+                    entity.Observacion = info.Observacion;
+                    entity.IdUsuarioUltMod = info.IdUsuarioUltMod;
+                    entity.Fecha_UltMod=DateTime.Now
+                    Context.SaveChanges();
+               
+                        }
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool anularDB(ro_SancionesPorMarcaciones_Info info)
+        {
+            try
+            {
+                using (Entities_rrhh Context = new Entities_rrhh())
+                {
+
+                    ro_SancionesPorMarcaciones entity = Context.ro_SancionesPorMarcaciones.Where(v => v.IdEmpresa == info.IdEmpresa && v.IdAjuste == info.IdAjuste).FirstOrDefault();
+                    if (entity == null)
+                        return false;
+                    entity.Estado =false;
+                    entity.Fecha_UltAnu = DateTime.Now;
+                    Context.SaveChanges();
+
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public int get_id(int IdEmpresa)
+        {
+            try
+            {
+                using (Entities_rrhh Context=new Entities_rrhh())
+                {
+                    var select = (from q in Context.ro_SancionesPorMarcaciones
+                                  where q.IdEmpresa == IdEmpresa
+                                  select q
+                                );
+                    if (select.Count() == 0)
+                        return 1;
+                    else
+                        return select.Count() + 1;
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
--- ERP/Core.Erp.Data/RRHH/ro_SancionesPorMarcaciones_det_Data.cs
+++ ERP/Core.Erp.Data/RRHH/ro_SancionesPorMarcaciones_det_Data.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Erp.Info.RRHH;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,47 @@ namespace Core.Erp.Data.RRHH
 {
    public class ro_SancionesPorMarcaciones_det_Data
     {
+       public List<ro_SancionesPorMarcaciones_det_Info> get_list(int IdEmpresa, int IdAjuste)
+        {
+            try
+            {
+                List<ro_SancionesPorMarcaciones_det_Info> lista;
+                using (Entities_rrhh context=new Entities_rrhh())
+                {
+                    lista = (from q in context.vwro_SancionesPorMarcaciones_det
+
+                             where q.IdEmpleado == IdEmpresa
+                             && q.IdAjuste == IdAjuste
+                             select new ro_SancionesPorMarcaciones_det_Info
+                             {
+                                IdEmpresa=q.IdEmpresa,
+                                IdAjuste=q.IdAjuste,
+                                Secuencia=q.Secuencia,
+                                IdCalendario=q.IdCalendario,
+                                IdEmpleado=q.IdEmpleado,
+                                IdSucursal=q.IdSucursal,
+                                IdTipoMarcaciones=q.IdTipoMarcaciones,
+                                EsHoraHorario=q.EsHoraHorario,
+                                EsHoraMarcacion=q.EsHoraMarcacion,
+                                es_fechaRegistro=q.es_fechaRegistro,
+                                pe_cedulaRuc=q.pe_cedulaRuc,
+                                pe_apellido=q.pe_apellido,
+                                pe_nombre=q.pe_nombre,
+                                pe_nombreCompleto=q.pe_nombreCompleto,
+                                em_codigo=q.em_codigo,
+                                IdRegistro=q.IdRegistro,
+                                Minutos=q.Minutos,
+                                Observacion=q.Observacion
+                             }).ToList();
+                }
+
+                return lista;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
