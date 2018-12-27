f07558c3daeba71c4c823f7e3d0c4b5d8e667043
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-12-27 14:18:16 -0500

    m

--- ERP/Core.Erp.Info/General/tb_parametro_Info.cs
+++ ERP/Core.Erp.Info/General/tb_parametro_Info.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace Core.Erp.Info.General
     public class tb_parametro_Info
     {
         public int IdEmpresa { get; set; }
+        [Required(ErrorMessage = ("el campo impuesto es obligatorio"))]
         public string IdCod_Impuesto { get; set; }
+        [Required(ErrorMessage = ("el campo porcentaje es obligatorio"))]
         public double Porcentaje { get; set; }
         public bool EsMultiSucursal { get; set; }
     }
--- ERP/Core.Erp.Web/Areas/General/Controllers/ParametrosGeneralController.cs
+++ ERP/Core.Erp.Web/Areas/General/Controllers/ParametrosGeneralController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Core.Erp.Bus.General;
+using Core.Erp.Info.General;
+using Core.Erp.Web.Helps;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,40 @@ namespace Core.Erp.Web.Areas.General.Controllers
 {
     public class ParametrosGeneralController : Controller
     {
-        // GET: General/ParametrosGeneral
+        #region Variables
+        tb_parametro_Bus bus_parametro = new tb_parametro_Bus();
+        tb_sis_Impuesto_Bus bus_imp = new tb_sis_Impuesto_Bus();
+        
+        #endregion
+
+        #region Index
         public ActionResult Index()
         {
-            return View();
+            int IdEmpresa = Convert.ToInt32(SessionFixed.IdEmpresa);
+            tb_parametro_Info model = bus_parametro.GetInfo(IdEmpresa);
+            if (model == null)
+                model = new tb_parametro_Info();
+            cargar_combos();
+            return View(model);
         }
+        [HttpPost]
+        public ActionResult Index(tb_parametro_Info model)
+        {
+            if (!bus_parametro.GuardarDB(model))
+                ViewBag.mensaje = "No se pudieron actualizar los registros";
+            cargar_combos();
+            return View(model);
+        }
+
+        #endregion
+        #region Metodos
+        private void cargar_combos(string IdTipoImpuesto ="")
+        {
+            var lst_imp = bus_imp.get_list(IdTipoImpuesto, false);
+            ViewBag.lst_imp = lst_imp;
+        }
+
+        #endregion
+
     }
 }
