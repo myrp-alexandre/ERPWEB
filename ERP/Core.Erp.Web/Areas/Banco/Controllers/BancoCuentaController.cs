a04260dc8c0c426541267f3cc4330338e9c66489
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-12-27 10:09:10 -0500

    55

--- ERP/Core.Erp.Web/Areas/Banco/Controllers/BancoCuentaController.cs
+++ ERP/Core.Erp.Web/Areas/Banco/Controllers/BancoCuentaController.cs
@@ -3,9 +3,14 @@ using Core.Erp.Bus.Contabilidad;
 using Core.Erp.Bus.General;
 using Core.Erp.Info.Banco;
 using Core.Erp.Web.Helps;
+using DevExpress.Web.Mvc;
+using ExcelDataReader;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Web;
 using System.Web.Mvc;
+using static Core.Erp.Info.General.tb_sis_log_error_InfoList;
 
 namespace Core.Erp.Web.Areas.Banco.Controllers
 {
@@ -16,6 +21,9 @@ namespace Core.Erp.Web.Areas.Banco.Controllers
         ba_Banco_Cuenta_Bus bus_cuenta = new ba_Banco_Cuenta_Bus();
         tb_banco_Bus bus_banco = new tb_banco_Bus();
         ct_plancta_Bus bus_cuentacontable = new ct_plancta_Bus();
+        ba_Banco_Cuenta_List ListaBanco = new ba_Banco_Cuenta_List();
+        tb_sis_log_error_List SisLogError = new tb_sis_log_error_List();
+
         #endregion
 
         #region Index
@@ -116,5 +124,147 @@ namespace Core.Erp.Web.Areas.Banco.Controllers
             return RedirectToAction("Index");
         }
         #endregion
+
+        #region Importacion
+        public ActionResult UploadControlUpload()
+        {
+            UploadControlExtension.GetUploadedFiles("UploadControlFile", UploadControlSettings.UploadValidationSettings, UploadControlSettings.FileUploadComplete);
+            return null;
+        }
+        public ActionResult Importar(int IdEmpresa = 0)
+        {
+            #region Validar Session
+            if (string.IsNullOrEmpty(SessionFixed.IdTransaccionSession))
+                return RedirectToAction("Login", new { Area = "", Controller = "Account" });
+            SessionFixed.IdTransaccionSession = (Convert.ToDecimal(SessionFixed.IdTransaccionSession) + 1).ToString();
+            SessionFixed.IdTransaccionSessionActual = SessionFixed.IdTransaccionSession;
+            #endregion
+
+            ba_Banco_Cuenta_Info model = new ba_Banco_Cuenta_Info
+            {
+                IdEmpresa = IdEmpresa,
+                IdTransaccionSession = Convert.ToDecimal(SessionFixed.IdTransaccionSessionActual)
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Importar(ba_Banco_Cuenta_Info model)
+        {
+            try
+            {
+                var Lista_Banco = ListaBanco.get_list(model.IdTransaccionSession);
+                
+                if (!bus_cuenta.GuardarDbImportacion(Lista_Banco))
+                {
+                    ViewBag.mensaje = "Error al importar el archivo";
+                    return View(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                SisLogError.set_list((ex.InnerException) == null ? ex.Message.ToString() : ex.InnerException.ToString());
+
+                ViewBag.error = ex.Message.ToString();
+                return View(model);
+            }
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult GridViewPartial_Banco_importacion()
+        {
+            SessionFixed.IdTransaccionSessionActual = Request.Params["TransaccionFixed"] != null ? Request.Params["TransaccionFixed"].ToString() : SessionFixed.IdTransaccionSessionActual;
+            var model = ListaBanco.get_list(Convert.ToDecimal(SessionFixed.IdTransaccionSessionActual));
+            return PartialView("_GridViewPartial_Banco_importacion", model);
+        }
+        
+        public JsonResult ActualizarVariablesSession(int IdEmpresa = 0, decimal IdTransaccionSession = 0)
+        {
+            string retorno = string.Empty;
+            SessionFixed.IdEmpresa = IdEmpresa.ToString();
+            SessionFixed.IdTransaccionSession = IdTransaccionSession.ToString();
+            SessionFixed.IdTransaccionSessionActual = IdTransaccionSession.ToString();
+            return Json(retorno, JsonRequestBehavior.AllowGet);
+        }
+        #endregion
+
     }
+    public class UploadControlSettings
+    {
+        public static DevExpress.Web.UploadControlValidationSettings UploadValidationSettings = new DevExpress.Web.UploadControlValidationSettings()
+        {
+            AllowedFileExtensions = new string[] { ".xlsx" },
+            MaxFileSize = 40000000
+        };
+        public static void FileUploadComplete(object sender, DevExpress.Web.FileUploadCompleteEventArgs e)
+        {
+            #region Variables
+         
+            ba_Banco_Cuenta_List ListaBanco = new ba_Banco_Cuenta_List();
+            List<ba_Banco_Cuenta_Info> Lista_Banco = new List<ba_Banco_Cuenta_Info>();
+
+
+            int cont = 0;
+            decimal IdTransaccionSession = Convert.ToDecimal(SessionFixed.IdTransaccionSessionActual);
+            int IdEmpresa = Convert.ToInt32(SessionFixed.IdEmpresa);
+            #endregion
+
+
+            Stream stream = new MemoryStream(e.UploadedFile.FileBytes);
+            if (stream.Length > 0)
+            {
+                IExcelDataReader reader = null;
+                reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+
+                #region Banco                
+                while (reader.Read())
+                {
+                    if (!reader.IsDBNull(0) && cont > 0)
+                    {
+                        ba_Banco_Cuenta_Info info = new ba_Banco_Cuenta_Info
+                        {
+                            IdEmpresa = IdEmpresa,
+                            IdBanco_Financiero = Convert.ToInt32(reader.GetValue(0)),
+                            IdBanco = Convert.ToInt32(reader.GetValue(1)),
+                            ba_Tipo = Convert.ToString(reader.GetValue(2)),
+                            ba_Num_Cuenta = Convert.ToString(reader.GetValue(3)),
+                            ba_num_digito_cheq = Convert.ToInt32(reader.GetValue(4)),
+                            IdCtaCble = Convert.ToString(reader.GetValue(6)),
+                            IdUsuario = SessionFixed.IdUsuario
+                        };
+                        Lista_Banco.Add(info);
+                    }
+                    else
+                        cont++;
+                }
+                ListaBanco.set_list(Lista_Banco, IdTransaccionSession);
+                #endregion
+
+                cont = 0;
+                //Para avanzar a la siguiente hoja de excel
+                reader.NextResult();
+               
+            }
+        }
+    }
+
+    public class ba_Banco_Cuenta_List
+    {
+        string Variable = "ba_Banco_Cuenta_Info";
+        public List<ba_Banco_Cuenta_Info> get_list(decimal IdTransaccionSession)
+        {
+            if (HttpContext.Current.Session[Variable + IdTransaccionSession.ToString()] == null)
+            {
+                List<ba_Banco_Cuenta_Info> list = new List<ba_Banco_Cuenta_Info>();
+
+                HttpContext.Current.Session[Variable + IdTransaccionSession.ToString()] = list;
+            }
+            return (List<ba_Banco_Cuenta_Info>)HttpContext.Current.Session[Variable + IdTransaccionSession.ToString()];
+        }
+
+        public void set_list(List<ba_Banco_Cuenta_Info> list, decimal IdTransaccionSession)
+        {
+            HttpContext.Current.Session[Variable + IdTransaccionSession.ToString()] = list;
+        }
+    }
+
 }
