32338baa2cb174c8d565a9d18911ade29b862e52
carloscede <carloscede@hotmail.es>
2018-12-27 11:12:33 -0800

    .....

---
+++ ERP_database/ERP_database/dbo/Tables/ro_SancionesPorMarcaciones.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[ro_SancionesPorMarcaciones] (
+    [IdEmpresa]          INT           NOT NULL,
+    [IdAjuste]           INT           NOT NULL,
+    [IdNomina_Tipo]      INT           NOT NULL,
+    [IdNomina_TipoLiqui] INT           NOT NULL,
+    [FechaInicio]        DATE          NOT NULL,
+    [FecaFin]            DATE          NOT NULL,
+    [Observacion]        VARCHAR (MAX) NULL,
+    [Estado]             BIT           NOT NULL,
+    [IdUsuario]          BIT           NULL,
+    [Fecha_Transac]      DATETIME      NULL,
+    [IdUsuarioUltMod]    VARCHAR (20)  NULL,
+    [Fecha_UltMod]       DATETIME      NULL,
+    [IdUsuarioUltAnu]    VARCHAR (20)  NULL,
+    [Fecha_UltAnu]       DATETIME      NULL,
+    [MotiAnula]          VARCHAR (200) NULL,
+    CONSTRAINT [PK_ro_SancionesPorMarcaciones] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdAjuste] ASC),
+    CONSTRAINT [FK_ro_SancionesPorMarcaciones_ro_Nomina_Tipoliqui] FOREIGN KEY ([IdEmpresa], [IdNomina_Tipo], [IdNomina_TipoLiqui]) REFERENCES [dbo].[ro_Nomina_Tipoliqui] ([IdEmpresa], [IdNomina_Tipo], [IdNomina_TipoLiqui])
+);
+
