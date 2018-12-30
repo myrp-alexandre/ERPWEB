596aa2d01805f52ce6e3dc8a42f29b3f75dcd524
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2018-12-30 11:33:52 -0500

    L

--- ERP_database/ERP_database/web/Views/VWFAC_007.sql
+++ ERP_database/ERP_database/web/Views/VWFAC_007.sql
@@ -1,16 +1,15 @@
 ﻿CREATE VIEW [web].[VWFAC_007]
 AS
-SELECT fa_factura_det.IdEmpresa, fa_factura_det.IdSucursal, fa_factura_det.IdBodega, fa_factura_det.IdCbteVta, fa_factura_det.Secuencia, fa_factura_det.IdProducto, in_Producto.pr_descripcion, in_presentacion.nom_presentacion, 
-                  in_Producto.lote_num_lote, in_Producto.lote_fecha_vcto, fa_factura_det.vt_cantidad, fa_factura_det.vt_Precio, fa_factura_det.vt_PorDescUnitario, fa_factura_det.vt_Subtotal, 
-                  fa_factura_det.vt_DescUnitario * fa_factura_det.vt_cantidad AS DescTotal, IIF(vt_por_iva > 0, vt_Subtotal, 0) AS vt_SubtotalIVA, IIF(vt_por_iva = 0, vt_Subtotal, 0) AS vt_Subtotal0, fa_factura_det.vt_iva, fa_factura_det.vt_total, 
-                  fa_factura_det.vt_por_iva, fa_cliente_contactos.Nombres, tb_sucursal.Su_Descripcion, fa_factura.vt_serie1 + '-' + fa_factura.vt_serie2 + '-' + fa_factura.vt_NumFactura vt_NumFactura, fa_factura.vt_fecha, fa_factura.vt_fech_venc, 
-                  fa_Vendedor.Ve_Vendedor, fa_TerminoPago.nom_TerminoPago, fa_TerminoPago.Num_Coutas, fa_factura.vt_Observacion
-FROM     fa_factura_det INNER JOIN
-                  in_Producto ON fa_factura_det.IdEmpresa = in_Producto.IdEmpresa AND fa_factura_det.IdProducto = in_Producto.IdProducto INNER JOIN
-                  in_presentacion ON in_Producto.IdEmpresa = in_presentacion.IdEmpresa AND in_Producto.IdPresentacion = in_presentacion.IdPresentacion INNER JOIN
-                  fa_factura ON fa_factura_det.IdEmpresa = fa_factura.IdEmpresa AND fa_factura_det.IdSucursal = fa_factura.IdSucursal AND fa_factura_det.IdBodega = fa_factura.IdBodega AND 
-                  fa_factura_det.IdCbteVta = fa_factura.IdCbteVta INNER JOIN
-                  fa_cliente_contactos ON fa_factura.IdEmpresa = fa_cliente_contactos.IdEmpresa AND fa_factura.IdCliente = fa_cliente_contactos.IdCliente AND fa_factura.IdContacto = fa_cliente_contactos.IdContacto INNER JOIN
-                  fa_Vendedor ON fa_factura.IdEmpresa = fa_Vendedor.IdEmpresa AND fa_factura.IdVendedor = fa_Vendedor.IdVendedor INNER JOIN
-                  tb_sucursal ON fa_factura.IdEmpresa = tb_sucursal.IdEmpresa AND fa_factura.IdSucursal = tb_sucursal.IdSucursal INNER JOIN
-                  fa_TerminoPago ON fa_factura.vt_tipo_venta = fa_TerminoPago.IdTerminoPago
+SELECT d.IdEmpresa, d.IdSucursal, d.IdBodega, d.IdCbteVta, d.Secuencia, d.IdProducto, pro.pr_descripcion, d.vt_cantidad, d.vt_Precio, d.vt_cantidad * d.vt_Precio AS SubtotalSinDscto, d.vt_cantidad * d.vt_DescUnitario AS DescuentoTotal, 
+                  d.vt_Subtotal AS SubtotalConDscto, d.vt_iva, d.vt_Subtotal + d.vt_iva AS vt_Total, d.vt_por_iva, CASE WHEN d .vt_por_iva > 0 THEN vt_cantidad * vt_Precio ELSE 0 END AS SubtotalIVA, 
+                  CASE WHEN d .vt_por_iva = 0 THEN vt_cantidad * vt_Precio ELSE 0 END AS SubtotalSinIVA, c.vt_fecha, c.vt_serie1 + '-' + c.vt_serie2 + '-' + c.vt_NumFactura AS vt_NumFactura, per.pe_nombreCompleto AS cli_Nombre, 
+                  per.pe_cedulaRuc AS cli_cedulaRuc, con.Direccion AS cli_direccion, con.Telefono AS cli_Telefonos, con.Correo AS cli_correo, su.Su_Descripcion, su.Su_Telefonos, su.Su_Direccion, cat.Nombre AS FormaDePago, IdCatalogo_FormaPago, c.vt_ValorEfectivo, 
+                  c.vt_Cambio, c.vt_autorizacion, c.Fecha_Autorizacion
+FROM     dbo.fa_cliente_contactos AS con INNER JOIN
+                  dbo.fa_factura AS c ON con.IdEmpresa = c.IdEmpresa AND con.IdCliente = c.IdCliente AND con.IdContacto = c.IdContacto INNER JOIN
+                  dbo.fa_factura_det AS d ON c.IdEmpresa = d.IdEmpresa AND c.IdSucursal = d.IdSucursal AND c.IdBodega = d.IdBodega AND c.IdCbteVta = d.IdCbteVta INNER JOIN
+                  dbo.in_Producto AS pro ON d.IdEmpresa = pro.IdEmpresa AND d.IdProducto = pro.IdProducto INNER JOIN
+                  dbo.fa_cliente AS cli ON con.IdEmpresa = cli.IdEmpresa AND con.IdCliente = cli.IdCliente INNER JOIN
+                  dbo.tb_persona AS per ON cli.IdPersona = per.IdPersona INNER JOIN
+                  dbo.tb_sucursal AS su ON c.IdEmpresa = su.IdEmpresa AND c.IdSucursal = su.IdSucursal LEFT OUTER JOIN
+                  dbo.fa_catalogo AS cat ON c.IdCatalogo_FormaPago = cat.IdCatalogo
