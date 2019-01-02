26652436111e7f3e3f0054aa182b5f520c998d76
WendyCherres <wendycherres@gmail.com>
2019-01-02 14:51:08 -0500

    ...

---
+++ ERP/Core.Erp.Web/Reportes/Facturacion/FAC_010_resumen_forma_pago_Rpt.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Core.Erp.Web.Reportes.Facturacion
+{
+    partial class FAC_010_resumen_forma_pago_Rpt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Detail = new DevExpress.XtraReports.UI.DetailBand();
+            this.TopMargin = new DevExpress.XtraReports.UI.TopMarginBand();
+            this.BottomMargin = new DevExpress.XtraReports.UI.BottomMarginBand();
+            this.p_IdEmpresa_rfp = new DevExpress.XtraReports.Parameters.Parameter();
+            this.IdSucursal_rfp = new DevExpress.XtraReports.Parameters.Parameter();
+            this.p_fecha_ini_rfp = new DevExpress.XtraReports.Parameters.Parameter();
+            this.p_fecha_fin_rfp = new DevExpress.XtraReports.Parameters.Parameter();
+            this.objectDataSource1 = new DevExpress.DataAccess.ObjectBinding.ObjectDataSource(this.components);
+            this.xrTable1 = new DevExpress.XtraReports.UI.XRTable();
+            this.xrTableRow1 = new DevExpress.XtraReports.UI.XRTableRow();
+            this.xrTableCell2 = new DevExpress.XtraReports.UI.XRTableCell();
+            this.xrTableCell3 = new DevExpress.XtraReports.UI.XRTableCell();
+            this.ReportHeader = new DevExpress.XtraReports.UI.ReportHeaderBand();
+            ((System.ComponentModel.ISupportInitialize)(this.objectDataSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xrTable1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // Detail
+            // 
+            this.Detail.HeightF = 100F;
+            this.Detail.Name = "Detail";
+            this.Detail.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // TopMargin
+            // 
+            this.TopMargin.HeightF = 0F;
+            this.TopMargin.Name = "TopMargin";
+            this.TopMargin.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.TopMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // BottomMargin
+            // 
+            this.BottomMargin.HeightF = 0F;
+            this.BottomMargin.Name = "BottomMargin";
+            this.BottomMargin.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.BottomMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // p_IdEmpresa_rfp
+            // 
+            this.p_IdEmpresa_rfp.Name = "p_IdEmpresa_rfp";
+            this.p_IdEmpresa_rfp.Type = typeof(short);
+            this.p_IdEmpresa_rfp.ValueInfo = "0";
+            this.p_IdEmpresa_rfp.Visible = false;
+            // 
+            // IdSucursal_rfp
+            // 
+            this.IdSucursal_rfp.Name = "IdSucursal_rfp";
+            this.IdSucursal_rfp.Type = typeof(short);
+            this.IdSucursal_rfp.ValueInfo = "0";
+            this.IdSucursal_rfp.Visible = false;
+            // 
+            // p_fecha_ini_rfp
+            // 
+            this.p_fecha_ini_rfp.Name = "p_fecha_ini_rfp";
+            this.p_fecha_ini_rfp.Type = typeof(System.DateTime);
+            this.p_fecha_ini_rfp.Visible = false;
+            // 
+            // p_fecha_fin_rfp
+            // 
+            this.p_fecha_fin_rfp.Name = "p_fecha_fin_rfp";
+            this.p_fecha_fin_rfp.Visible = false;
+            // 
+            // objectDataSource1
+            // 
+            this.objectDataSource1.DataSource = typeof(Core.Erp.Info.Reportes.Facturacion.FAC_010_Info);
+            this.objectDataSource1.Name = "objectDataSource1";
+            // 
+            // xrTable1
+            // 
+            this.xrTable1.Font = new System.Drawing.Font("Verdana", 8F, System.Drawing.FontStyle.Bold);
+            this.xrTable1.LocationFloat = new DevExpress.Utils.PointFloat(0F, 0F);
+            this.xrTable1.Name = "xrTable1";
+            this.xrTable1.Rows.AddRange(new DevExpress.XtraReports.UI.XRTableRow[] {
+            this.xrTableRow1});
+            this.xrTable1.SizeF = new System.Drawing.SizeF(250F, 25F);
+            this.xrTable1.StylePriority.UseFont = false;
+            this.xrTable1.StylePriority.UseTextAlignment = false;
+            this.xrTable1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft;
+            // 
+            // xrTableRow1
+            // 
+            this.xrTableRow1.Cells.AddRange(new DevExpress.XtraReports.UI.XRTableCell[] {
+            this.xrTableCell2,
+            this.xrTableCell3});
+            this.xrTableRow1.Name = "xrTableRow1";
+            this.xrTableRow1.Weight = 1D;
+            // 
+            // xrTableCell2
+            // 
+            this.xrTableCell2.Name = "xrTableCell2";
+            this.xrTableCell2.Padding = new DevExpress.XtraPrinting.PaddingInfo(5, 0, 0, 0, 100F);
+            this.xrTableCell2.StylePriority.UsePadding = false;
+            this.xrTableCell2.Text = "Forma de Pago";
+            this.xrTableCell2.Weight = 1.5D;
+            // 
+            // xrTableCell3
+            // 
+            this.xrTableCell3.Name = "xrTableCell3";
+            this.xrTableCell3.Padding = new DevExpress.XtraPrinting.PaddingInfo(5, 0, 0, 0, 100F);
+            this.xrTableCell3.StylePriority.UsePadding = false;
+            this.xrTableCell3.StylePriority.UseTextAlignment = false;
+            this.xrTableCell3.Text = "Total";
+            this.xrTableCell3.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter;
+            this.xrTableCell3.Weight = 0.99999999999999978D;
+            // 
+            // ReportHeader
+            // 
+            this.ReportHeader.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
+            this.xrTable1});
+            this.ReportHeader.HeightF = 25F;
+            this.ReportHeader.Name = "ReportHeader";
+            // 
+            // FAC_010_resumen_forma_pago_Rpt
+            // 
+            this.Bands.AddRange(new DevExpress.XtraReports.UI.Band[] {
+            this.Detail,
+            this.TopMargin,
+            this.BottomMargin,
+            this.ReportHeader});
+            this.ComponentStorage.AddRange(new System.ComponentModel.IComponent[] {
+            this.objectDataSource1});
+            this.DataSource = this.objectDataSource1;
+            this.Margins = new System.Drawing.Printing.Margins(0, 0, 0, 0);
+            this.Parameters.AddRange(new DevExpress.XtraReports.Parameters.Parameter[] {
+            this.p_IdEmpresa_rfp,
+            this.IdSucursal_rfp,
+            this.p_fecha_ini_rfp,
+            this.p_fecha_fin_rfp});
+            this.Version = "17.2";
+            this.BeforePrint += new System.Drawing.Printing.PrintEventHandler(this.FAC_010_resumen_forma_pago_Rpt_BeforePrint);
+            ((System.ComponentModel.ISupportInitialize)(this.objectDataSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xrTable1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraReports.UI.DetailBand Detail;
+        private DevExpress.XtraReports.UI.TopMarginBand TopMargin;
+        private DevExpress.XtraReports.UI.BottomMarginBand BottomMargin;
+        public DevExpress.XtraReports.Parameters.Parameter p_IdEmpresa_rfp;
+        public DevExpress.XtraReports.Parameters.Parameter IdSucursal_rfp;
+        public DevExpress.XtraReports.Parameters.Parameter p_fecha_ini_rfp;
+        public DevExpress.XtraReports.Parameters.Parameter p_fecha_fin_rfp;
+        private DevExpress.DataAccess.ObjectBinding.ObjectDataSource objectDataSource1;
+        private DevExpress.XtraReports.UI.XRTable xrTable1;
+        private DevExpress.XtraReports.UI.XRTableRow xrTableRow1;
+        private DevExpress.XtraReports.UI.XRTableCell xrTableCell2;
+        private DevExpress.XtraReports.UI.XRTableCell xrTableCell3;
+        private DevExpress.XtraReports.UI.ReportHeaderBand ReportHeader;
+    }
+}
---
+++ ERP/Core.Erp.Web/Reportes/Facturacion/FAC_010_resumen_forma_pago_Rpt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+using Core.Erp.Info.Reportes.Facturacion;
+using System.Collections.Generic;
+using Core.Erp.Bus.Reportes.Facturacion;
+
+namespace Core.Erp.Web.Reportes.Facturacion
+{
+    public partial class FAC_010_resumen_forma_pago_Rpt : DevExpress.XtraReports.UI.XtraReport
+    {
+        public FAC_010_resumen_forma_pago_Rpt()
+        {
+            InitializeComponent();
+        }      
+
+        private void FAC_010_resumen_forma_pago_Rpt_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+           
+        }
+    }
+}
