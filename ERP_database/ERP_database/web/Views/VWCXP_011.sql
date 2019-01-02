ccf619c599d64970e8a5bca89be6cea47741ae99
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2019-01-02 10:49:43 -0500

    121

--- ERP_database/ERP_database/web/Views/VWCXP_011.sql
+++ ERP_database/ERP_database/web/Views/VWCXP_011.sql
@@ -1,8 +1,178 @@
 ﻿CREATE VIEW web.VWCXP_011
 AS
-SELECT cp_SolicitudPago.IdEmpresa, cp_SolicitudPago.IdSolicitud, cp_SolicitudPago.IdSucursal, cp_SolicitudPago.Fecha, cp_SolicitudPago.IdProveedor, cp_SolicitudPago.Concepto, cp_SolicitudPago.Estado, cp_SolicitudPago.Valor, 
-                  cp_SolicitudPago.Solicitante, tb_persona.pe_nombreCompleto, tb_persona.pe_cedulaRuc, tb_sucursal.Su_Descripcion
-FROM     cp_SolicitudPago INNER JOIN
-                  cp_proveedor ON cp_SolicitudPago.IdEmpresa = cp_proveedor.IdEmpresa AND cp_SolicitudPago.IdProveedor = cp_proveedor.IdProveedor INNER JOIN
-                  tb_persona ON cp_proveedor.IdPersona = tb_persona.IdPersona INNER JOIN
-                  tb_sucursal ON cp_SolicitudPago.IdEmpresa = tb_sucursal.IdEmpresa AND cp_SolicitudPago.IdSucursal = tb_sucursal.IdSucursal
+SELECT dbo.cp_SolicitudPago.IdEmpresa, dbo.cp_SolicitudPago.IdSolicitud, dbo.cp_SolicitudPago.IdSucursal, dbo.cp_SolicitudPago.Fecha, dbo.cp_SolicitudPago.IdProveedor, dbo.cp_SolicitudPago.Concepto, dbo.cp_SolicitudPago.Estado, 
+                  dbo.cp_SolicitudPago.Valor, dbo.cp_SolicitudPago.Solicitante, dbo.tb_persona.pe_nombreCompleto, dbo.tb_persona.pe_cedulaRuc, dbo.tb_sucursal.Su_Descripcion, dbo.seg_usuario.Nombre
+FROM     dbo.cp_SolicitudPago INNER JOIN
+                  dbo.cp_proveedor ON dbo.cp_SolicitudPago.IdEmpresa = dbo.cp_proveedor.IdEmpresa AND dbo.cp_SolicitudPago.IdProveedor = dbo.cp_proveedor.IdProveedor INNER JOIN
+                  dbo.tb_persona ON dbo.cp_proveedor.IdPersona = dbo.tb_persona.IdPersona INNER JOIN
+                  dbo.tb_sucursal ON dbo.cp_SolicitudPago.IdEmpresa = dbo.tb_sucursal.IdEmpresa AND dbo.cp_SolicitudPago.IdSucursal = dbo.tb_sucursal.IdSucursal INNER JOIN
+                  dbo.seg_usuario ON dbo.cp_SolicitudPago.IdUsuarioCreacion = dbo.seg_usuario.IdUsuario
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 2, @level0type = N'SCHEMA', @level0name = N'web', @level1type = N'VIEW', @level1name = N'VWCXP_011';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane2', @value = N' Alias = 900
+         Table = 1170
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+', @level0type = N'SCHEMA', @level0name = N'web', @level1type = N'VIEW', @level1name = N'VWCXP_011';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane1', @value = N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[39] 4[3] 2[3] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "cp_SolicitudPago"
+            Begin Extent = 
+               Top = 7
+               Left = 48
+               Bottom = 170
+               Right = 293
+            End
+            DisplayFlags = 280
+            TopColumn = 7
+         End
+         Begin Table = "cp_proveedor"
+            Begin Extent = 
+               Top = 175
+               Left = 48
+               Bottom = 338
+               Right = 322
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "tb_persona"
+            Begin Extent = 
+               Top = 343
+               Left = 48
+               Bottom = 506
+               Right = 322
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "tb_sucursal"
+            Begin Extent = 
+               Top = 511
+               Left = 48
+               Bottom = 674
+               Right = 320
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "seg_usuario"
+            Begin Extent = 
+               Top = 67
+               Left = 567
+               Bottom = 292
+               Right = 861
+            End
+            DisplayFlags = 280
+            TopColumn = 9
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+      Begin ColumnWidths = 9
+         Width = 284
+         Width = 1200
+         Width = 1200
+         Width = 1200
+         Width = 1200
+         Width = 1200
+         Width = 1200
+         Width = 1200
+         Width = 1200
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 11
+         Column = 1440
+        ', @level0type = N'SCHEMA', @level0name = N'web', @level1type = N'VIEW', @level1name = N'VWCXP_011';
+
