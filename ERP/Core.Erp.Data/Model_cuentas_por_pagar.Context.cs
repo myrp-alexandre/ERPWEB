e80efc765a31b27cc2cc36aa7eb3e6b7afef35e3
Jorge Aroca <jaroca@fixed-sa.com>
2019-01-02 20:23:18 -0500

    12

--- ERP/Core.Erp.Data/Model_cuentas_por_pagar.Context.cs
+++ ERP/Core.Erp.Data/Model_cuentas_por_pagar.Context.cs
@@ -59,10 +59,10 @@ namespace Core.Erp.Data
         public DbSet<vwcp_orden_pago> vwcp_orden_pago { get; set; }
         public DbSet<cp_orden_giro_det> cp_orden_giro_det { get; set; }
         public DbSet<vwcp_orden_giro_det> vwcp_orden_giro_det { get; set; }
-        public DbSet<cp_SolicitudPago> cp_SolicitudPago { get; set; }
         public DbSet<cp_proveedor> cp_proveedor { get; set; }
         public DbSet<vwcp_orden_giro> vwcp_orden_giro { get; set; }
         public DbSet<cp_orden_giro> cp_orden_giro { get; set; }
+        public DbSet<cp_SolicitudPago> cp_SolicitudPago { get; set; }
     
         public virtual ObjectResult<spcp_Get_Data_orden_pago_con_cancelacion_data_Result> spcp_Get_Data_orden_pago_con_cancelacion_data(Nullable<int> idEmpresa, Nullable<decimal> idPersona_ini, Nullable<decimal> idPersona_fin, string idTipoPersona, Nullable<decimal> idEntidad_ini, Nullable<decimal> idEntidad_fin, string idEstado_Aprobacion, string idUsuario, Nullable<bool> mostrar_saldo_0)
         {
