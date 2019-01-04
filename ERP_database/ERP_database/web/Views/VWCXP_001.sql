9e41875f7369cee96eef6fb7eb47e7422d2f334d
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2019-01-04 08:14:50 -0500

    123

--- ERP_database/ERP_database/web/Views/VWCXP_001.sql
+++ ERP_database/ERP_database/web/Views/VWCXP_001.sql
@@ -1,12 +1,11 @@
-﻿CREATE VIEW web.VWCXP_001
-AS
+﻿CREATE view [web].[VWCXP_001]
+as
 SELECT dbo.cp_orden_giro.IdEmpresa, dbo.cp_orden_giro.IdCbteCble_Ogiro, dbo.cp_orden_giro.IdTipoCbte_Ogiro, dbo.cp_TipoDocumento.Codigo, dbo.cp_TipoDocumento.Descripcion, dbo.cp_codigo_SRI.codigoSRI, 
-                  dbo.cp_codigo_SRI.co_descripcion, dbo.tb_empresa.em_nombre, dbo.tb_sucursal.Su_Descripcion, per.pe_nombreCompleto AS pr_nombre, dbo.cp_orden_giro.IdIden_credito, 
-                  dbo.cp_orden_giro.IdOrden_giro_Tipo, dbo.cp_orden_giro.IdProveedor, dbo.cp_orden_giro.co_fechaOg, dbo.cp_orden_giro.co_serie, 
-                  dbo.cp_orden_giro.co_factura, dbo.cp_orden_giro.co_FechaFactura, dbo.cp_orden_giro.co_FechaFactura_vct, dbo.cp_orden_giro.co_observacion, dbo.cp_orden_giro.co_subtotal_iva, dbo.cp_orden_giro.co_subtotal_siniva, 
-                  dbo.cp_orden_giro.co_baseImponible, dbo.cp_orden_giro.co_total, dbo.cp_orden_giro.co_valorpagar, dbo.ct_cbtecble_det.secuencia, dbo.ct_cbtecble_det.IdCtaCble, dbo.ct_plancta.pc_Cuenta, dbo.ct_cbtecble_det.dc_Valor, 
-                  CASE WHEN dbo.ct_cbtecble_det.dc_Valor > 0 THEN dbo.ct_cbtecble_det.dc_Valor ELSE 0 END AS dc_Valor_Debe, CASE WHEN dbo.ct_cbtecble_det.dc_Valor < 0 THEN ABS(dbo.ct_cbtecble_det.dc_Valor) ELSE 0 END AS dc_Valor_Haber, 
-                  dbo.ct_cbtecble_det.dc_Observacion
+                  dbo.cp_codigo_SRI.co_descripcion, dbo.tb_empresa.em_nombre, dbo.tb_sucursal.Su_Descripcion, per.pe_nombreCompleto AS pr_nombre, dbo.cp_orden_giro.IdIden_credito, dbo.cp_orden_giro.IdOrden_giro_Tipo, 
+                  dbo.cp_orden_giro.IdProveedor, dbo.cp_orden_giro.co_fechaOg, dbo.cp_orden_giro.co_serie, dbo.cp_orden_giro.co_factura, dbo.cp_orden_giro.co_FechaFactura, dbo.cp_orden_giro.co_FechaFactura_vct, 
+                  dbo.cp_orden_giro.co_observacion, dbo.cp_orden_giro.co_subtotal_iva, dbo.cp_orden_giro.co_subtotal_siniva, dbo.cp_orden_giro.co_baseImponible, dbo.cp_orden_giro.co_total, isnull(dbo.cp_orden_giro.co_total - isnull(ret.re_valor_retencion,0),0) co_valorpagar, 
+                  dbo.ct_cbtecble_det.secuencia, dbo.ct_cbtecble_det.IdCtaCble, dbo.ct_plancta.pc_Cuenta, dbo.ct_cbtecble_det.dc_Valor, CASE WHEN dbo.ct_cbtecble_det.dc_Valor > 0 THEN dbo.ct_cbtecble_det.dc_Valor ELSE 0 END AS dc_Valor_Debe, 
+                  CASE WHEN dbo.ct_cbtecble_det.dc_Valor < 0 THEN ABS(dbo.ct_cbtecble_det.dc_Valor) ELSE 0 END AS dc_Valor_Haber, dbo.ct_cbtecble_det.dc_Observacion
 FROM     dbo.cp_orden_giro INNER JOIN
                   dbo.tb_sucursal ON dbo.cp_orden_giro.IdEmpresa = dbo.tb_sucursal.IdEmpresa AND dbo.cp_orden_giro.IdSucursal = dbo.tb_sucursal.IdSucursal INNER JOIN
                   dbo.tb_empresa ON dbo.tb_sucursal.IdEmpresa = dbo.tb_empresa.IdEmpresa INNER JOIN
@@ -16,7 +15,16 @@ FROM     dbo.cp_orden_giro INNER JOIN
                   dbo.cp_orden_giro.IdCbteCble_Ogiro = dbo.ct_cbtecble_det.IdCbteCble INNER JOIN
                   dbo.ct_plancta ON dbo.ct_cbtecble_det.IdEmpresa = dbo.ct_plancta.IdEmpresa AND dbo.ct_cbtecble_det.IdCtaCble = dbo.ct_plancta.IdCtaCble INNER JOIN
                   dbo.tb_persona AS per ON dbo.cp_proveedor.IdPersona = per.IdPersona LEFT OUTER JOIN
-                  dbo.cp_codigo_SRI ON dbo.cp_orden_giro.IdIden_credito = dbo.cp_codigo_SRI.IdCodigo_SRI 
+                  dbo.cp_codigo_SRI ON dbo.cp_orden_giro.IdIden_credito = dbo.cp_codigo_SRI.IdCodigo_SRI
+				  LEFT JOIN (
+				  SELECT c.IdEmpresa_Ogiro, c.IdTipoCbte_Ogiro, c.IdCbteCble_Ogiro, sum(d.re_valor_retencion) re_valor_retencion 
+				  FROM cp_retencion as c
+				  inner join cp_retencion_det as d
+				  on c.IdEmpresa = d.IdEmpresa and c.IdRetencion = d.IdRetencion
+				  group by c.IdEmpresa_Ogiro, c.IdTipoCbte_Ogiro, c.IdCbteCble_Ogiro
+				  ) as ret on ret.IdEmpresa_Ogiro = cp_orden_giro.IdEmpresa
+				  and ret.IdTipoCbte_Ogiro = cp_orden_giro.IdTipoCbte_Ogiro
+				  and ret.IdCbteCble_Ogiro = cp_orden_giro.IdCbteCble_Ogiro 
 
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane1', @value = N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
