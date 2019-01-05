3f8cb263c68430dcdb762ef498a156d37bf47d53
Jorge Aroca <jaroca@fixed-sa.com>
2019-01-05 01:26:26 -0500

    1

---
+++ ERP_database/ERP_database/dbo/Views/vwct_cbtecble_det.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW vwct_cbtecble_det
+AS
+SELECT        dbo.ct_cbtecble_det.IdEmpresa, dbo.ct_cbtecble_det.IdTipoCbte, dbo.ct_cbtecble_det.IdCbteCble, dbo.ct_cbtecble_det.secuencia, dbo.ct_cbtecble_det.IdCtaCble, dbo.ct_cbtecble_det.dc_Valor, 
+                         dbo.ct_cbtecble_det.dc_Observacion, dbo.ct_cbtecble_det.dc_para_conciliar, dbo.ct_cbtecble_det.IdGrupoPresupuesto, dbo.ct_cbtecble_det.IdCtaCble +' - '+ dbo.ct_plancta.pc_Cuenta pc_Cuenta
+FROM            dbo.ct_cbtecble_det INNER JOIN
+                         dbo.ct_plancta ON dbo.ct_cbtecble_det.IdEmpresa = dbo.ct_plancta.IdEmpresa AND dbo.ct_cbtecble_det.IdCtaCble = dbo.ct_plancta.IdCtaCble
