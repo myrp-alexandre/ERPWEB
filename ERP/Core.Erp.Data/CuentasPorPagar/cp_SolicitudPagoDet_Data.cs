576bf76d43ca95a68b46336f6c0578e17bc6fae7
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2019-01-08 15:45:39 -0500

    mk

--- ERP/Core.Erp.Data/CuentasPorPagar/cp_SolicitudPagoDet_Data.cs
+++ ERP/Core.Erp.Data/CuentasPorPagar/cp_SolicitudPagoDet_Data.cs
@@ -14,6 +14,7 @@ namespace Core.Erp.Data.CuentasPorPagar
         {
             try
             {
+                int secuencia = 1;
                 List<cp_SolicitudPagoDet_Info> Lista;
                 using (Entities_cuentas_por_pagar Context = new Entities_cuentas_por_pagar())
                 {
@@ -47,6 +48,7 @@ namespace Core.Erp.Data.CuentasPorPagar
 
                              }).ToList();
                 }
+                Lista.ForEach(v=>v.Secuencia=secuencia++);
                 return Lista;
             }
             catch (Exception)
