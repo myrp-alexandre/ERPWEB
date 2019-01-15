8ed0a037456e8d5238c0a2c143556c5e48e862ef
carloscede <carloscede@hotmail.es>
2019-01-14 23:21:08 -0800

    ....

---
+++ ERP/Core.Erp.Web/Areas/RRHH/Controllers/PrestamosConsultasController.cs
@@ -0,0 +1,36 @@
+﻿using Core.Erp.Info.General;
+using Core.Erp.Web.Helps;
+using DevExpress.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Core.Erp.Web.Areas.RRHH.Controllers
+{
+    public class PrestamosConsultasController : Controller
+    {
+        // GET: RRHH/PrestamosConsultas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region metodo bajo demanda
+        public ActionResult CmbEmpleado_novedades()
+        {
+            decimal model = new decimal();
+            return PartialView("_CmbEmpleado_prestamo_consulta", model);
+        }
+        public List<tb_persona_Info> get_list_bajo_demanda(ListEditItemsRequestedByFilterConditionEventArgs args)
+        {
+            return bus_persona.get_list_bajo_demanda(args, Convert.ToInt32(SessionFixed.IdEmpresa), cl_enumeradores.eTipoPersona.EMPLEA.ToString());
+        }
+        public tb_persona_Info get_info_bajo_demanda(ListEditItemRequestedByValueEventArgs args)
+        {
+            return bus_persona.get_info_bajo_demanda(args, Convert.ToInt32(SessionFixed.IdEmpresa), cl_enumeradores.eTipoPersona.EMPLEA.ToString());
+        }
+        #endregion
+    }
+}
