460a8466d63d84d1d6af11785fff4adebbf35ac0
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2019-01-16 12:01:31 -0500

    j

--- ERP/Core.Erp.Web/Areas/Banco/Controllers/TipoFlujoBancoController.cs
+++ ERP/Core.Erp.Web/Areas/Banco/Controllers/TipoFlujoBancoController.cs
@@ -25,7 +25,7 @@ namespace Core.Erp.Web.Areas.Banco.Controllers
         ba_TipoFlujo_Bus bus_tipo = new ba_TipoFlujo_Bus();
         public ActionResult CmbFlujo_Tipo()
         {
-            decimal model = new decimal();
+            ba_TipoFlujo_Info model = new ba_TipoFlujo_Info();
             return PartialView("_CmbFlujo_Tipo", model);
         }
         public List<ba_TipoFlujo_Info> get_list_bajo_demandaFlujo(ListEditItemsRequestedByFilterConditionEventArgs args)
