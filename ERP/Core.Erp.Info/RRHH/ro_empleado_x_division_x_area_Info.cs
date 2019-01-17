9287dba3fb56fb5d9e486e918b4c83af92af9e35
carloscede <carloscede@hotmail.es>
2019-01-16 23:21:49 -0800

    .....

---
+++ ERP/Core.Erp.Info/RRHH/ro_empleado_x_division_x_area_Info.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Info.RRHH
+{
+   public class ro_empleado_x_division_x_area_Info
+    {
+    }
+}
