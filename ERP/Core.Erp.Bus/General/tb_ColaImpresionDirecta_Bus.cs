c300dbe59893fc3a6d3ad48de6f8703bbe9e71b0
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2019-01-19 13:35:24 -0500

    1

---
+++ ERP/Core.Erp.Bus/General/tb_ColaImpresionDirecta_Bus.cs
@@ -0,0 +1,39 @@
+﻿using Core.Erp.Data.General;
+using Core.Erp.Info.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Bus.General
+{
+    public class tb_ColaImpresionDirecta_Bus
+    {
+        tb_ColaImpresionDirecta_Data odata = new tb_ColaImpresionDirecta_Data();
+
+        public bool GuardarDB(tb_ColaImpresionDirecta_Info info)
+        {
+            try
+            {
+                return odata.GuardarDB(info);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public tb_ColaImpresionDirecta_Info GetInfoPorImprimir(string IPMaquina)
+        {
+            try
+            {
+                return odata.GetInfoPorImprimir(IPMaquina);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
--- ERP/Core.Erp.Data/General/tb_ColaImpresionDirecta_Data.cs
+++ ERP/Core.Erp.Data/General/tb_ColaImpresionDirecta_Data.cs
@@ -1,15 +1,12 @@
 ﻿using Core.Erp.Info.General;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.Erp.Data.General
 {
     public class tb_ColaImpresionDirecta_Data
     {
-        public bool guardarDB(tb_ColaImpresionDirecta_Info info)
+        public bool GuardarDB(tb_ColaImpresionDirecta_Info info)
         {
             try
             {
@@ -17,14 +14,51 @@ namespace Core.Erp.Data.General
                 {
                     db.tb_ColaImpresionDirecta.Add(new tb_ColaImpresionDirecta
                     {
-
+                        IdEmpresa = info.IdEmpresa,
+                        CodReporte = info.CodReporte,
+                        IPUsuario = info.IPUsuario,
+                        IPMaquina = info.IPMaquina,
+                        Parametros = info.Parametros,
+                        Usuario = info.Usuario,
+                        NombreEmpresa = info.NombreEmpresa,
+                        FechaEnvio = DateTime.Now                        
                     });
                 }
                 return true;
             }
             catch (Exception)
             {
+                throw;
+            }
+        }
 
+        public tb_ColaImpresionDirecta_Info GetInfoPorImprimir(string IPMaquina)
+        {
+            try
+            {
+                using (Entities_general db = new Entities_general())
+                {
+                    var entity = db.tb_ColaImpresionDirecta.Where(q => q.IPMaquina == IPMaquina && q.FechaImpresion == null).FirstOrDefault();
+                    if (entity == null)
+                        return null;
+                    return new tb_ColaImpresionDirecta_Info
+                    {
+                        IdEmpresa = entity.IdEmpresa,
+                        IdImpresion = entity.IdImpresion,
+                        CodReporte = entity.CodReporte,
+                        IPUsuario = entity.IPUsuario,
+                        IPMaquina = entity.IPMaquina,
+                        Parametros = entity.Parametros,
+                        Usuario = entity.Usuario,
+                        NombreEmpresa = entity.NombreEmpresa,
+                        FechaEnvio = entity.FechaEnvio,
+                        FechaImpresion = entity.FechaImpresion,
+                        Comentario = entity.Comentario
+                    };
+                }
+            }
+            catch (Exception)
+            {
                 throw;
             }
         }
