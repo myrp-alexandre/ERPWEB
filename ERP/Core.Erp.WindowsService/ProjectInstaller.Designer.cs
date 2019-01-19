fefa71def0a74c3a93eeea8453eb4b1dc8bf1258
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2019-01-19 16:55:54 -0500

    2

---
+++ ERP/Core.Erp.WindowsService/ProjectInstaller.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Core.Erp.WindowsService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.FixedERP_ImpresionDirecta = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // FixedERP_ImpresionDirecta
+            // 
+            this.FixedERP_ImpresionDirecta.Description = "Servicio para administración de cola de impresión directa desde la web con el sis" +
+    "tema Fixed ERP";
+            this.FixedERP_ImpresionDirecta.DisplayName = "FixedERP ImpresionDirecta";
+            this.FixedERP_ImpresionDirecta.ServiceName = "FixedERP ImpresionDirecta";
+            this.FixedERP_ImpresionDirecta.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.FixedERP_ImpresionDirecta});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller FixedERP_ImpresionDirecta;
+    }
+}
