ee6d0ebe0b1f96aad9fe540eda6380ed9690ac83
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2019-01-19 16:52:33 -0500

    2

--- ERP/Core.Erp.WindowsService/ImpresionDirecta.Designer.cs
+++ ERP/Core.Erp.WindowsService/ImpresionDirecta.Designer.cs
@@ -28,8 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.ServiceName = "Service1";
+            // 
+            // ImpresionDirecta
+            // 
+            this.ServiceName = "FixedERP ImpresionDirecta";
+
         }
 
         #endregion
