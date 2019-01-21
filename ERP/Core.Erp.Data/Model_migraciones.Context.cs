265d8726115f015eddd588b65e30b0dc9b71d95c
carloscede <carloscede@hotmail.es>
2019-01-21 15:14:42 -0800

    ....

--- ERP/Core.Erp.Data/Model_migraciones.Context.cs
+++ ERP/Core.Erp.Data/Model_migraciones.Context.cs
@@ -15,6 +15,7 @@ namespace Core.Erp.Data
     using System.Data.Entity.Core.Objects;
     using System.Linq;
 
+
     public partial class Entity_Eventos : DbContext
     {
         public Entity_Eventos()
