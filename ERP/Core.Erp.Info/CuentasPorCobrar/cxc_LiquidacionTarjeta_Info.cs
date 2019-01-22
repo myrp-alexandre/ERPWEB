33f5ddf956c831fbcd279bddf1c630c5e8069ef7
WendyCherres <wendycherres@gmail.com>
2019-01-22 14:03:03 -0500

    ...

---
+++ ERP/Core.Erp.Info/CuentasPorCobrar/cxc_LiquidacionTarjeta_Info.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Info
+{
+    public class cxc_LiquidacionTarjeta_Info
+    {
+        public int IdEmpresa { get; set; }
+        public int IdSucursal { get; set; }
+        public decimal IdLiquidacion { get; set; }
+        public string Lote { get; set; }
+        public System.DateTime Fecha { get; set; }
+        public int IdTarjeta { get; set; }
+        public int IdBanco { get; set; }
+        public string Observacion { get; set; }
+        public bool Estado { get; set; }
+        public double Valor { get; set; }
+        public Nullable<int> IdEmpresa_ct { get; set; }
+        public Nullable<int> IdTipoCbte_ct { get; set; }
+        public Nullable<decimal> IdCbteCble_ct { get; set; }
+        public string IdUsuarioCreacion { get; set; }
+        public Nullable<System.DateTime> FechaCreacion { get; set; }
+        public string IdUsuarioModificacion { get; set; }
+        public Nullable<System.DateTime> FechaModificacion { get; set; }
+        public string IdUsuarioAnulacion { get; set; }
+        public Nullable<System.DateTime> FechaAnulacion { get; set; }
+        public string MotivoAnulacion { get; set; }
+    }
+}
