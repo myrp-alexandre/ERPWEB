1611adde6bf704cbf16ade627372bbb464ee0458
carloscede <carloscede@hotmail.es>
2019-01-23 23:19:25 -0800

    ....

--- ERP_database/ERP_database/dbo/Tables/ro_empleado_x_jornada.sql
+++ ERP_database/ERP_database/dbo/Tables/ro_empleado_x_jornada.sql
@@ -5,8 +5,12 @@
     [IdJornada]   INT          NOT NULL,
     [ValorHora]   FLOAT (53)   NOT NULL,
     [MaxNumHoras] INT          NOT NULL,
-    CONSTRAINT [PK_ro_empleado_x_jornada] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdEmpleado] ASC, [Secuencia] ASC)
+    CONSTRAINT [PK_ro_empleado_x_jornada] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdEmpleado] ASC, [Secuencia] ASC),
+    CONSTRAINT [FK_ro_empleado_x_jornada_ro_empleado] FOREIGN KEY ([IdEmpresa], [IdEmpleado]) REFERENCES [dbo].[ro_empleado] ([IdEmpresa], [IdEmpleado]),
+    CONSTRAINT [FK_ro_empleado_x_jornada_ro_jornada] FOREIGN KEY ([IdEmpresa], [IdJornada]) REFERENCES [dbo].[ro_jornada] ([IdEmpresa], [IdJornada])
 );
+
+
 
 
 
