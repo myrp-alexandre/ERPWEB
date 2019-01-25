4e77543edd6502c2c4855a1df156d720a5c3c56e
WendyCherres <wendycherres@gmail.com>
2019-01-25 12:58:15 -0500

    ..

--- ERP/Core.Erp.Data/Reportes/RRHH/ROL_022_Data.cs
+++ ERP/Core.Erp.Data/Reportes/RRHH/ROL_022_Data.cs
@@ -35,132 +35,132 @@ namespace Core.Erp.Data.Reportes.RRHH
                 {
                     if (tipoRubro == "E")
                     {
-                        Lista = (from q in Context.VWROL_022
-                                 where q.IdEmpresa == IdEmpresa
-                                 && q.IdPeriodo == IdPeriodo
-                               //  && q.IdDivision >= IdDivisionInicio
-                                 //&& q.IdDivision <= IdDivisionFin
-                                 && q.IdArea >= IdAreaInicio
-                                 && q.IdArea <= IdAreaFin
-                                 && IdSucursalInicio <= q.IdSucursal && q.IdSucursal <= IdSucursalFin
-                                 && q.IdArea >= IdAreaInicio
-                                 && q.IdArea <= IdAreaFin
-                                 && q.IdNominaTipo == IdNomina
-                                 && q.IdNominaTipoLiqui == IdNominaTipo
-                                 && q.Valor > 0
-                                 && (q.IdRubro == info_rub_calculados.IdRubro_bono_x_antiguedad || q.IdRubro == info_rub_calculados.IdRubro_tot_egr || q.ru_tipo == "E")
-                                 select new ROL_022_Info
-                                 {
-                                     IdEmpresa = q.IdEmpresa,
-                                     IdSucursal = q.IdSucursal,
-                                     IdNominaTipo = q.IdNominaTipo,
-                                     IdNominaTipoLiqui = q.IdNominaTipoLiqui,
-                                     IdPeriodo = q.IdPeriodo,
-                                     IdRubro = q.IdRubro,
-                                     Rubro = q.Rubro,
-                                     Area = q.Area,
-                                     ru_orden = q.ru_orden,
-                                     Valor = q.Valor,
-                                     RubroDescripcion = q.RubroDescripcion,
-                                     pe_FechaFin = q.pe_FechaFin,
-                                     pe_FechaIni = q.pe_FechaIni,
-                                     ru_tipo = q.ru_tipo,
-                                     Grupo = q.Grupo,
-                                     em_codigo = q.em_codigo,
-                                     IdEmpleado = q.IdEmpleado,
-                                     Ruc = q.Ruc,
-                                     pe_apellido = q.pe_apellido,
-                                     IdArea = q.IdArea,
-                                     NombreCompleto = q.NombreCompleto
-                                 }).ToList();
+                        //Lista = (from q in Context.VWROL_022
+                        //         where q.IdEmpresa == IdEmpresa
+                        //         && q.IdPeriodo == IdPeriodo
+                        //       //  && q.IdDivision >= IdDivisionInicio
+                        //         //&& q.IdDivision <= IdDivisionFin
+                        //         && q.IdArea >= IdAreaInicio
+                        //         && q.IdArea <= IdAreaFin
+                        //         && IdSucursalInicio <= q.IdSucursal && q.IdSucursal <= IdSucursalFin
+                        //         && q.IdArea >= IdAreaInicio
+                        //         && q.IdArea <= IdAreaFin
+                        //         && q.IdNominaTipo == IdNomina
+                        //         && q.IdNominaTipoLiqui == IdNominaTipo
+                        //         && q.Valor > 0
+                        //         && (q.IdRubro == info_rub_calculados.IdRubro_bono_x_antiguedad || q.IdRubro == info_rub_calculados.IdRubro_tot_egr || q.ru_tipo == "E")
+                        //         select new ROL_022_Info
+                        //         {
+                        //             IdEmpresa = q.IdEmpresa,
+                        //             IdSucursal = q.IdSucursal,
+                        //             IdNominaTipo = q.IdNominaTipo,
+                        //             IdNominaTipoLiqui = q.IdNominaTipoLiqui,
+                        //             IdPeriodo = q.IdPeriodo,
+                        //             IdRubro = q.IdRubro,
+                        //             Rubro = q.Rubro,
+                        //             Area = q.Area,
+                        //             ru_orden = q.ru_orden,
+                        //             Valor = q.Valor,
+                        //             RubroDescripcion = q.RubroDescripcion,
+                        //             pe_FechaFin = q.pe_FechaFin,
+                        //             pe_FechaIni = q.pe_FechaIni,
+                        //             ru_tipo = q.ru_tipo,
+                        //             Grupo = q.Grupo,
+                        //             em_codigo = q.em_codigo,
+                        //             IdEmpleado = q.IdEmpleado,
+                        //             Ruc = q.Ruc,
+                        //             pe_apellido = q.pe_apellido,
+                        //             IdArea = q.IdArea,
+                        //             NombreCompleto = q.NombreCompleto
+                        //         }).ToList();
                     }
                     else if (tipoRubro == "I")
                     {
-                        Lista = (from q in Context.VWROL_022
-                                 where q.IdEmpresa == IdEmpresa
-                                 && q.IdPeriodo == IdPeriodo
-                                 && q.IdArea >= IdAreaInicio
-                                 && q.IdArea <= IdAreaFin
-                                 && IdSucursalInicio <= q.IdSucursal && q.IdSucursal <= IdSucursalFin
-                                 && q.IdArea >= IdAreaInicio
-                                 && q.IdArea <= IdAreaFin
-                                 && q.IdNominaTipo == IdNomina
-                                 && q.IdNominaTipoLiqui == IdNominaTipo
-                                 && q.Valor > 0
-                                 && (q.IdRubro == info_rub_calculados.IdRubro_tot_ing || q.ru_tipo == "I")
-                                 select new ROL_022_Info
-                                 {
-                                     IdEmpresa = q.IdEmpresa,
-                                     IdSucursal = q.IdSucursal,
-                                     IdNominaTipo = q.IdNominaTipo,
-                                     IdNominaTipoLiqui = q.IdNominaTipoLiqui,
-                                     IdPeriodo = q.IdPeriodo,
-                                     IdRubro = q.IdRubro,
-                                     Rubro = q.Rubro,
-                                     Area = q.Area,
-                                     ru_orden = q.ru_orden,
-                                     Valor = q.Valor,
-                                     RubroDescripcion = q.RubroDescripcion,
-                                     pe_FechaFin = q.pe_FechaFin,
-                                     pe_FechaIni = q.pe_FechaIni,
-                                     ru_tipo = q.ru_tipo,
-                                     Grupo = q.Grupo,
-                                     em_codigo = q.em_codigo,
-                                     IdEmpleado = q.IdEmpleado,
-                                     Ruc = q.Ruc,
-                                     pe_apellido = q.pe_apellido,
-                                     IdArea = q.IdArea,
-                                     NombreCompleto = q.NombreCompleto
+                        //Lista = (from q in Context.VWROL_022
+                        //         where q.IdEmpresa == IdEmpresa
+                        //         && q.IdPeriodo == IdPeriodo
+                        //         && q.IdArea >= IdAreaInicio
+                        //         && q.IdArea <= IdAreaFin
+                        //         && IdSucursalInicio <= q.IdSucursal && q.IdSucursal <= IdSucursalFin
+                        //         && q.IdArea >= IdAreaInicio
+                        //         && q.IdArea <= IdAreaFin
+                        //         && q.IdNominaTipo == IdNomina
+                        //         && q.IdNominaTipoLiqui == IdNominaTipo
+                        //         && q.Valor > 0
+                        //         && (q.IdRubro == info_rub_calculados.IdRubro_tot_ing || q.ru_tipo == "I")
+                        //         select new ROL_022_Info
+                        //         {
+                        //             IdEmpresa = q.IdEmpresa,
+                        //             IdSucursal = q.IdSucursal,
+                        //             IdNominaTipo = q.IdNominaTipo,
+                        //             IdNominaTipoLiqui = q.IdNominaTipoLiqui,
+                        //             IdPeriodo = q.IdPeriodo,
+                        //             IdRubro = q.IdRubro,
+                        //             Rubro = q.Rubro,
+                        //             Area = q.Area,
+                        //             ru_orden = q.ru_orden,
+                        //             Valor = q.Valor,
+                        //             RubroDescripcion = q.RubroDescripcion,
+                        //             pe_FechaFin = q.pe_FechaFin,
+                        //             pe_FechaIni = q.pe_FechaIni,
+                        //             ru_tipo = q.ru_tipo,
+                        //             Grupo = q.Grupo,
+                        //             em_codigo = q.em_codigo,
+                        //             IdEmpleado = q.IdEmpleado,
+                        //             Ruc = q.Ruc,
+                        //             pe_apellido = q.pe_apellido,
+                        //             IdArea = q.IdArea,
+                        //             NombreCompleto = q.NombreCompleto
 
-                                 }).ToList();
+                        //         }).ToList();
                     }
                     else
                     {
-                        Lista = (from q in Context.VWROL_022
-                                 where q.IdEmpresa == IdEmpresa
-                                 && q.IdPeriodo == IdPeriodo
-                               //  && q.IdDivision >= IdDivisionInicio
-                                // && q.IdDivision <= IdDivisionFin
-                                 && q.IdArea >= IdAreaInicio
-                                 && q.IdArea <= IdAreaFin
-                                 && IdSucursalInicio <= q.IdSucursal && q.IdSucursal <= IdSucursalFin
-                                 && q.IdArea >= IdAreaInicio
-                                 && q.IdArea <= IdAreaFin
-                                 && q.IdNominaTipo == IdNomina
-                                 && q.IdNominaTipoLiqui == IdNominaTipo
-                                 && q.Valor > 0
-                                 && (
-                                 (q.IdRubro == info_rub_calculados.IdRubro_tot_pagar
-                                 || q.IdRubro == info_rub_calculados.IdRubro_tot_ing
-                                 || q.IdRubro == info_rub_calculados.IdRubro_tot_egr
-                                 )
-                                 || (q.ru_tipo == "I" || q.ru_tipo == "E")
-                                 )
-                                 select new ROL_022_Info
-                                 {
-                                     IdEmpresa = q.IdEmpresa,
-                                     IdSucursal = q.IdSucursal,
-                                     IdNominaTipo = q.IdNominaTipo,
-                                     IdNominaTipoLiqui = q.IdNominaTipoLiqui,
-                                     IdPeriodo = q.IdPeriodo,
-                                     IdRubro = q.IdRubro,
-                                     Rubro = q.Rubro,
-                                     Area = q.Area,
-                                     ru_orden = q.ru_orden,
-                                     Valor = q.Valor,
-                                     RubroDescripcion = q.RubroDescripcion,
-                                     pe_FechaFin = q.pe_FechaFin,
-                                     pe_FechaIni = q.pe_FechaIni,
-                                     ru_tipo = q.ru_tipo,
-                                     Grupo = q.Grupo,
-                                     em_codigo = q.em_codigo,
-                                     IdEmpleado = q.IdEmpleado,
-                                     Ruc = q.Ruc,
-                                     pe_apellido = q.pe_apellido,
-                                     IdArea = q.IdArea,                                     
-                                     NombreCompleto=q.NombreCompleto
+                        //Lista = (from q in Context.VWROL_022
+                        //         where q.IdEmpresa == IdEmpresa
+                        //         && q.IdPeriodo == IdPeriodo
+                        //       //  && q.IdDivision >= IdDivisionInicio
+                        //        // && q.IdDivision <= IdDivisionFin
+                        //         && q.IdArea >= IdAreaInicio
+                        //         && q.IdArea <= IdAreaFin
+                        //         && IdSucursalInicio <= q.IdSucursal && q.IdSucursal <= IdSucursalFin
+                        //         && q.IdArea >= IdAreaInicio
+                        //         && q.IdArea <= IdAreaFin
+                        //         && q.IdNominaTipo == IdNomina
+                        //         && q.IdNominaTipoLiqui == IdNominaTipo
+                        //         && q.Valor > 0
+                        //         && (
+                        //         (q.IdRubro == info_rub_calculados.IdRubro_tot_pagar
+                        //         || q.IdRubro == info_rub_calculados.IdRubro_tot_ing
+                        //         || q.IdRubro == info_rub_calculados.IdRubro_tot_egr
+                        //         )
+                        //         || (q.ru_tipo == "I" || q.ru_tipo == "E")
+                        //         )
+                        //         select new ROL_022_Info
+                        //         {
+                        //             IdEmpresa = q.IdEmpresa,
+                        //             IdSucursal = q.IdSucursal,
+                        //             IdNominaTipo = q.IdNominaTipo,
+                        //             IdNominaTipoLiqui = q.IdNominaTipoLiqui,
+                        //             IdPeriodo = q.IdPeriodo,
+                        //             IdRubro = q.IdRubro,
+                        //             Rubro = q.Rubro,
+                        //             Area = q.Area,
+                        //             ru_orden = q.ru_orden,
+                        //             Valor = q.Valor,
+                        //             RubroDescripcion = q.RubroDescripcion,
+                        //             pe_FechaFin = q.pe_FechaFin,
+                        //             pe_FechaIni = q.pe_FechaIni,
+                        //             ru_tipo = q.ru_tipo,
+                        //             Grupo = q.Grupo,
+                        //             em_codigo = q.em_codigo,
+                        //             IdEmpleado = q.IdEmpleado,
+                        //             Ruc = q.Ruc,
+                        //             pe_apellido = q.pe_apellido,
+                        //             IdArea = q.IdArea,                                     
+                        //             NombreCompleto=q.NombreCompleto
 
-                                 }).ToList();
+                        //         }).ToList();
                     }
 
                 }
