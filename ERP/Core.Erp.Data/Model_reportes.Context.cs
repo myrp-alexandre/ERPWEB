0cf48ae6901fae5a0aebb708ab3b7927d200099b
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2019-01-29 10:18:17 -0500

    J

--- ERP/Core.Erp.Data/Model_reportes.Context.cs
+++ ERP/Core.Erp.Data/Model_reportes.Context.cs
@@ -9,12 +9,12 @@
 
 namespace Core.Erp.Data
 {
+
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     using System.Data.Entity.Core.Objects;
     using System.Linq;
-
 
     public partial class Entities_reportes : DbContext
     {
@@ -83,7 +83,6 @@ namespace Core.Erp.Data
         public DbSet<VWPRO_001> VWPRO_001 { get; set; }
         public DbSet<VWPRE_001> VWPRE_001 { get; set; }
         public DbSet<VWROL_008> VWROL_008 { get; set; }
-        public DbSet<VWFAC_003> VWFAC_003 { get; set; }
         public DbSet<VWCXP_011> VWCXP_011 { get; set; }
         public DbSet<VWBAN_005> VWBAN_005 { get; set; }
         public DbSet<VWBAN_006> VWBAN_006 { get; set; }
@@ -111,6 +110,7 @@ namespace Core.Erp.Data
         public DbSet<VWROL_021> VWROL_021 { get; set; }
         public DbSet<VWROL_020> VWROL_020 { get; set; }
         public DbSet<VWROL_002> VWROL_002 { get; set; }
+        public DbSet<VWFAC_003> VWFAC_003 { get; set; }
     
         public virtual ObjectResult<SPACTF_004_detalle_Result> SPACTF_004_detalle(Nullable<int> idEmpresa, Nullable<System.DateTime> fecha_corte, string idUsuario, Nullable<int> idActivoFijoTipo_ini, Nullable<int> idActivoFijoTipo_fin, Nullable<int> idCategoria_ini, Nullable<int> idCategoria_fin, string estado_Proceso)
         {
