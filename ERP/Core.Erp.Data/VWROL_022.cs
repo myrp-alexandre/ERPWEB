185c2ac71a20ee347ad4f7790e29b99006dac254
WendyCherres <wendycherres@gmail.com>
2019-01-30 12:16:12 -0500

    ,,,

--- ERP/Core.Erp.Data/VWROL_022.cs
+++ ERP/Core.Erp.Data/VWROL_022.cs
@@ -15,40 +15,17 @@ namespace Core.Erp.Data
     public partial class VWROL_022
     {
         public int IdEmpresa { get; set; }
-        public Nullable<int> IdSucursal { get; set; }
-        public int IdNominaTipo { get; set; }
-        public int IdNominaTipoLiqui { get; set; }
-        public decimal IdEmpleado { get; set; }
-        public Nullable<int> IdArea { get; set; }
-        public Nullable<int> IDividion { get; set; }
-        public Nullable<int> IdAreaEmpleado { get; set; }
-        public string AreaGeneral { get; set; }
-        public int IdPeriodo { get; set; }
-        public string IdRubro { get; set; }
-        public bool se_distribuye { get; set; }
-        public int Orden { get; set; }
-        public Nullable<double> Porcentaje { get; set; }
-        public Nullable<double> Valor { get; set; }
-        public Nullable<bool> rub_visible_reporte { get; set; }
-        public string Observacion { get; set; }
-        public string ru_descripcion { get; set; }
-        public System.DateTime pe_FechaIni { get; set; }
-        public System.DateTime pe_FechaFin { get; set; }
-        public string ru_tipo { get; set; }
-        public string rub_codigo { get; set; }
-        public string ru_codRolGen { get; set; }
-        public string ca_descripcion { get; set; }
-        public string em_codigo { get; set; }
-        public string pe_cedulaRuc { get; set; }
-        public string pe_nombreCompleto { get; set; }
-        public decimal IdRol { get; set; }
-        public string Descripcion { get; set; }
-        public string rub_grupo { get; set; }
-        public Nullable<double> Dias { get; set; }
         public Nullable<int> IdDivision { get; set; }
+        public Nullable<int> IdArea { get; set; }
+        public decimal IdEmpleado { get; set; }
         public Nullable<int> IdJornada { get; set; }
         public int IdNomina_Tipo { get; set; }
+        public string Descripcion { get; set; }
+        public string ru_descripcion { get; set; }
         public string empleado { get; set; }
+        public string ca_descripcion { get; set; }
+        public string ru_tipo { get; set; }
         public int ru_orden { get; set; }
+        public Nullable<double> Valor { get; set; }
     }
 }
