e5201cf8e696dbe25800abb3dfcce51e8a0aa67b
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2019-02-04 11:48:36 -0500

    gererbr

---
+++ ERP/Core.Erp.Data/Reportes/Banco/BAN_008_Data.cs
@@ -0,0 +1,52 @@
+﻿using Core.Erp.Info.Reportes.Banco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Data.Reportes.Banco
+{
+    public class BAN_008_Data
+    {
+        public List<BAN_008_Info> GetList(int IdEmpresa, DateTime fecha_ini, DateTime fecha_fin , int IdBanco)
+        {
+            try
+            {
+                List<BAN_008_Info> Lista;
+                using (Entities_reportes Context = new Entities_reportes())
+                {
+                    Lista = Context.SPBAN_008(IdEmpresa, fecha_ini, fecha_fin, IdBanco).Select(q => new BAN_008_Info
+                    {
+                        IdEmpresa = q.IdEmpresa,
+                        IdBanco = q.IdBanco,
+                        IdCbteCble = q.IdCbteCble,
+                        IdSucursal = q.IdSucursal,
+                        IdTipoCbte = q.IdTipoCbte,
+                        ba_descripcion = q.ba_descripcion,
+                        cb_Cheque = q.cb_Cheque,
+                        cb_Fecha = q.cb_Fecha,
+                        cb_giradoA = q.cb_giradoA,
+                        cb_Observacion = q.cb_Observacion,
+                        Estado = q.Estado,
+                        Orden = q.Orden,
+                        Referencia = q.Referencia,
+                        SaldoInicial = q.SaldoInicial,
+                        secuencia = q.secuencia,
+                        Su_Descripcion = q.Su_Descripcion,
+                        Tipo = q.Tipo,
+                        Valor = q.Valor,
+                        ValorAbsoluto = q.ValorAbsoluto
+                    }).ToList();
+                }
+                return Lista;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ ERP/Core.Erp.Info/Reportes/Banco/BAN_008_Info.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Info.Reportes.Banco
+{
+    public class BAN_008_Info
+    {
+        public Nullable<int> IdEmpresa { get; set; }
+        public int IdTipoCbte { get; set; }
+        public decimal IdCbteCble { get; set; }
+        public int secuencia { get; set; }
+        public Nullable<decimal> SaldoInicial { get; set; }
+        public string cb_Observacion { get; set; }
+        public string cb_Cheque { get; set; }
+        public string cb_giradoA { get; set; }
+        public Nullable<System.DateTime> cb_Fecha { get; set; }
+        public Nullable<decimal> Valor { get; set; }
+        public string Tipo { get; set; }
+        public Nullable<decimal> ValorAbsoluto { get; set; }
+        public int Orden { get; set; }
+        public string Referencia { get; set; }
+        public string ba_descripcion { get; set; }
+        public Nullable<int> IdBanco { get; set; }
+        public string Estado { get; set; }
+        public int IdSucursal { get; set; }
+        public string Su_Descripcion { get; set; }
+    }
+}
