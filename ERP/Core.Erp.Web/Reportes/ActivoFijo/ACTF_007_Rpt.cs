db0fc54529af95259fdee4b569dfe6ef4e01abea
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2019-02-14 16:47:53 -0500

    d

--- ERP/Core.Erp.Web/Reportes/ActivoFijo/ACTF_007_Rpt.cs
+++ ERP/Core.Erp.Web/Reportes/ActivoFijo/ACTF_007_Rpt.cs
@@ -3,15 +3,30 @@ using System.Drawing;
 using System.Collections;
 using System.ComponentModel;
 using DevExpress.XtraReports.UI;
+using Core.Erp.Bus.Reportes.ActivoFijo;
+using Core.Erp.Info.Reportes.ActivoFijo;
+using System.Collections.Generic;
 
 namespace Core.Erp.Web.Reportes.ActivoFijo
 {
     public partial class ACTF_007_Rpt : DevExpress.XtraReports.UI.XtraReport
     {
+        public string empresa { get; set; }
         public ACTF_007_Rpt()
         {
             InitializeComponent();
         }
 
+        private void ACTF_007_Rpt_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            lbl_empresa.Text = empresa;
+            lbl_fecha.Text = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss");
+            int IdEmpresa = p_IdEmpresa.Value == null ? 0 : Convert.ToInt32(p_IdEmpresa.Value);
+            int IdActivoFijo = p_IdActivoFijo.Value == null ? 0 : Convert.ToInt32(p_IdActivoFijo.Value);
+
+            ACTF_007_Bus bus_rpt = new ACTF_007_Bus();
+            List<ACTF_007_Info> lst_rpt = bus_rpt.GetList(IdEmpresa, IdActivoFijo);
+            this.DataSource = lst_rpt;
+        }
     }
 }
